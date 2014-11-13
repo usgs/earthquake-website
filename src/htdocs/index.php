8a0dca02f6c0e49c9ab316a457cb27386414abbe
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-11-13 08:11:23 -0700

    Changed title

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($TEMPLATE)) {
-  $TITLE = 'Home Page';
+  $TITLE = 'Featured Items';
   $HEAD = '<link rel="stylesheet" href="index.css"/>';
   include 'classes/ImageList.class.php';
   include 'template.inc.php';
