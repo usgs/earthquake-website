0e38b39f25e5e0c5b78746d088c361f697902f46
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-11-13 08:27:17 -0700

    changed prepare title to data & products, cleaned up some code

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -29,19 +29,19 @@ if (!isset($TEMPLATE)) {
       'images/the-great-shakeout-oct-16-2014.gif', //image
       'The Great ShakeOut - Oct 16, 2014', //title
       'This year\'s edition of The Great ShakeOut is scheduled for
-       October 16, 2014, at 10:16 AM local time. Participants across
-       the nation and around the world are encouraged to practice the safety
-       actions of &ldquo;drop, cover, and hold on&rdquo;, and to explore other
-       earthquake preparedness steps.'); //content
+          October 16, 2014, at 10:16 AM local time. Participants across
+          the nation and around the world are encouraged to practice the safety
+          actions of &ldquo;drop, cover, and hold on&rdquo;, and to explore other
+          earthquake preparedness steps.'); //content
 
     $list->addItem(
       'http://www.usgs.gov/newsroom/article.asp?ID=4028', //link
       'images/seismometers-to-measure-dc-shaking.jpg', //image
       'Seismometers to Measure DC Shaking', //title
       'Thirty seismometers are being installed in the Nation\'s capital this
-       winter to monitor ground tremors to better estimate the intensity of
-       ground shaking that can be expected during future earthquakes in the
-       area.'); //content
+          winter to monitor ground tremors to better estimate the intensity of
+          ground shaking that can be expected during future earthquakes in the
+          area.'); //content
 
     $list->display();
 ?>
@@ -59,21 +59,21 @@ if (!isset($TEMPLATE)) {
         '/images/earthquakes_button_square.gif', //image
         'Earthquakes', //title
         'Information about real-time earthquakes, online catalog search of
-         archives, seismicity maps and statistics.'); //content
+            archives, seismicity maps and statistics.'); //content
 
       $list->addItem(
         '/hazards/', //link
         '/images/hazards_button_square.gif', //image
         'hazards', //title
         'US faults, information about seismic hazards in various areas,
-         tools for seismic design values.'); //content
+            tools for seismic design values.'); //content
 
       $list->addItem(
         '/learn/', //link
         '/images/learn_button_square.gif', //image
         'Learn', //title
         'The science of earthquakes, FAQ, glossary, links to earthquake
-         topics, Earthquakes for kids.'); //content
+            topics, Earthquakes for kids.'); //content
 
       $list->display();
 ?>
@@ -85,23 +85,24 @@ if (!isset($TEMPLATE)) {
       $list->addItem(
         '/prepare/', //link
         '/images/prepare_button_square.png', //image
-        'Prepare', //title
-        'What should you do to be prepared for an earthquake?'); //content
+        'Data & Products', //title
+        'Monitoring, station, and other various seismic data available for
+            download. Data products to view and download.'); //content
 
       $list->addItem(
         '/monitoring/', //link
         '/images/monitoring_button_square.gif', //image
         'Monitoring', //title
         'Global, national and regional networks recording earthquakes and
-         crustal movements, maps, station information, real-time seismic
-         waveforms.'); //content
+            crustal movements, maps, station information, real-time seismic
+            waveforms.'); //content
 
       $list->addItem(
         '/research/', //link
         '/images/research_button_square.jpg', //image
         'Research', //title
         'Research projects in the Earthquake Hazards Program, external
-         research support, science center activities, software.'); //content
+            research support, science center activities, software.'); //content
 
       $list->display();
 ?>
