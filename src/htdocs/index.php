d8fd7870e71149409d843f3445f8e11d458a36fe
Jeremy Fee <jmfee@usgs.gov>
2014-11-18 13:40:30 -0700

    Update sections to be a single list

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -15,54 +15,52 @@ if (!isset($TEMPLATE)) {
 
 <hr/>
 
-<section class="two-up sections row">
-  <ul class="column">
-    <li>
-      <h3><a href="/earthquakes/">Earthquakes</a></h3>
+<section class="sections">
+  <ul class="row two-up">
+    <li class="column">
+      <h2><a href="/earthquakes/">Earthquakes</a></h2>
       <div class="row">
-        <img src="/images/earthquakes_button_square.gif" alt="Earthquakes Icon" class="column mobile-one-of-four"/>
+        <img src="/images/earthquakes_button_square.gif" alt="" class="column mobile-one-of-four"/>
         <p class="column mobile-three-of-four">
           Information about real-time earthquakes, online catalog search of
           archives, seismicity maps and statistics.
         </p>
       </a>
     </li>
-    <li>
-      <h3><a href="/hazards/">Hazards</a></h3>
+    <li class="column">
+      <h2><a href="/hazards/">Hazards</a></h2>
       <div class="row">
-        <img src="/images/hazards_button_square.gif" alt="Hazards Icon" class="column mobile-one-of-four"/>
+        <img src="/images/hazards_button_square.gif" alt="" class="column mobile-one-of-four"/>
         <p class="column mobile-three-of-four">
           US faults, information about seismic hazards in various areas,
           tools for seismic design values.
         </p>
       </a>
     </li>
-    <li>
-      <h3><a href="/data/">Data &amp; Products</a></h3>
+    <li class="column">
+      <h2><a href="/data/">Data &amp; Products</a></h2>
       <div class="row">
-        <img src="/images/data_products.jpg" alt="Data &amp; Products Icon" class="column mobile-one-of-four"/>
+        <img src="/images/data_products.jpg" alt="" class="column mobile-one-of-four"/>
         <p class="column mobile-three-of-four">
           Monitoring, station, and other various seismic data available for
           download. Data products to view and download.
         </p>
       </a>
     </li>
-  </ul>
-  <ul class="column">
-    <li>
-      <h3><a href="/learn/">Learn</a></h3>
+    <li class="column">
+      <h2><a href="/learn/">Learn</a></h2>
       <div class="row">
-        <img src="/images/learn_button_square.gif" alt="Learn Icon" class="column mobile-one-of-four"/>
+        <img src="/images/learn_button_square.gif" alt="" class="column mobile-one-of-four"/>
         <p class="column mobile-three-of-four">
           The science of earthquakes, FAQ, glossary, links to earthquake
           topics, Earthquakes for kids.
         </p>
       </a>
     </li>
-    <li>
-      <h3><a href="/monitoring">Monitoring</a></h3>
+    <li class="column">
+      <h2><a href="/monitoring">Monitoring</a></h2>
       <div class="row">
-        <img src="/images/monitoring_button_square.gif" alt="Monitoring Icon" class="column mobile-one-of-four"/>
+        <img src="/images/monitoring_button_square.gif" alt="" class="column mobile-one-of-four"/>
         <p class="column mobile-three-of-four">
           Global, national and regional networks recording earthquakes and
           crustal movements, maps, station information, real-time seismic
@@ -70,10 +68,10 @@ if (!isset($TEMPLATE)) {
         </p>
       </a>
     </li>
-    <li>
-      <h3><a href="/research/">Research</a></h3>
+    <li class="column">
+      <h2><a href="/research/">Research</a></h2>
       <div class="row">
-        <img src="/images/research_button_square.jpg" alt="Research Icon" class="column mobile-one-of-four"/>
+        <img src="/images/research_button_square.jpg" alt="" class="column mobile-one-of-four"/>
         <p class="column mobile-three-of-four">
           Research projects in the Earthquake Hazards Program, external
           research support, science center activities, software.
