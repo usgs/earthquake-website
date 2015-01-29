6971d3ec421642ed39ee1ffddbd3e70a89f53ee7
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-29 12:10:58 -0700

    created and setup cssmin

---
+++ gruntconfig/cssmin.js
@@ -0,0 +1,20 @@
+'use strict':
+
+var config = require('./config');
+
+var cssmin = {
+  test: {
+    expand: true,
+    cwd: config.build + '/test',
+    src: '**/*.css'
+    dest: config.dist
+  },
+  build: {
+    expand: true,
+    cwd: config.build + '/src',
+    src: '**/*.css',
+    dest: config.dist
+  }
+};
+
+module.exports = cssmin;
