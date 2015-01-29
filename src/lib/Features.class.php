d8b36c2f3a9ffb6d46c5fdb32378b39c5b20284c
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-29 15:44:54 -0700

    replaced tabs with spaces

--- src/lib/Features.class.php
+++ src/lib/Features.class.php
@@ -38,177 +38,177 @@
  */
 class Features {
 
-	// id for features list.
-	public $id = 'usgs_earthquake_research';
-	// author for features list.
-	public $author = 'U.S. Geological Survey';
-	// site url (ends with a /).
-	public $siteUrl = 'http://earthquake.usgs.gov/';
-	// base url for section (ends with a /).
-	public $baseUrl;
-	// title of features list.
-	public $title;
-	// items in list.
-	public $items;
+  // id for features list.
+  public $id = 'usgs_earthquake_research';
+  // author for features list.
+  public $author = 'U.S. Geological Survey';
+  // site url (ends with a /).
+  public $siteUrl = 'http://earthquake.usgs.gov/';
+  // base url for section (ends with a /).
+  public $baseUrl;
+  // title of features list.
+  public $title;
+  // items in list.
+  public $items;
 
-	/**
-	 * Construct a new Features list.
-	 */
-	public function __construct ($items=null) {
-		$this->items = array();
-	}
+  /**
+   * Construct a new Features list.
+   */
+  public function __construct ($items=null) {
+    $this->items = array();
+  }
 
-	/**
-	 * Get list of items currently "publish"able.
-	 *
-	 * @return {Array<Item>} all items without a publish date, or a publish date in the past.
-	 */
-	public function getItems () {
-		$now = time();
-		$items = array();
-		foreach ($this->items as $item) {
-			if (!isset($item['publish']) || $item['publish'] <= $now) {
-				$items[] = $item;
-			}
-		}
-		return $items;
-	}
+  /**
+   * Get list of items currently "publish"able.
+   *
+   * @return {Array<Item>} all items without a publish date, or a publish date in the past.
+   */
+  public function getItems () {
+    $now = time();
+    $items = array();
+    foreach ($this->items as $item) {
+      if (!isset($item['publish']) || $item['publish'] <= $now) {
+        $items[] = $item;
+      }
+    }
+    return $items;
+  }
 
-	/**
-	 * Format Features list as Html.
-	 *
-	 * @param $numItems {Integer} default 4.
-	 *        total number of items to include in list.
-	 * @param $numFeatured {Integer} default 1.
-	 *        number of items to "feature".
-	 * @return {String} html.
-	 */
-	public function toHtml($numItems = 4, $numFeatured = 1) {
-		$items = $this->getItems();
-		$len = count($items);
+  /**
+   * Format Features list as Html.
+   *
+   * @param $numItems {Integer} default 4.
+   *        total number of items to include in list.
+   * @param $numFeatured {Integer} default 1.
+   *        number of items to "feature".
+   * @return {String} html.
+   */
+  public function toHtml($numItems = 4, $numFeatured = 1) {
+    $items = $this->getItems();
+    $len = count($items);
 
-		$r = '';
-		for ($i = 0; $i < $len && $i < $numFeatured; $i++) {
-			$r .= $this->getFeaturedHtml($items[$i]);
-		}
+    $r = '';
+    for ($i = 0; $i < $len && $i < $numFeatured; $i++) {
+      $r .= $this->getFeaturedHtml($items[$i]);
+    }
 
-		$r .= '<ul class="sub-features">';
-		for ($i = $numFeatured; $i < $len && $i < $numItems; $i++) {
-			$r .= '<li>' .
-				$this->getItemHtml($items[$i]) .
-				'</li>';
-		}
-		$r .= '</ul>';
-		return $r;
-	}
+    $r .= '<ul class="sub-features">';
+    for ($i = $numFeatured; $i < $len && $i < $numItems; $i++) {
+      $r .= '<li>' .
+        $this->getItemHtml($items[$i]) .
+        '</li>';
+    }
+    $r .= '</ul>';
+    return $r;
+  }
 
-	/**
-	 * Format a featured item as Html
-	 *
-	 * @param item {Item}
-	 * @return {String} html formatted item.
-	 */
-	protected function getFeaturedHtml ($item) {
-		$r = '<div class="main-featured row">' .
-					'<h2>' .
-						'<a href="' . $item['link'] . '">' . $item['title'] . '</a>' .
-					'</h2>' .
-					'<div class="one-of-four column">' .
-						'<img class="main-featured-image" src="' . $item['image'] .
-								'" alt=""/>' .
-					'</div>' .
-					'<div class="three-of-four column">' . $item['content'] . '</div>' .
-				'</div>';
-		return $r;
-	}
+  /**
+   * Format a featured item as Html
+   *
+   * @param item {Item}
+   * @return {String} html formatted item.
+   */
+  protected function getFeaturedHtml ($item) {
+    $r = '<div class="main-featured row">' .
+          '<h2>' .
+            '<a href="' . $item['link'] . '">' . $item['title'] . '</a>' .
+          '</h2>' .
+          '<div class="one-of-four column">' .
+            '<img class="main-featured-image" src="' . $item['image'] .
+                '" alt=""/>' .
+          '</div>' .
+          '<div class="three-of-four column">' . $item['content'] . '</div>' .
+        '</div>';
+    return $r;
+  }
 
-	/**
-	 * Format an item as Html.
-	 *
-	 * @param item {Item}
-	 * @return {String} html formatted item.
-	 */
-	protected function getItemHtml ($item) {
-		return '<h2>' .
-			'<a href="' . $item['link'] . '">'. $item['title'] . '</a>' .
-			'</h2>' .
-			'<div>' . $item['content'] . '</div>';
-	}
+  /**
+   * Format an item as Html.
+   *
+   * @param item {Item}
+   * @return {String} html formatted item.
+   */
+  protected function getItemHtml ($item) {
+    return '<h2>' .
+      '<a href="' . $item['link'] . '">'. $item['title'] . '</a>' .
+      '</h2>' .
+      '<div>' . $item['content'] . '</div>';
+  }
 
-	/**
-	 * Format Features list as Atom feed.
-	 *
-	 * @return {String} atom feed.
-	 */
-	public function toAtom() {
-		$items = $this->getItems();
-		$r = '';
-		$r .= '<?xml version="1.0" encoding="utf-8"?>' . PHP_EOL .
-			'<feed xmlns="http://www.w3.org/2005/Atom">' .
-				'<title>' . $this->title . '</title>' .
-				'<updated>' . $this->getAtomDate(time()) . '</updated>' .
-				'<author>' .
-					'<name>' . $this->author . '</name>' .
-					'<uri>' . $this->siteUrl . '</uri>' .
-				'</author>' .
-				'<id>' . $this->id . '</id>';
-		foreach ($items as $item) {
-			$r .= $this->getAtomEntry($item);
-		}
-		$r .= '</feed>';
-		return $r;
-	}
+  /**
+   * Format Features list as Atom feed.
+   *
+   * @return {String} atom feed.
+   */
+  public function toAtom() {
+    $items = $this->getItems();
+    $r = '';
+    $r .= '<?xml version="1.0" encoding="utf-8"?>' . PHP_EOL .
+      '<feed xmlns="http://www.w3.org/2005/Atom">' .
+        '<title>' . $this->title . '</title>' .
+        '<updated>' . $this->getAtomDate(time()) . '</updated>' .
+        '<author>' .
+          '<name>' . $this->author . '</name>' .
+          '<uri>' . $this->siteUrl . '</uri>' .
+        '</author>' .
+        '<id>' . $this->id . '</id>';
+    foreach ($items as $item) {
+      $r .= $this->getAtomEntry($item);
+    }
+    $r .= '</feed>';
+    return $r;
+  }
 
-	/**
-	 * Format an item as an Atom entry.
-	 *
-	 * @param item {Item}
-	 * @return {String} atom entry element.
-	 */
-	protected function getAtomEntry ($item) {
-		$r = '';
-		$r .= '<entry>' .
-			'<id>' . $item['id'] . '</id>' .
-			'<title>' . $item['title'] . '</title>' .
-			'<updated>' . $this->getAtomDate($item['modified']) . '</updated>' .
-			'<link rel="alternate" type="text/html" href="' . $this->getLink($item['link']) . '"/>' .
-			'<summary type="html"><![CDATA[' .
-				'<img src="' . $this->getLink($item['thumbnail']) . '" width="100" align="left" hspace="10"/>' .
-				$item['content'] .
-			']]></summary>';
-		if (isset($item['tags'])) {
-			foreach ($item['tags'] as $tag) {
-				$r .= '<category term="' . $tag . '"/>';
-			}
-		}
-		$r .= '</entry>';
-		return $r;
-	}
+  /**
+   * Format an item as an Atom entry.
+   *
+   * @param item {Item}
+   * @return {String} atom entry element.
+   */
+  protected function getAtomEntry ($item) {
+    $r = '';
+    $r .= '<entry>' .
+      '<id>' . $item['id'] . '</id>' .
+      '<title>' . $item['title'] . '</title>' .
+      '<updated>' . $this->getAtomDate($item['modified']) . '</updated>' .
+      '<link rel="alternate" type="text/html" href="' . $this->getLink($item['link']) . '"/>' .
+      '<summary type="html"><![CDATA[' .
+        '<img src="' . $this->getLink($item['thumbnail']) . '" width="100" align="left" hspace="10"/>' .
+        $item['content'] .
+      ']]></summary>';
+    if (isset($item['tags'])) {
+      foreach ($item['tags'] as $tag) {
+        $r .= '<category term="' . $tag . '"/>';
+      }
+    }
+    $r .= '</entry>';
+    return $r;
+  }
 
-	/**
-	 * Format a time as ISO8601.
-	 *
-	 * @param $time {Integer}
-	 *        unix epoch timestamp in seconds.
-	 * @return {String} ISO8601 formatted date.
-	 */
-	protected function getAtomDate ($time) {
-		return gmdate('Y-m-d\TH:i:s\Z', $time);
-	}
+  /**
+   * Format a time as ISO8601.
+   *
+   * @param $time {Integer}
+   *        unix epoch timestamp in seconds.
+   * @return {String} ISO8601 formatted date.
+   */
+  protected function getAtomDate ($time) {
+    return gmdate('Y-m-d\TH:i:s\Z', $time);
+  }
 
-	/**
-	 * Get an absolute link from a relative link.
-	 *
-	 * @param $link {String}
-	 *        relative or absolute link.
-	 * @return {String} absolute link.
-	 */
-	protected function getLink ($link) {
-		if (strpos($link, 'http') === 0) {
-			return $link;
-		}
-		return $this->siteUrl . $this->baseUrl . $link;
-	}
+  /**
+   * Get an absolute link from a relative link.
+   *
+   * @param $link {String}
+   *        relative or absolute link.
+   * @return {String} absolute link.
+   */
+  protected function getLink ($link) {
+    if (strpos($link, 'http') === 0) {
+      return $link;
+    }
+    return $this->siteUrl . $this->baseUrl . $link;
+  }
 
 }
 
--- test/_config.inc.php
+++ test/_config.inc.php
@@ -14,29 +14,29 @@ $SITE_SITENAV =
 
 // at bottom of page
 $SITE_COMMONNAV =
-	navItem(($SITE_URL), 'Home') .
-	navItem('/aboutus/', 'About Us') .
-	navItem('/contactus/regional.php', 'Contact Us') .
-	navItem('http://www.usgs.gov/legal/', 'Legal');
+  navItem(($SITE_URL), 'Home') .
+  navItem('/aboutus/', 'About Us') .
+  navItem('/contactus/regional.php', 'Contact Us') .
+  navItem('http://www.usgs.gov/legal/', 'Legal');
 
 $HEAD =
 // site theme, should use a site root-relative URL
-	'<link rel="stylesheet" href="/theme/site/earthquake/index.css"/>' .
+  '<link rel="stylesheet" href="/theme/site/earthquake/index.css"/>' .
 // page head content
-	($HEAD ? $HEAD : '') .
+  ($HEAD ? $HEAD : '') .
 // description meta
-	'<meta name="description" content="' .
-		'USGS Earthquake Hazards Program, responsible for' .
-		' monitoring, reporting, and researching earthquakes and' .
-		' earthquake hazards' .
+  '<meta name="description" content="' .
+    'USGS Earthquake Hazards Program, responsible for' .
+    ' monitoring, reporting, and researching earthquakes and' .
+    ' earthquake hazards' .
 '"/>' .
 // keywords meta
-	'<meta name="keywords" content="' .
-		'aftershock,earthquake,epicenter,fault,foreshock,geologist,' .
-		'geophysics,hazard,hypocenter,intensity,intensity scale,magnitude,' .
-		'magnitude scale,mercalli,plate,richter,seismic,seismicity,' .
-		'seismogram,seismograph,seismologist,seismology,subduction,' .
-		'tectonics,tsunami,quake,sismologico,sismologia' .
+  '<meta name="keywords" content="' .
+    'aftershock,earthquake,epicenter,fault,foreshock,geologist,' .
+    'geophysics,hazard,hypocenter,intensity,intensity scale,magnitude,' .
+    'magnitude scale,mercalli,plate,richter,seismic,seismicity,' .
+    'seismogram,seismograph,seismologist,seismology,subduction,' .
+    'tectonics,tsunami,quake,sismologico,sismologia' .
 '"/>';
 
 ?>
