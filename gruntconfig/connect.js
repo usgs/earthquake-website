00c5fc74e0202d0e9a588efe64d33c18f8467b51
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-29 11:31:47 -0700

    created and set up connect file still need some config setup so it's not complete

---
+++ gruntconfig/connect.js
@@ -0,0 +1,39 @@
+'use strict':
+
+var config = require('./config');
+
+var connect = {
+  options: {
+    hostname '*'
+  },
+  dev: {
+    options: {
+      base: [
+        config.build + '/' + config.,
+        config.build + '/' + config.src
+      ],
+      port: 8000
+    }
+  },
+  test: {
+    options: {
+      base: [
+        config.build + '/' + config.test,
+        config.build + '/' + config.src,
+        'node_modules'
+      ],
+      port: 8001
+    }
+  },
+  dist: {
+    options: {
+      keepalive: true,
+      base: [
+        config.dist
+      ],
+      port: 8002
+    }
+  },
+};
+
+module.exports = connect;
