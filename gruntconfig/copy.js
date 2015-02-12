95138f6dda4508b2d4d33279d4689ac2c8f5651d
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-12 10:35:12 -0700

    removed commented out code

--- gruntconfig/copy.js
+++ gruntconfig/copy.js
@@ -2,40 +2,6 @@
 
 var config = require('./config');
 
-// var copy = {
-//   app: {
-//     expand: true,
-//       cwd: [config.src + '/htdocs'],
-//       dest: [config.dist + '/htdocs'],
-//       src: [
-//         '**/*',
-//         '!**/*.scss'
-//       ]
-//     },
-//   conf: {
-//     expand: true,
-//     cwd: [config.src + '/conf'],
-//     dest: [config.dist + '/conf'],
-//     src: [
-//       '**/*',
-//       '!**/*.orig'
-//     ]
-//   },
-//   lib: {
-//     expand: true,
-//     cwd: [config.src + '/lib'],
-//     dest: [config.dist + '/lib'],
-//     src: ['**/*']
-//   },
-//   templateConfig: {
-//     expand: true,
-//     cwd: [config.src + '/htdocs'],
-//     dest: config.test,
-//     src: ['_config.inc.php']
-//   }
-// };
-
-var copy = {
   build: {
     expand: true,
     cwd: config.src,
