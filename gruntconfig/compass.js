1b5e3c3ecad83fe0bc8d1b5c32b946dc92e8ac9e
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-12 10:26:50 -0700

    fixed paths and added specify path

--- gruntconfig/compass.js
+++ gruntconfig/compass.js
@@ -6,17 +6,11 @@ var compass = {
   dev: {
     options: {
       sassDir: [config.src + '/htdocs'],
-      cssDir: config.build + '/src',
+      specify: config.src + '/earthquake-website',
+      cssDir: config.build + config.src,
       environment: 'development'
     }
-   }//,
-  // test: {
-  //   options: {
-  //     sassDir: config.test,
-  //     cssDir: config.build + '/test',
-  //     environment: 'development'
-  //   }
-  // }
+  }
 };
 
 module.exports = compass;
