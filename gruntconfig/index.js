0f4926def6d74b1e6ea2e9de3d7b3528a6ca87eb
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-13 13:25:40 -0700

    added tasks that are needed and removed tasks that are no longer used

--- gruntconfig/index.js
+++ gruntconfig/index.js
@@ -1,23 +1,22 @@
 'use strict';
 
 var config = {
-  browserify: require('./browserify'),
   clean: require('./clean'),
   compass: require('./compass'),
+  concurrent: require('./concurrent'),
   connect: require('./connect'),
   copy: require('./copy'),
   jshint: require('./jshint'),
-  mocha_phantomjs: require('./mocha_phantomjs'),
   watch: require('./watch'),
 
   tasks: [
-    'grunt-browserify',
+    'grunt-concurrent',
+    'grunt-connect-proxy',
     'grunt-contrib-clean',
     'grunt-contrib-compass',
     'grunt-contrib-connect',
     'grunt-contrib-copy',
     'grunt-contrib-jshint',
-    'grunt-mocha-phantomjs',
     'grunt-contrib-watch'
   ]
 };
