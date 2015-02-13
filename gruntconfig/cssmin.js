0f971066606cabc6860c812efbb8024add1dba33
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-13 14:16:09 -0700

    created cssmin file

---
+++ gruntconfig/cssmin.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var config = require('./config');
+
+var cssmin = {
+  dist: {
+    src: config.build + '/' + 'htdocs/' + '**/*.css',
+    dest: config.dist + 'earthquake-website.css'
+  }
+};
+
+module.exports = cssmin;
