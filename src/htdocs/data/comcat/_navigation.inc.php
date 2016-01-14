4e0137ed84034536225ea758a34c12eb79bc7b18
Jeremy Fee <jmfee@usgs.gov>
2016-01-14 09:54:42 -0700

    Add index, skeleton for catalog pages

--- src/htdocs/data/comcat/_navigation.inc.php
+++ src/htdocs/data/comcat/_navigation.inc.php
@@ -1,6 +1,9 @@
 <?php
 
 
+echo navItem('/data/comcat/index.php',
+    'About ANSS Comprehensive Catalog and Important Caveats');
+
 echo navGroup('Metadata',
     navItem('/data/comcat/catalog/', 'Catalogs') .
     navItem('/data/comcat/contributor/', 'Contributors'));
