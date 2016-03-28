7e7e40f3efb4a12c522fbc0272a231417c51b20f
Lisa Wald <lisa@usgs.gov>
2016-03-28 22:54:51 +0000

    dyfi 2015 updates and updates to catalog files, added headers

--- src/htdocs/data/dyfi/summary-maps.php
+++ src/htdocs/data/dyfi/summary-maps.php
@@ -10,69 +10,134 @@
   }
 ?>
 
-<p>All responses for each year are shown on the yearly maps.  Years for which
-there were no responses do not have a map.<br />
-
-<p>Downloads:
-  <ul>
-    <li><a href="DYFIallUS.zip">U.S. Maps (73.6MB ZIP file)</a></li>
-    <li><a href="DYFIallWorld.zip">Global Maps (69.6MB ZIP file)</a></li>
-  </ul>
-</p>
-<hr/>
-
-  <h2>U.S. Cumulative Responses 1991&mdash;2014</h2>
-  <img class="map-cumul" src="images/DYFIUS_large_through2014.png" alt="USALLlarge" />
-
-  <h2>Global Cumulative Responses 1991&mdash;2014</h2>
-  <img class="map-cumul" src="images/DYFIglobalthrough2014.png" alt="GlobalALLlarge" />
-<hr/>
-
 <div class="row">
-  <div class=" column one-of-two">
-    <h2>U.S. Yearly Response</h2>
 
-    <img class="map" src="images/DYFIUS_2014large.png" alt="2014 US Responses" />
-    <img class="map" src="images/DYFIUS_2013large.png" alt="2013 US Responses" />
-    <img class="map" src="images/DYFIUS_2012large.png" alt="2012 US Responses" />
-    <img class="map" src="images/DYFIUS_2011large.png" alt="2011 US Responses" />
-    <img class="map" src="images/DYFIUS_2010large.png" alt="2010 US Responses" />
-    <img class="map" src="images/DYFIUS_2009large.png" alt="2009 US Responses" />
-    <img class="map" src="images/DYFIUS_2008large.png" alt="2008 US Responses" />
-    <img class="map" src="images/DYFIUS_2007large.png" alt="2007 US Responses" />
-    <img class="map" src="images/DYFIUS_2006large.png" alt="2006 US Responses" />
-    <img class="map" src="images/DYFIUS_2005large.png" alt="2005 US Responses" />
-    <img class="map" src="images/DYFIUS_2004large.png" alt="2004 US Responses" />
-    <img class="map" src="images/DYFIUS_2003large.png" alt="2003 US Responses" />
-    <img class="map" src="images/DYFIUS_2002large.png" alt="2002 US Responses" />
-    <img class="map" src="images/DYFIUS_2001large.png" alt="2001 US Responses" />
-    <img class="map" src="images/DYFIUS_2000large.png" alt="2000 US Responses" />
-    <img class="map" src="images/DYFIUS_1999large.png" alt="1999 US Responses" />
-    <img class="map" src="images/DYFIUS_1998large.png" alt="1998 US Responses" />
-    <img class="map" src="images/DYFIUS_1994large.png" alt="1994 US Responses" />
-    <img class="map" src="images/DYFIUS_1992large.png" alt="1991 US Responses" />
-    <img class="map" src="images/DYFIUS_1991large.png" alt="1991 US Responses" />
+<section class=" column one-of-two">
+  <h2>DYFI Annual Maps - U.S. &amp; Global</h2>
+  <p>
+    All responses for each year are shown on the annual maps.  Years for which there were no responses do not have a map. All files are PDFs.
+  </p>
 
-  </div>
+  <table>
+    <thead>
+      <th>Year</th>
+      <th>U.S.</th>
+      <th>Global</th>
+    </thead>
+    <tr>
+      <td>2015</td>
+      <td><a href="pdfs-us/US2015.pdf">U.S. 2015 Map</a></td>
+      <td><a href="pdfs-global/Global2015.pdf">Global 2015 Map</a></td>
+    </tr>
+    <tr>
+      <td>2014</td>
+      <td><a href="pdfs-us/US2014.pdf">U.S. 2014 Map</a></td>
+      <td><a href="pdfs-global/Global2014.pdf">Global 2014 Map</a></td>
+    </tr>
+    <tr>
+      <td>2013</td>
+      <td><a href="pdfs-us/US2013.pdf">U.S. 2013 Map</a></td>
+      <td><a href="pdfs-global/Global2013.pdf">Global 2013 Map</a></td>
+    </tr>
+    <tr>
+      <td>2012</td>
+      <td><a href="pdfs-us/US2012.pdf">U.S. 2012 Map</a></td>
+      <td><a href="pdfs-global/Global2012.pdf">Global 2012 Map</a></td>
+    </tr>
+    <tr>
+      <td>2011</td>
+      <td><a href="pdfs-us/US2011.pdf">U.S. 2011 Map</a></td>
+      <td><a href="pdfs-global/Global2011.pdf">Global 2011 Map</a></td>
+    </tr>
+    <tr>
+      <td>2010</td>
+      <td><a href="pdfs-us/US2010.pdf">U.S. 2010 Map</a></td>
+      <td><a href="pdfs-global/Global2010.pdf">Global 2010 Map</a></td>
+    </tr>
+    <tr>
+      <td>2009</td>
+      <td><a href="pdfs-us/US2009.pdf">U.S. 2009 Map</a></td>
+      <td><a href="pdfs-global/Global2009.pdf">Global 2009 Map</a></td>
+    </tr>
+    <tr>
+      <td>2008</td>
+      <td><a href="pdfs-us/US2008.pdf">U.S. 2008 Map</a></td>
+      <td><a href="pdfs-global/Global2008.pdf">Global 2008 Map</a></td>
+    </tr>
+    <tr>
+      <td>2007</td>
+      <td><a href="pdfs-us/US2007.pdf">U.S. 2007 Map</a></td>
+      <td><a href="pdfs-global/Global2007.pdf">Global 2007 Map</a></td>
+    </tr>
+    <tr>
+      <td>2006</td>
+      <td><a href="pdfs-us/US2006.pdf">U.S. 2006 Map</a></td>
+      <td><a href="pdfs-global/Global2006.pdf">Global 2006 Map</a></td>
+    </tr>
+    <tr>
+      <td>2005</td>
+      <td><a href="pdfs-us/US2005.pdf">U.S. 2005 Map</a></td>
+      <td><a href="pdfs-global/Global2005.pdf">Global 2005 Map</a></td>
+    </tr>
+    <tr>
+      <td>2004</td>
+      <td><a href="pdfs-us/US2004.pdf">U.S. 2004 Map</a></td>
+      <td><a href="pdfs-global/Global2004.pdf">Global 2004 Map</a></td>
+    </tr>
+    <tr>
+      <td>2003</td>
+      <td><a href="pdfs-us/US2003.pdf">U.S. 2003 Map</a></td>
+      <td><a href="pdfs-global/Global2003.pdf">Global 2003 Map</a></td>
+    </tr>
+    <tr>
+      <td>2002</td>
+      <td><a href="pdfs-us/US2002.pdf">U.S. 2002 Map</a></td>
+      <td><a href="pdfs-global/Global2002.pdf">Global 2002 Map</a></td>
+    </tr>
+    <tr>
+      <td>2001</td>
+      <td><a href="pdfs-us/US2001.pdf">U.S. 2001 Map</a></td>
+      <td><a href="pdfs-global/Global2001.pdf">Global 2001 Map</a></td>
+    </tr>
+    <tr>
+      <td>2000</td>
+      <td><a href="pdfs-us/US2000.pdf">U.S. 2000 Map</a></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>1999</td>
+      <td><a href="pdfs-us/US1999.pdf">U.S. 1999 Map</a></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>1998</td>
+      <td><a href="pdfs-us/US1998.pdf">U.S. 1998 Map</a></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>1994</td>
+      <td><a href="pdfs-us/US1994.pdf">U.S. 1994 Map</a></td>
+      <td><a href="pdfs-global/Global1994.pdf">Global 1994 Map</a></td>
+    </tr>
+    <tr>
+      <td>1992</td>
+      <td><a href="pdfs-us/US1992.pdf">U.S. 1992 Map</a></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>1991</td>
+      <td><a href="pdfs-us/US1991.pdf">U.S. 1991 Map</a></td>
+      <td></td>
+    </tr>
+  </table>
+</section>
 
-  <div class=" column one-of-two">
-    <h2>Yearly Responses</h2>
+  <section class="column one-of-two">
+  <h2>U.S. Cumulative Responses 1991&mdash;2015</h2>
+  <img class="map-cumul" src="images/US_cumulative2015.png" alt="all U.S. DYFI responses through 2015"/>
+  <h2>Global Cumulative Responses 1991&mdash;2015</h2>
+  <img class="map-cumul" src="images/Global_cumulative2015.png" alt="all Global DYFI responses through 2015"/>
 
-    <img class="map" src="images/DYFIglobal_2014large.png" alt="2014 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2013large.png" alt="2013 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2012large.png" alt="2012 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2011large.png" alt="2011 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2010large.png" alt="2010 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2009large.png" alt="2009 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2008large.png" alt="2008 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2007large.png" alt="2007 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2006large.png" alt="2006 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2005large.png" alt="2005 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2004large.png" alt="2004 Global Responsese" />
-    <img class="map" src="images/DYFIglobal_2003large.png" alt="2003 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2002large.png" alt="2002 Global Responses" />
-    <img class="map" src="images/DYFIglobal_2001large.png" alt="2001 Global Responses" />
-    <img class="map" src="images/DYFIglobal_1994large.png" alt="1994 Global Responses" />
+  </section>
 
-  </div>
 </div>
