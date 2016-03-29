e49f8a2754d2467e7341084ce5ecee67e783585e
lisa-usgs <lisa@usgs.gov>
2016-03-29 11:21:50 -0600

    initial commit
    Former-commit-id: 7008df6dc97ec4418d618b40002c2f7e5b11c01f

---
+++ src/htdocs/earthquakes/browse/stats.php
@@ -0,0 +1,304 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: Paul Earle,
+  // Last modified: 03/01/2016
+  if (!isset($TEMPLATE)) {
+  $TITLE = 'Earthquake Statistics';
+  $NAVIGATION = true;
+  include 'template.inc.php';
+  }
+?>
+
+<h2>Worldwide Earthquakes 2000&ndash;2015</h2>
+<div class="horizontal-scrolling">
+  <table>
+
+    <thead>
+      <tr>
+        <th>Magnitude</th>
+        <th>2000</th>
+        <th>2001</th>
+        <th>2002</th>
+        <th>2003</th>
+        <th>2004</th>
+        <th>2005</th>
+        <th>2006</th>
+        <th>2007</th>
+        <th>2008</th>
+        <th>2009</th>
+        <th>2010</th>
+        <th>2011</th>
+        <th>2012</th>
+        <th>2013</th>
+        <th>2014</th>
+        <th>2015</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <th scope="row">8.0+</th>
+        <td>1</td><td>1</td><td>0</td><td>1</td><td>2</td>
+        <td>1</td><td>2</td><td>4</td><td>0</td><td>1</td>
+        <td>1</td><td>1</td><td>2</td><td>2</td><td>1</td>
+        <td>1</td>
+      </tr>
+
+      <tr>
+        <th scope="row">7&ndash;7.9</th>
+        <td>14</td><td>15</td><td>13</td><td>14</td><td>14</td>
+        <td>10</td><td> 9</td><td>14</td><td>12</td><td>16</td>
+        <td>23</td><td>19</td><td>12</td><td>17</td><td>11</td>
+        <td>18</td>
+      </tr>
+
+      <tr>
+        <th scope="row">6&ndash;6.9</th>
+        <td>146</td><td>121</td><td>127</td><td>140</td><td>141</td>
+        <td>140</td><td>142</td><td>178</td><td>168</td><td>144</td>
+        <td>150</td><td>185</td><td>108</td><td>123</td><td>143</td>
+        <td>127</td>
+      </tr>
+
+      <tr>
+        <th scope="row">5&ndash;5.9</th>
+        <td>1344</td><td>1224</td><td>1201</td><td>1203</td><td>1515</td>
+        <td>1693</td><td>1712</td><td>2074</td><td>1768</td><td>1896</td>
+        <td>2209</td><td>2276</td><td>1401</td><td>1453</td><td>1574</td>
+        <td>1419</td>
+      </tr>
+
+      <tr >
+        <th scope="row">Estimated<br/>Deaths</th>
+        <td>231</td><td>21357</td><td>1685</td><td>33819</td><td>298101</td>
+        <td>87992</td><td>6605</td><td>708</td><td>88708</td><td>1790</td>
+        <td>226050</td><td>21942</td><td>689</td><td>1572</td><td>756</td>
+        <td>9624</td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+<h2>United States Earthquakes 2000&ndash;2012</h2>
+<div class="horizontal-scrolling">
+  <table>
+
+    <thead>
+      <tr>
+        <th>Magnitude</th>
+        <th>2000</th>
+        <th>2001</th>
+        <th>2002</th>
+        <th>2003</th>
+        <th>2004</th>
+        <th>2005</th>
+        <th>2006</th>
+        <th>2007</th>
+        <th>2008</th>
+        <th>2009</th>
+        <th>2010</th>
+        <th>2011</th>
+        <th>2012</th>
+      </tr>
+    </thead>
+
+    <tbody>
+
+      <tr>
+        <th scope="row">8+</th>
+        <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
+        <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
+        <td>0</td><td>0</td><td>0</td>
+      </tr>
+
+      <tr>
+        <th scope="row">7&ndash;7.9</th>
+        <td>0</td><td>1</td><td>1</td><td>2</td><td>0</td>
+        <td>1</td><td>0</td><td>1</td><td>0</td><td>0</td>
+        <td>1</td><td>1</td><td>0</td>
+      </tr>
+
+      <tr>
+        <th scope="row">6&ndash;6.9</th>
+        <td>6</td><td>5</td><td>4</td><td>7</td><td>2</td>
+        <td>4</td><td>7</td><td>9</td><td>9</td><td>4</td>
+        <td>8</td><td>3</td><td>5</td>
+      </tr>
+
+      <tr>
+        <th scope="row">5&ndash;5.9</th>
+        <td>63</td><td>41</td><td>63</td><td>54</td><td>25</td>
+        <td>47</td><td>51</td><td>72</td><td>85</td><td>58</td>
+        <td>89</td><td>51</td><td>27</td>
+      </tr>
+
+      <tr >
+        <th scope="row">Estimated<br/>Deaths</th>
+        <td>0</td><td>0</td><td>0</td><td>2</td><td>0</td>
+        <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
+        <td>0</td><td>0</td><td>0</td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+<h2>Worldwide Earthquakes 1990&ndash;1999</h2>
+<div class="horizontal-scrolling">
+  <table>
+
+  <thead>
+  <tr >
+  <th>Magnitude</th>
+  <th>1990</th>
+  <th>1991</th>
+  <th>1992</th>
+  <th>1993</th>
+  <th>1994</th>
+  <th>1995</th>
+  <th>1996</th>
+  <th>1997</th>
+  <th>1998</th>
+  <th>1999</th>
+  </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <th scope="row"> 8+</th>
+      <td> 0</td><td> 0</td><td> 0</td><td> 0</td><td> 2</td>
+      <td> 2</td><td> 1</td><td> 0</td><td> 1</td><td> 0</td>
+    </tr>
+
+    <tr>
+      <th scope="row">7&ndash;7.9</th>
+      <td>18</td><td>16</td><td> 13</td><td> 12</td><td> 11</td>
+      <td> 18</td><td> 14</td><td> 16</td><td> 11</td><td> 18</td>
+    </tr>
+
+    <tr>
+      <th scope="row">6&ndash;6.9</th>
+      <td>109</td><td> 96</td><td> 166</td><td> 137</td><td> 146</td>
+      <td>183</td><td> 149</td><td> 120</td><td> 117</td><td> 116</td>
+    </tr>
+
+    <tr>
+      <th scope="row">5&ndash;5.9</th>
+      <td> 1617</td><td> 1457</td><td> 1498</td><td> 1426</td><td> 1542</td>
+      <td> 1318</td><td> 1222</td><td> 1113</td><td> 979</td><td> 1104</td>
+    </tr>
+
+    <tr>
+      <th scope="row">Estimated<br />Deaths</th>
+      <td>52056</td><td>3210</td><td>3920</td><td>10096</td><td>1634</td>
+      <td>7980</td><td>589</td><td>3069</td><td>9430</td><td>22662</td>
+    </tr>
+
+  </tbody>
+</table>
+</div>
+
+
+<h2>United States Earthquakes 1990&ndash;1999</h2>
+<div class="horizontal-scrolling">
+  <table>
+
+    <thead>
+      <tr>
+        <th>Magnitude </th>
+        <th>1990</th>
+        <th>1991</th>
+        <th>1992</th>
+        <th>1993</th>
+        <th>1994</th>
+        <th>1995</th>
+        <th>1996</th>
+        <th>1997</th>
+        <th>1998</th>
+        <th>1999</th>
+      </tr>
+    </thead>
+
+    <tbody>
+
+      <tr>
+        <th scope="row">8+</th>
+        <td> 0</td><td> 0</td><td> 0</td><td> 0</td><td> 0</td>
+        <td> 0</td><td> 0</td><td> 0</td><td> 0</td><td> 0</td>
+      </tr>
+
+      <tr>
+        <th scope="row">7&ndash;7.9</th>
+        <td>0</td><td>1</td><td>2</td><td>0</td><td>1</td>
+        <td>0</td><td>2</td><td>0</td><td>0</td><td>1</td>
+      </tr>
+
+      <tr >
+        <th scope="row">6&ndash;6.9</th>
+        <td> 2</td><td> 4</td><td>15</td><td> 9</td><td> 4</td>
+        <td> 6</td><td> 4</td><td> 6</td><td> 3</td><td> 6</td>
+      </tr>
+
+      <tr>
+        <th scope="row">5&ndash;5.9</th>
+        <td> 64</td><td> 49</td><td> 72</td><td> 62</td><td> 64</td>
+        <td> 45</td><td>100</td><td> 63</td><td> 62</td><td> 50</td>
+      </tr>
+
+      <tr>
+        <th scope="row">Estimated<br />Deaths</th>
+        <td>0</td><td>2</td><td>3</td><td>2</td><td>60</td>
+        <td>1</td><td>0</td><td>0</td><td>0</td><td>0</td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+<h2>Average Annual Number of Earthquakes by Magnitude</h2>
+<table>
+
+  <tr>
+    <th>Magnitude</th>
+    <th>Annual Average</th>
+  </tr>
+
+  <tr>
+    <td>8 and higher</td>
+    <td>1 &sup1;</td>
+  </tr>
+  <tr>
+    <td> 7 - 7.9 </td>
+    <td> 17 &sup1; </td>
+  </tr>
+  <tr>
+    <td> 6 - 6.9 </td>
+    <td> 134 &sup2; </td>
+  </tr>
+  <tr>
+    <td> 5 - 5.9 </td>
+    <td> 1319 &sup2;</td>
+  </tr>
+  <tr>
+    <td> 4 - 4.9 </td>
+    <td> 13,000<br /><small>(estimated)</small></td>
+  </tr>
+  <tr>
+    <td> 3 - 3.9 </td>
+    <td> 130,000<br /><small>(estimated)</small></td>
+  </tr>
+  <tr>
+    <td> 2 - 2.9 </td>
+    <td> 1,300,000<br /><small>(estimated)</small></td>
+  </tr>
+
+  <tr>
+    <td colspan="3">
+      &sup1; Based on observations since 1900.
+      <br />
+      &sup2; Based on observations since 1990.
+    </td>
+  </tr>
+
+</table>
