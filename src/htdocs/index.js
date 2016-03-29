ec33b8aebdf221514f357280015936e19d30b864
lisa-usgs <lisa@usgs.gov>
2016-03-29 11:25:51 -0600

    initial commit
    Former-commit-id: f16d352833e7f3bf69cf18f2d69bd7812171d731

---
+++ src/htdocs/index.js
@@ -0,0 +1,14 @@
+/* global require */
+'use strict';
+
+(function () {
+  var EqList = require('listwidget/EqList');
+
+  EqList({
+    container: document.getElementById('significant-earthquakes')
+  });
+  EqList({
+    container: document.getElementById('m2pastDay-earthquakes'),
+    feed: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojsonp'
+  });
+})();
