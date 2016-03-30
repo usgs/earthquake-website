b9588ca7bd76b68112d490b42bfcb2e552751339
Jeremy Fee <jmfee@usgs.gov>
2016-03-30 15:49:46 -0600

    Add offsiteHost proxying for app content
    Former-commit-id: 1a4e0a8a14a5c0273ec5cdc475844914d7e8a9d3

--- gruntconfig/config.js
+++ gruntconfig/config.js
@@ -1,9 +1,26 @@
 'use strict';
 
+var fs = require('fs'),
+    ini = require('ini');
+
+
+var iniConfig = ini.parse(fs.readFileSync('./src/conf/config.ini', 'utf-8'));
+
 var BASE_PORT = 8080;
 
 var config = {
+  ini: iniConfig,
   liveReloadPort: BASE_PORT + 9,
+  offsitePaths: [
+    // apps that should proxy to config.ini's OFFSITE_HOST
+    '/fdsnws/',
+    '/earthquakes/eventpage/',
+    '/earthquakes/feed/',
+    '/earthquakes/map/',
+    '/scenarios/eventpage/',
+    '/scenarios/feed/',
+    '/scenarios/map/'
+  ],
   src: 'src',
   srcPort: BASE_PORT,
   templatePort: BASE_PORT + 8
