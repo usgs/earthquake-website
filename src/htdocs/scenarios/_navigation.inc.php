edcfe88d88943c5df43a16c33595666748cec34a
Lisa Wald <lisa@usgs.gov>
2016-03-30 22:20:32 +0000

    initial deploy of scenarios

---
+++ src/htdocs/scenarios/_navigation.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+echo navItem('/scenarios/', 'Earthquake Scenarios');
+
+?>
---
+++ src/htdocs/scenarios/related.php
@@ -0,0 +1,26 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: David Wald, wald@usgs.gov
+  // Last modified:
+  if (!isset($TEMPLATE)) {
+  $TITLE = 'Related Scenarios';
+  $NAVIGATION = true;
+  include 'template.inc.php';
+  }
+?>
+
+<ul>
+  <li><a href="/earthquakes/shakemap/global/shake/casc9.0_expanded_peak_se/">"Cascadia Rising"</a> - M9 on the Cascadia Subduction Zone (2016)</li>
+  <li><a href="/earthquakes/shakemap/global/shake/haywiredm7.05_se/">Haywired</a> - M7 on the Hayward Fault (2016)</li>
+  <li><a href="/earthquakes/shakemap/sc/shake/ardent_sentry_2015_se/">"Ardent Sentry "Southern California ShakeOut</a> - M7.8 on the Southern San Andreas Fault (2012 update)</li>
+  <li><a href="/earthquakes/shakemap/sc/shake/shakeout2_full_se/">Southern California ShakeOut</a> - M7.8 on the Southern San Andreas Fault (2008)</li>
+  <li><a href="/earthquakes/shakemap/ut/shake/shakeoutff_se/">Great Utah ShakeOut (2012)</a> - M7 on the Wasatch Fault</li>
+
+  <li><a href="/research/cus_seisvelmodel/">Computer Simulation of a Magnitude 7.7 Earthquake in the New Madrid Seismic Zone</a></li>
+  <li><a href="ceus_nle_2011/">Central &amp; Eastern US Scenarios for 2011 National Level Exercise</a></li>
+  <li><a href="movies/sanjose.php">Animation of ground motions recorded by the San Jose Dense Seismic Array from the M5.6 earthquake near Scotty's Junction, NV</a></li>
+  <li><a href="movies/seattle.php">Computer Simulation of a Magnitude 6.5 Earthquake on the Seattle Fault</a></li>
+  <li><a href="movies/nisqually.php">Simulation of the 2001 M6.8 Nisqually, Washington Earthquake</a></li>
+  <li><a href="movies/westseattle.php">Simulation of Seismic Energy Focussing in West Seattle</a></li>
+  <li><a href="movies/kingdome.php">Animation of Ground Motions Produced by the Implosion of the Kingdome</a></li>
+</ul>
