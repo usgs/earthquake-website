6c2e437e233be56d0c8970edbf4cf516642d116a
lisa-usgs <lisa@usgs.gov>
2016-04-11 15:53:26 -0600

    2016-04-11
    Former-commit-id: b563e495157c44963b088e3d519f49da05e456af

--- src/htdocs/_features.inc.php
+++ src/htdocs/_features.inc.php
@@ -1,8 +1,6 @@
 <?php
 
-include_once '..' . DIRECTORY_SEPARATOR .
-    'lib' . DIRECTORY_SEPARATOR .
-    'Features.class.php';
+include_once 'Features.class.php';
 
 date_default_timezone_set('UTC');
 
