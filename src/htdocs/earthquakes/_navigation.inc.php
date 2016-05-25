cc40d59c28e43ec89f66e20903e3215d54155e2c
lisa-usgs <lisa@usgs.gov>
2016-05-24 19:56:08 -0600

    initial commit of byregion/states files

--- src/htdocs/earthquakes/_navigation.inc.php
+++ src/htdocs/earthquakes/_navigation.inc.php
@@ -6,10 +6,10 @@
   "<section>",
 
     navItem("${section}/map/", "Latest Earthquakes") .
-    navItem("${section}/eqarchives/", "Earthquake Lists, Maps & Statistics") .
+    navItem("${section}/browse/", "Earthquake Lists, Maps & Statistics") .
     navItem("${section}/search/", "Search Earthquake Catalog") .
     navItem("${section}/feed/", "Real-time Feeds & Notifications") .
-    navItem("${section}/region.php", "Information by Region") .
+    navItem("${section}/byregion/", "Information by Region") .
     navItem("/data/comcat/", "ANSS ComCat Documentation") .
     navItem("${section}/errata.php", "Errata for Latest Earthquakes") ,
 
