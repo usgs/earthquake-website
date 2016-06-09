66b6db1488b8c5c0455cc1afe3d14578e239f012
Thompson <emthompson@usgs.gov>
2016-06-09 12:57:14 -0600

    Added more regional catalogs

--- src/htdocs/scenarios/catalog/nnlegacy/index.php
+++ src/htdocs/scenarios/catalog/nnlegacy/index.php
@@ -6,7 +6,7 @@ include '../_catalog.inc.php';
 <h2 id="description">Description</h2>
 
 <p>
-  This catalog is a place holder for early scenarios created for Northern California.
+  This catalog is a place holder for early scenarios created for Nevada.
   Legacy catalogs such as this do not contain the level of detailed documentation
   required for newly created scenarios. These scenarios are maintained
   for archival purposes. 
@@ -16,7 +16,7 @@ include '../_catalog.inc.php';
 
 <h3 id="contributors">Contributors</h3>
 <ul>
-  <li><a href="../../contributor/nc/">NC (Scenario Contributor)</a></li>
+  <li><a href="../../contributor/nn/">NN (Scenario Contributor)</a></li>
 </ul>
 
 
