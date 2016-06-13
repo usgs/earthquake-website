c061069c3309aa6debe574ff7e56b4aefde976dd
lisa-usgs <lisa@usgs.gov>
2016-06-13 10:23:02 -0600

    added world responsive imagemap to by region page
    Former-commit-id: 930359b16839ef6bffd2dd3db85c054827317eea

--- src/htdocs/earthquakes/byregion/index.js
+++ src/htdocs/earthquakes/byregion/index.js
@@ -3,6 +3,13 @@
 var SvgImageMap = require('svgimagemap/SvgImageMap');
 
 SvgImageMap({
+  el: document.querySelector('.worldmap'),
+  imageAlt: 'World map',
+  imageUrl: 'images/worldseis.gif',
+  mapUrl: 'images/worldmap_imagemap.html'
+});
+
+SvgImageMap({
   el: document.querySelector('.usmap'),
   imageAlt: 'U.S. map',
   imageUrl: 'images/usmap-states.png',
