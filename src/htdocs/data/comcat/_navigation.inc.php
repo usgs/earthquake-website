cd731dd51be4f7ced3b0c1e249168df797a8b102
lisa-usgs <lisa@usgs.gov>
2016-06-13 09:10:26 -0600

    implemented left nav standard on ComCat Doc pages
    Former-commit-id: b760aacd9ac7f65fde227a76cc4a605c039e4f50

--- src/htdocs/data/comcat/_navigation.inc.php
+++ src/htdocs/data/comcat/_navigation.inc.php
@@ -1,5 +1,16 @@
 <?php
+$section = "/data/comcat";
 
-  echo navItem("/data/comcat/", "ComCat Documentation");
+echo "<a href='/data' class='up-one-level'>Data</a>";
+
+echo navGroup(("ComCat Documentation") ,
+
+  navItem("${section}/index.php#1", "Data Types, Definitions, Formats, Availability") .
+  navItem("${section}/index.php#2", "Data Access") .
+  navItem("${section}/index.php#3", "Contribute and Receive Data-PDL") .
+  navItem("${section}/index.php#4", "Sources of Data - Catalogs and Real-time") .
+  navItem("${section}/index.php#5", "Event Association") .
+  navItem("${section}/index.php#6", "References")
+);
 
 ?>
