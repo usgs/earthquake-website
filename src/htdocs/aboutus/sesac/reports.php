32225b7a69e72cd8093cac92283c249b230500f2
lisa-usgs <lisa@usgs.gov>
2016-06-23 15:51:31 -0600

    initial About Us section migration

---
+++ src/htdocs/aboutus/sesac/reports.php
@@ -0,0 +1,48 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: Bill Leith, leith@usgs.gov
+  // Last modified: 06/23/2016, Lisa Wald
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'SESAC Reports';
+      $NAVIGATION = true;
+      include 'template.inc.php';
+    }
+?>
+
+<ul>
+<li>
+  <a href="docs/sesac_15report.pdf">2015 Annual Report</a> (PDF) | <a href="docs/SESAC_SpecialLetter_Sept23.pdf">Special Supplement</a><br/>
+</li>
+<li>
+  <a href="docs/sesac_14report.pdf">2014 Annual Report</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_13report.pdf">2013 Annual Report</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_12report.pdf">2012 Annual Report</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_10_11report.pdf">2010&ndash;2011 Annual Report</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_08report.pdf">2008&ndash;2009 Annual Report</a> (PDF) | <a href="docs/sesac_08cover.pdf">Cover Letter</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_07report.pdf">2007 Annual Report</a> (PDF) | <a href="docs/sesac_07cover.pdf">Cover Letter</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_06report.pdf">2006 Annual Report</a> (PDF) | <a href="docs/sesac_06cover.pdf">Cover Letter</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_05report.pdf">2005 Annual Report</a> (PDF) | <a href="docs/sesac_05cover.pdf">Cover Letter</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_04report.pdf">2004 Annual Report</a> (PDF) | <a href="/aboutus/sesac/docs/sesac_04cover.pdf">Cover Letter</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_03report.pdf">2003 Annual Report</a> (PDF)<br />
+</li>
+<li>
+  <a href="docs/sesac_02report.pdf">2002 Annual Report</a> (PDF)<br />
+</li></ul>
