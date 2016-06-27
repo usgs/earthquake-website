804f579f9634c7650950ec3fbbf34e111d50f419
lisa-usgs <lisa@usgs.gov>
2016-06-27 12:09:26 -0600

    added office imagemap
    Former-commit-id: a74a1ccfc5be39fb045df9823e558ab5cffd9fbc

--- src/htdocs/contactus/index.php
+++ src/htdocs/contactus/index.php
@@ -4,9 +4,14 @@
 // Last modified: 11/24/2015
 if (!isset($TEMPLATE)) {
   $TITLE = 'Contact Us';
+  $HEAD = '<link rel="stylesheet" href="/lib/hazdev-svgimagemap.css"></script>' .
+  $FOOT = '<script src="/lib/hazdev-svgimagemap.js"></script>' .
+      '<script src="index.js"></script>';
   include 'template.inc.php';
 }
 ?>
+
+<p>Click on office location on map or select location from list below.</p>
 
 <div class="row">
   <div class="column one-of-two">
@@ -26,20 +31,16 @@ if (!isset($TEMPLATE)) {
     	<li><a href="pasadena/">Pasadena Field Office, Pasadena, CA</a></li>
     	<li><a href="albuquerque/">Albuquerque Seismological Laboratory</a></li>
     </ul>
-  </div>
-
-  <div class="column one-of-two">
     <h3>Questions About Earthquakes</h3>
     <ul>
       <li><a href="media.php">Contacts and Information for the Media</a></li>
       <li><a href="mailto:sis_eq_questions@usgs.gov">Earthquake Questions and Website Feedback</a></li>
     </ul>
+  </div>
 
-    <div class="column one-of-two">
-      <h3>Web Team</h3>
-      <ul>
-        <li><a href="webteam.php">Web Team</a></li>
-      </ul>
+  <div class="column one-of-two">
+
+    <div class="EQoffices"></div>
 
     <h3>Looking for Something Else?</h3>
     <ul>
@@ -47,5 +48,10 @@ if (!isset($TEMPLATE)) {
       <li><a href="http://www.usgs.gov/ohr/">USGS Employment Opportunities</a></li>
       <li><a href="http://www.usgs.gov/">USGS - Everything!</a></li>
     </ul>
+    <h3>Web Team</h3>
+    <ul>
+      <li><a href="webteam.php">Web Team</a></li>
+    </ul>
+
   </div>
 </div>
