5ce9839a8863bc54615853f7ffecd1ec72bab476
lisa-usgs <lisa@usgs.gov>
2016-06-30 15:56:58 -0600

    adding files and subdirectories under Data and Products

---
+++ src/htdocs/data/scr_catalog.php
@@ -0,0 +1,58 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: ,
+  // Last modified:
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'Earthquake Catalog for Stable Continental Regions';
+      $NAVIGATION = true;
+      include 'template.inc.php';
+    }
+?>
+
+<h2>Intraplate Earthquakes (495-2002)</h2>
+
+<p>
+  We present an updated global earthquake catalog for Stable Continental Regions (SCRs). SCRs are defined as regions of continental crust that have not experienced any major tectonism, magmatism, basement metamorphism or anorogenic intrusion since the early Creataceous, and no rifting or major extension or transtension since the Paleogene [<em>EPRI</em>, 1994].
+</p>
+<p>
+  The last global study on intraplate seismicity was the EPRI [1994] study, which includes events up until 1990. Thus, earthquake activity over the last 13 years could not have been taken into account. This is a relatively long period, considering that worldwide high quality seismic monitoring only began in 1964, and it is only from this year that most large events have well constrained locations, magnitudes, and focal mechanisms.
+</p>
+<p>
+  The current updated catalog contains crustal (depths &#8804; 45 km) events with moment magnitudes <strong>M </strong> &#8805; 4.5 within SCRs from the historic era up until November 2003. For each event, the epicenter, depth, Moment Magnitude, other reported magnitudes, and focal mechanism parameters (if available) are listed. The database contains 1373 events, which is an increase of approximately 58% with respect to the database developed for the EPRI [1994] study.
+</p>
+<p>
+  Source catalogs for the database are the Catalog of shallow intraplate earthquakes [<em>Triep and Sykes</em>, 1996], the EPRI earthquake datasheets [<em>EPRI,</em> 1994], the Centennial catalog [<em>Engdahl and Villase&ntilde;or,</em> 2002], the Bulletin of the International Seismological Center, the Preliminary Determination of Earthquakes [<em>United States Geological Survey National Earthquake Information Center</em>], and the Centroid Moment Tensor Catalog [<em>Harvard Seismology</em>].
+</p>
+<p>
+  Our boundaries for the SCRs are polygons directly extracted from the original EPRI [1994] maps. In an attempt to remove events that are in fact related to plate boundary deformation, events within 200 km of plate boundaries, as well as events within 40 km of the SCR-ACR (Active Continental Regions; regions of continental crust that do not satisfy the definition above) boundaries were removed from the database.
+</p>
+<p>
+  A uniform magnitude scale, the Moment Magnitude, was applied throughout the catalog. The Moment Magnitude is defined as <strong>M </strong> = 2/3 log M<sub>o</sub><sub></sub> - 10.7 [<em>Hanks and Kanamori,</em> 1979], where M<sub>o</sub> is Seismic Moment (in dyne cm). For those events where M<sub>o</sub> was not available, it was determined from regressions of M<sub>o</sub> on other magnitude scales (m<sub>b</sub>, M<sub>s</sub>), which had already been developed for the EPRI study [1994].
+</p>
+<p>
+  All events in the earthquake catalog were put into one of five categories: 1) interior rifts/ taphrogens, 2) rifted continental margins, 3) non-rifted crust, 4) possible interior rifts, and 5) possible rifted margins. The distribution of earthquakes within SCRs over these five categories is the subject of the accompanying paper [<em>Schulte and Mooney</em>, 2004]. In that paper the catalog itself is also discussed in more detail.
+</p>
+<ul>
+  <li><a href="scr_catalog.txt">Catalog.txt</a></li>
+  <li><a href="scr_fielddescription.txt">Field_description.txt</a> </li>
+</ul>
+
+<h3>References</h3>
+<ul class='referencelist'>
+
+  <li>Bakun, W. H. and M. G. Hopper, Magnitudes and locations of the 1811-1812 New Madrid, Missouri, and the 1886 Charleston, South Carolina, earthquakes, submitted to <em>Bull. Seismol. Soc. Am </em>., 2003. </li>
+  <li>Bent, A. L., The 1933 M s = 7.3 Baffin Bay earthquake: strike slip faulting along the northeastern Canadian passive margin,<em> Geophys. J. Int </em>. 150, 724-736, 2002. </li>
+  <li>Electric Power Research Institute (EPRI), Johnston, A. C., Coppersmith, K. J., Kanter, L.R. and Cornell, C. A., <em>The earthquakes of stable continental regions,</em> vol. 1-5, edited by J.F. Schneider, Palo Alto, California, 1994. </li>
+  <li>Engdahl, E. R. and A. Villase&ntilde;or, Global seismicity: 1900-1999, in Lee, W. H. K. et al. (editors), International handbook of earthquake engineering and seismology, <em>International Geophysics series,</em> vol. 81A, pp. 665- 690, 2002. </li>
+  <li>Hanks, T. H. and Kanamori, H., A moment magnitude scale, <em>J. Geoph. Res </em>. vol. 84 (B5), 2348- 2350, 1979. </li>
+  <li>Harvard Seismology, <a href="http://www.globalcmt.org/">Centroid Moment Tensor project</a> </li>
+  <li>International Seismological Centre, On-line Bulletin, <a href="http://www.isc.ac.uk/iscbulletin/search/bulletin/">http://www.isc.ac.uk/search/</a>, Internat. Seis. Cent., Thatcham, United Kingdom, 2001. </li>
+<li>Johnston, A. C. Seismic moment assessment of earthquakes in stable continental regions-I. Instrumental seismicity, <em>Geoph. J. Int </em>. 124, 381 - 414, 1996. </li>
+  <li>Schulte, S. M. and W. D. Mooney, Stable Continental Region earthquakes and ancient rifts, submitted to <em>Geoph.. J. Int.,</em> 2004. </li>
+  <li>Triep, E. G. and L. R. Sykes, Catalog of shallow intracontinental earthquakes, <em><a href="http://www.ldeo.columbia.edu/seismology/triep/intra.expl.html">http://www.ldeo.columbia.edu/seismology/triep/intra.expl.html</a>, </em>1996. </li>
+  <li>United States Geological Survey National Earthquake Information Center, Preliminary Determination of Earthquakes, <a href="/earthquakes/eqarchives/epic/">online search</a> </li>
+  <li>Zoback, M. L., First- and second-order patterns of stress in the lithosphere: the world stress map project, <em>J. Geophys. Res. </em> vol 97 (B8), 11,703-11,728, 1992 </li>
+</ul>
+
+<h3>Contact</h3>
+<p>Saskia M. Schulte and Walter D. Mooney</li>
