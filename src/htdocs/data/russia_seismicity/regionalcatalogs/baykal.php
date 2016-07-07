916a2bb7a080b08b6a017a6de65b74293cff840e
lisa-usgs <lisa@usgs.gov>
2016-07-07 16:08:56 -0600

    fixing mistake from previous merge

--- src/htdocs/data/russia_seismicity/regionalcatalogs/baykal.php
+++ src/htdocs/data/russia_seismicity/regionalcatalogs/baykal.php
@@ -1,12 +1,7 @@
 <?php
   // Author: Lisa Wald
-<<<<<<< HEAD
   // Contact: Bill Leith, leith@usgs.gov
   // Last modified: 07/07/2016, Lisa Wald
-=======
-  // Contact: Walter Mooney, mooney@usgs.gov
-  // Last modified:
->>>>>>> 24b1787612207f26775998792e2a258cc0a4d9ec
     if (!isset($TEMPLATE)) {
       $TITLE = 'Regional Catalogs';
       $NAVIGATION = true;
