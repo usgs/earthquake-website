97ac5eb408b35ef26bc3744b011ef4c10a933fd8
lisa-usgs <lisa@usgs.gov>
2016-07-14 11:40:45 -0600

    retrieving work from previous research branch before corrupted
    Former-commit-id: 2324a00aaf24bc7477beca0cc74d8ae95f1629ad

---
+++ src/htdocs/research/fy16-nepal2015/index.php
@@ -0,0 +1,159 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: Sue Hough, hough@usgs.gov
+  // Last modified: 07/12/2016, Lisa Wald
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'The M7.8 Nepal Earthquake, 2015 &ndash; A Small Push to Mt. Everest';
+      $NAVIGATION = true;
+      $HEAD = '
+        <link rel="stylesheet" href="index.css"/>
+      ';
+      include 'template.inc.php';
+    }
+?>
+
+<figure class="right bricks">
+	<img src="images/bricks.jpg" alt="collapsed brick homes"/></a>
+	<figcaption>
+		Locals sifting through the rubble of their homes, salvaging construction materials that can be reused and looking for valuables. (Photo by Kashish Das Shrestha for USAID)
+	</figcaption>
+</figure>
+
+<p>
+  On Saturday April 25 at 11:56am local time, an M7.8 earthquake began 82 km (51 mi) NW of Nepal’s capital of Kathmandu.  The event was followed by many aftershocks, the largest being an M7.3 on May 12, 17 days after the mainshock.  The toll included about 9000 fatalities, 23,000 injuries, more than 500,000 destroyed houses, and about 270,000 damaged houses.
+</p>
+<p>
+  In the days and months following the 2015 earthquake, scientists spread out over the area and collected data that will help them learn more about earthquake hazards in the region, while also being sensitive to the humanitarian crisis and assisting with immediate hazard assessments.  They’ve already learned a great deal about the earthquake, the shaking, and the resulting damage, but their research is ongoing.  The following is a summary of what happened, the data that scientists have been able to gather, and how the data has helped them to figure out the underlying science of this event.
+</p>
+
+<figure class="left">
+  	<img src="images/xsect.gif" alt="cross-section of subsurface geologic structure"/></a>
+	<figcaption>
+    Generalized cross section showing the approximate locations of slip during the mainshock and largest aftershock ruptures on the Main Himalayan Thrust, and approximate aftershock locations of both events. (MFT = Main Frontal Thrust, MBT = Main Boundary Thrust, MCT = Main Central Thrust).
+	</figcaption>
+</figure>
+
+<h2>In the Shadow of Mountains</h2>
+<p>
+  The people of Nepal and the surrounding areas have been living with earthquakes for many generations.  The Himalayan mountain range was formed by millions of years of earthquakes as the India and Eurasia tectonic plates have continued to push against one another at a relative rate of 40-50 mm/yr (1.6-2 in/yr).  A large amount of that motion (18 mm/yr) is driving the uplift of the Himalayan Mountains.  The boundary region between these two plates has a history of large and great earthquakes, making it one of the most seismically hazardous regions in the world.
+</p>
+<p>
+  Three M6+ earthquakes have occurred within 250 km (155 mi) of the 2015 earthquake within the last 100 years. Most recently, an M6.9 in 1988 that caused around 1500 fatalities. In 1943 an M7.5-8.0 severely damaged Kathmandu and caused around 10,600 fatalities, and a previous earthquake of approximately M7.5 occurred in 1833.  Despite this long history of earthquakes and an elevated seismic hazard, the lack of resources in the rural areas has prevented many Nepalese from using modern construction methods and materials that are designed to withstand earthquake shaking.  More than 98% of the buildings in Nepal are built with low-quality materials by people not trained in proper construction practices.
+</p>
+
+<figure class="left map">
+	<img src="images/stations.gif" alt="map of world with red triangles"/></a>
+	<figcaption>
+    World map with Nepal in the center (black star) showing the seismic station locations and distances from the earthquake epicenter.
+	</figcaption>
+</figure>
+
+<h2>Collecting Data and Carrots</h2>
+<p>
+  The global seismographic network is sparse in Nepal, and data from some of the closest sources are not publically available.  Acquiring the quantity and quality of data required to accurately locate the epicenter and determine the shaking throughout the region, much less answer the many scientific questions posed by this earthquake, took more time and effort than usual.  The seismic station closest to the epicenter with real-time data was about 650 km (404 mi) away in Lhasa, Tibet.  The next closest stations were a NetQuakes instrument at the U.S. Embassy and three Quake-Catcher instruments in the Kathmandu area.
+</p>
+<p>
+  The USGS Did You Feel It? (DYFI) website often helps scientists fill in data gaps where seismic instrumentation is sparse in some areas of the U.S.  Responses to the online DYFI questionnaire from citizens is converted into shaking intensity values that supplement the instrumental ShakeMap data in order to provide a map of the distribution of shaking from an earthquake.  However, much of Nepal is remote and not connected to the Internet, so real-time shaking intensity data were sparse, and virtually no reports were collected from the mountainous areas hardest hit by the shaking.  Instead, scientists gathered data from the news, social media, and converted European Macroseismic Scale (EMS) intensities, totaling 3831 data points at locations throughout the Kathmandu Valley.
+</p>
+<p>
+  Scientists used InSAR imagery to look for surface rupture and landslides, and it was also used to direct field teams to areas that appeared to be most affected by the shaking. A multi-organizational reconnaissance team called GEER, Geotechnical Extreme Event Reconnaissance, sent two teams out after the earthquake to look for surface fault ruptures, landslides, liquefaction, and other surface disturbances, as well as damage to structures and infrastructure, such as bridges and pipelines. Several USGS scientists worked with these teams to collect data.
+</p>
+<p>
+  On this mission, scientists discovered a most unusual surface disturbance near the village of Mulpani. The GEER team observed many of the villagers, young and old eating carrots, and they saw a lot of carrots lying on the ground in seemingly random locations.  The villagers informed scientists that when the earthquake occurred, many of the carrots that were growing in the fields were brought out of the ground as water flowed up and flooded the fields (this is called liquefaction, where the ground behaves like a liquid).  Since the crops cannot be replanted once they are pulled up (or pushed up in this case), the villagers were eating as many carrots as they could before they started rotting.  Thus, the first known earthquake-related incident of “root vegetables ejected out of the ground” was recorded.
+</p>
+
+<figure class="right bricks">
+	<img src="images/aftershocks.jpg" alt="terrain map of Nepal region with colored circles"/></a>
+	<figcaption>
+    Regional map of the Nepal area showing the mainshock (star), and the aftershocks (circles) from April 25 through June 25, 2015.  The size of the circle indicates the earthquake magnitude, and the color represents the time after the mainshock.
+	</figcaption>
+</figure>
+
+<h2>What We Know So Far</h2>
+<p>
+  The epicenter of the earthquake was west of the Kathmandu Valley, but the rupture propagated east towards the valley and the capital city of Kathmandu.  The earthquake originated at a relatively shallow depth of 12-15 km (7.5-9.3 mi). The <a href="http://earthquake.usgs.gov/regional/nca/1868virtualtour/bayarea.php">InSar</a> data indicated that there was no surface rupture, either along a new trace or along known fault lines from previous earthquakes.  Scientists subsequently confirmed this by conducting a field survey in the Kathmandu Valley.  Field data and seismic data indicate that the 2015 fault rupture broke adjacent to the 1934 rupture, and it appears that a larger rupture occurred historically adjacent to the other end too.  Scientists observed impressive ground fractures observed in Kausaltar, but they were the result of local site conditions and shaking, not tectonic displacement.
+</p>
+<p>
+  Shaking from the earthquake was felt in much of central Nepal, north India, west Bangladesh, and parts of Bhutan.  The furthest reported shaking was in Kahuta, Pakistan. The intensity data show the shaking intensities in Kathmandu Valley were 6-7 on the 1998 European Macroseismic Scale (EMS-98). The distribution of shaking exceeded an intensity level of 8 only a few times.
+</p>
+<p>
+  Scientists discovered a few liquefaction sites (and one “carrot site”), but the landslides were, by far, the most pervasive surface disturbance.  Many landslides were triggered by strong shaking on the steep mountain slopes, and most were falls and slides of rock and soil rather than large debris avalanches (although there were a few of these). Landslides blocked roads, dammed rivers and damaged villages, making them a significant source of damage, second only to structural collapses. USGS scientists estimated that they witnessed tens of thousands of landslides during their field reconnaissance.
+</p>
+
+<figure class="left photos">
+	<img src="images/Langtang-photo.jpg" alt="two photos of a landslide"/></a>
+	<figcaption>
+    The Langtang debris avalanche. (a) Oblique northwest view of deposit with cliff from which the debris became airborne. Buildings in foreground were pushed over by the ensuing airblast. (b) Aerial view of deposit showing location of the Langtang River tunnel through ice and debris (blue arrows).
+	</figcaption>
+</figure>
+
+<p>
+  The debris avalanche that destroyed the village of Langtang was the largest and most destructive landslide.  During the shaking, an avalanche of snow and ice initiated high on the slopes of a 7,200-m-high peak and subsequently gathered debris as it moved downhill.  It then launched off a 500-m-tall cliff (1640 ft).  What wasn’t buried by the rapidly-moving debris was flattened by the accompanying air blast.  The air blast destroyed many structures and flattened trees within about 1 km (0.6 mi) in each direction up and down the valley.
+</p>
+<p>
+  Some of the landslides did not fail immediately with shaking but rather were progressive in nature, such as the May 24 rockslide that buried the village of Baisari on the Kali Gandaki River.  Cracks were first observed in the cliff after the mainshock, and they widened during the largest aftershock on May 12. Basairi was evacuated 10 days later when rocks began falling from the cliff.  Two days after the evacuation the slope beneath the cliff slid, damming the river and burying the entire village under about 30m (98 ft) of debris.  A 2.7-km-long (1.7-mi-long) lake formed behind the dammed river, and villages downstream were evacuated just before the water overtopped the dam and flooded the downstream area.
+</p>
+
+<figure class="right photo2">
+	<img src="images/Baisari-slide.jpg" alt="two photos of a landslide"/></a>
+	<figcaption>
+    Rock slide along the Kali Gandaki River on 24 May 2015 that buried the village of Baisari and blocked the flow of the river for 16 hours nearly one month following the main earthquake shock.
+	</figcaption>
+</figure>
+
+<p>
+  Of course, everyone wanted to know if Mt. Everest, the tallest mountain on Earth, had become even taller.  Data from a satellite-mapping receiver that Chinese scientists had installed in 2005 revealed that Mt. Everest was not taller… or shorter.  Instead, the motion on the fault had shifted the entire mountain 3 cm (1.2 in) to the southwest, leaving the height of the mountain still at 8848m (29,029ft).  The background motion of the mountain is about 4 cm (1.6 in) toward the northeast and 0.3cm (0.1 in) up (taller) each year.
+</p>
+
+<figure class="left tower">
+	<img src="images/tower.jpg" alt="two photos, tower standing, tower collapsed"/></a>
+	<figcaption>
+    Photos of the historic Dharahara Tower in Nepal before and after the earthquake. (Photo by Narendra Shrestha, EPA)
+	</figcaption>
+</figure>
+
+<h2>The Nature of Kathmandu Valley</h2>
+
+<p>
+  Some scientists think that the shaking and resulting damage was less than expected for an earthquake of M7.8 in this area, and they are seeking an explanation. Analysis of the seismograms and the geology of the area may have provided clues, although confirmation will take further research.
+</p>
+<p>
+  The recording of shaking on instruments in the Kathmandu Valley suggested that something about the valley had dampened the shaking. The Kathmandu Valley is a bowl of ancient lakebed sediments as thick as 500 m (1540 ft). In most cases, soft sedimentary valleys increase the shaking frequency that is damaging to most buildings, but this time the opposite may have happened. Scientists are describing this as a non-linear effect, meaning that the sediments in the valley lost their cohesiveness during the shaking and didn’t transmit the energy as efficiently as they would with their normal cohesiveness. In other words, the normally soft-rock-like valley may have become even softer temporarily, like a viscous fluid, and fluids cannot transmit certain types of seismic waves. Further research will be conducted to see if this theory holds up.
+</p>
+<p>
+  If there was indeed non-linear behavior in the valley, there are implications for the analysis of historical earthquakes in this area. Namely, using intensity data to determine the magnitudes of earthquakes for which we have no instrumental data may have underestimated the magnitudes. A second look at the macroseismic data we have for older events in the Himalayan area may be in order.
+</p>
+
+<h2 class="clear">Better Construction Can Make a Difference</h2>
+<p>
+  In Nepal, the fatalities and injuries were mostly caused by poor construction. Homes are made of rock, brick, or concrete that is either unreinforced or poorly reinforced, and although there are building codes, there are not enough resources to follow them, or to enforce them. Newer construction in Kathmandu fared better with less damage. This reminds us again that construction practices and enforcement of building codes can make a huge difference between a structure that is still standing after an earthquake, and one that has collapsed.
+</p>
+<p>
+  This is a partial tally of damage and fatalities:
+  <ul>
+    <li>500,717 houses destroyed in Nepal.</li>
+    <li>269,190 houses damaged in Nepal.</li>
+		<li>At least 200 people killed in the Langtang landslide.</li>
+    <li>180 people killed from the collapse of Nepal’s historic Dharahara Tower.</li>
+    <li>20 people killed and 120 injured from an avalanche at the Mount Everest Base Camp.</li>
+    <li>At least 78 people killed and 560 injured in India.</li>
+    <li>At least 25 people killed and 383 injured in China.</li>
+    <li>20 major hydroelectric power plants and several micro hydropower plants damaged.</li>
+  </ul>
+</p>
+
+<p class="sm">-written by Lisa Wald, U.S. Geological Survey</p>
+
+<h2>Additional Resources</h2>
+<ul>
+  <li><a href="http://earthquake.usgs.gov/earthquakes/eventpage/us20002926">Nepal M7.8 Event page</a></li>
+	<li><a href="http://earthquake.usgs.gov/earthquakes/eventpage/us20002ejl">M7.3 Aftershock event page</a></li>
+	<li><a href="http://www.usgs.gov/newsroom/article.asp?ID=4279">USGS News Release - Landslides Triggered by Nepal Earthquakes</a></li>
+  <li><a href="http://srl.geoscienceworld.org/content/86/6.toc">Seismological Research Letters Issue with Nepal Focus Section</a></li>
+</ul>
+
+<h2>Scientific Staff</h2>
+<ul>
+	<li>Earthquake Science Center, Menlo Park, California</li>
+	<li>Geologic Hazards Science Center, Golden, Colorado.</li>
+</ul>
