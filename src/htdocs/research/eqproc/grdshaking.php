c1d334165bae85f82154be0214fe6821bfaab926
lisa-usgs <lisa@usgs.gov>
2016-08-16 11:27:52 -0600

    changed format of topic pages and projects under eqproc

---
+++ src/htdocs/research/eqproc/grdshaking.php
@@ -0,0 +1,36 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: Jeanne Hardebeck,
+  // Last modified: Lisa Wald, 2016/08/10
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'Earthquake Processes and Effects';
+      $NAVIGATION = true;
+      $HEAD = '
+        <link rel="stylesheet" href="index.css"/>
+      ';
+      include 'template.inc.php';
+    }
+?>
+
+<h2>Ground Shaking</h2>
+
+<figure class='left w300'>
+  <img src="images/effects.jpg" alt="image of expected shaking amplification in LA Basin">
+  <figcaption>
+    Two important local geologic factors that affect the level of shaking experienced in earthquakes are (1) the softness of the surface rocks and (2) the thickness of surface sediments. This image of the Los Angeles region combines this information to predict the total amplification expected in future earthquakes from local geologic conditions or site effects.
+  </figcaption>
+</figure>
+
+<p>
+  The overall objective of this research is to improve the understanding of the damaging ground motions produced in earthquakes in order to develop better methods for seismic hazard assessment and mitigation in urban areas. Past earthquakes have shown that the amplification of motions due to surface-to-bedrock geology, 3D crustal structure, and topography have a major influence on seismic damage and loss in urban areas. Also of significant importance are the details of the rupture process on the fault, and the way a built structure is engineered.
+</p>
+<p>
+	As the waves propagate they are affected by the earth structure, such as changes in elastic properties resulting in effects such as constructive and destructive interference and basin amplification. Near the ground surface, strong shaking can result in nonlinear soil behavior or raise pore fluid pressure causing liquefaction. Likewise, the geometry of a man-made structure, the construction materials, the type of ground, and its anchorage in the ground affect its vulnerability to damage during the shaking. This research aims to understand each of these processes and to work with the seismic engineering community to bring the best estimates of strong ground shaking to engineering practice.
+</p>
+
+<h3>Research includes:</h3>
+<ul>
+	<li>investigation of the ground beneath high-impact areas to understand the shaking that would occur in a large earthquake.</li>
+	<li>investigation of the effect on seismic waves of traveling long distances through the earth’s crust, sedimentary basins, mountains, and other crustal features.</li>
+	<li>development of better imaging methods for identifying faults and crustal structure.</li>
+</ul>
