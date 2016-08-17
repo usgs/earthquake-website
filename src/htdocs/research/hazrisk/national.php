cd35c1b9bcf86e647c92de077ddd05534bf35c70
lisa-usgs <lisa@usgs.gov>
2016-08-17 15:18:34 -0600

    completed eqproc except rockphysics pubs page with database backend

---
+++ src/htdocs/research/hazrisk/national.php
@@ -0,0 +1,21 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: Mark pspell_add_to_personal,
+  // Last modified: 2016/08/08, Lisa Wald
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'Hazard and Risk Assessment';
+      $NAVIGATION = true;
+      include 'template.inc.php';
+    }
+?>
+
+<figure class='right'>
+	<img src="images/topic_hazrisk.gif" alt="">
+	<figcaption>National Seismic Hazard Map, 2014.</figcaption>
+</figure>
+
+<h2>National Hazard Maps</h2>
+
+<p>
+	Earthquakes cause an estimated annualized loss to the U.S. of several billions of dollars. To mitigate earthquake losses, it is necessary to evaluate the earthquake hazards across the country.  The seismic hazard maps address this need by integrating what scientists have learned about earthquake sources, crustal deformation, active faulting, and ground shaking. This information is translated into a form that can be used to reduce the risk from earthquakes and to improve public safety.  The resulting seismic hazard maps are improved and updated on a periodic basis by incorporating new information.  The USGS maps are the basis for seismic provisions in building codes and for risk models used in insurance rate structures. An integral part of this project is a database describing Quaternary faults and digital maps of those faults for the U.S. and its territories.
+</p>
---
+++ src/htdocs/research/hazrisk/risk.php
@@ -0,0 +1,35 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: Mark pspell_add_to_personal,
+  // Last modified: 2016/08/08, Lisa Wald
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'Hazard and Risk Assessment';
+      $NAVIGATION = true;
+      include 'template.inc.php';
+    }
+?>
+
+<h2>Risk</h2>
+
+<figure class='left'>
+	<img src="images/risk.gif" alt="US Seismic Design Tool">
+	<figcaption>The US Seismic Design Tool for engineers.</figcaption>
+</figure>
+
+<p>
+	The well known statement "Earthquakes don't kill people, buildings do" highlights the need to make our communities more earthquake resilient. The impact of earthquakes on public safety and the national economy can be reduced through improvement of the built- environment to resist earthquake effects such as ground shaking. Reduction of the economic impact on individuals and the nation can also be reduced by additional means such as earthquake insurance.
+</p>
+<p>
+	Improving the ability of the built environment to resist earthquakes requires research in at least three areas: (1) quantification of earthquake effects, such as ground shaking, into a form suitable for use by design engineers (e.g., structural and geotechnical engineers), (2) improvement of design practices, and (3) knowledge of the types of damage that occur as a consequence of earthquakes.
+</p>
+<p>
+	Objectives that work toward achieving this goal include:
+  <ul>
+  	<li>Develop state-of-the-art web tools to estimate earthquake losses. These will incorporate the latest scientific and engineering data available from the project along with existing data, such as that developed in the HAZUS Project (conducted by NIBS with funding by FEMA).</li>
+  	<li>Develop fragility/vulnerability models for specific structural systems for use by the engineering community and earthquake loss modeling community.</li>
+  	<li>Maintain close interaction with the earthquake insurance industry and earthquake loss modeling community to prepare products that can be used in the evolving area of earthquake loss estimation.</li>
+  </ul>
+</p>
+<p>
+	Furthermore, this task aims to increase the percent completion of earthquake hazard assessments for moderate to high hazard areas, by increasing the use of such assessments for mitigation.
+</p>
