9dde4c9d1bc2eccc967ab5f00d2a8f42152f41ae
lisa-usgs <lisa@usgs.gov>
2016-09-22 10:48:56 -0600

    us hazmap work

---
+++ src/htdocs/hazards/hazmaps/conterminous/1996/data.php
@@ -0,0 +1,49 @@
+<?php
+  // Author: Lisa Wald
+  // Contact:
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'United States - Lower 48 - Hazard Data - 1996';
+      $NAVIGATION = true;
+      include 'template.inc.php';
+    }
+?>
+
+<p>
+  Data files are presented in two formats, text and binary. Both formats for a given file contain the same information. The text file is white-space delimited and can easily be imported into your favorite spreadsheet application (i.e. Microsoft Excel™) for further analysis. The binary format is more useful for programatic access from custom pieces of software.
+</p>
+<p>
+  In addition to the output data files below we also make available to you the catalog data used when performing our analysis.
+</p>
+<h3>Catalog Data</a></h3>
+<ul>
+  <li>
+    <a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/data/wuscat.txt">Western U.S. Moment Catalog</a>
+  </li>
+  <li>
+    <a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/data/ceuscat.txt">Central and Eastern U.S. Bodywave Magnitude</a>
+  </li>
+</ul>
+
+<table>
+
+  Ground Motion	0.2 Second (5Hz)	10% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	0.3 Second (3.33Hz)	10% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	0.5 Second (2Hz)	10% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	Peak Ground Acceleration	10% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	0.2 Second (5Hz)	2% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	0.3 Second (3.33Hz)	2% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	1.0 Second (1Hz)	2% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	Peak Ground Acceleration	2% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	0.2 Second (5Hz)	5% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	0.3 Second (3.33Hz)	5% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	1.0 Second (1Hz)	5% in 50 Years	[ TEXT | BINARY ]
+  Ground Motion	Peak Ground Acceleration	5% in 50 Years	[ TEXT | BINARY ]
+  Hazard Curves	0.1 Second (10Hz)	Not Applicable	[ TEXT | BINARY ]
+  Hazard Curves	0.2 Second (5Hz)	Not Applicable	[ TEXT | BINARY ]
+  Hazard Curves	0.3 Second (3.33Hz)	Not Applicable	[ TEXT | BINARY ]
+  Hazard Curves	0.5 Second (2Hz)	Not Applicable	[ TEXT | BINARY ]
+  Hazard Curves	1.0 Second (1Hz)	Not Applicable	[ TEXT | BINARY ]
+  Hazard Curves	2.0 Second (0.5Hz)	Not Applicable	[ TEXT | BINARY ]
+  Hazard Curves	Peak Ground Acceleration	Not Applicable	[ TEXT | BINARY ]
+
+</table>
