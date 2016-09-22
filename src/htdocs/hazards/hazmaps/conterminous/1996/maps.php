8a918ebb157030f6061c5106aa572e4f968531b5
lisa-usgs <lisa@usgs.gov>
2016-09-22 10:48:56 -0600

    us hazmap work
    Former-commit-id: 9dde4c9d1bc2eccc967ab5f00d2a8f42152f41ae

---
+++ src/htdocs/hazards/hazmaps/conterminous/1996/maps.php
@@ -0,0 +1,260 @@
+<?php
+  // Author: Lisa Wald
+  // Contact:
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'United States - Lower 48 - Hazard Maps - 1996';
+      $NAVIGATION = true;
+      include 'template.inc.php';
+    }
+?>
+
+<div class='row'>
+  <div class='column three-of-five'>
+
+<p>
+  Each of these maps were produced assuming firm rock soil conditions at 760 m/sec.
+</p>
+
+<table>
+  <tr>
+    <th>Region</th>
+    <th>Spectral Acceleration</th>
+    <th>Return Period</th>
+    <th>Format</th>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>Peak Ground Acceleration</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>Peak Ground Acceleration</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>Peak Ground Acceleration</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>California/Nevada</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>Peak Ground Acceleration</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>Peak Ground Acceleration</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>Peak Ground Acceleration</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Central/Eastern US</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>Peak Ground Acceleration</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>Peak Ground Acceleration</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>Peak Ground Acceleration</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>0.2 second SA (5 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>0.3 second SA (3.33 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>10% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>2% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+  <tr>
+    <td>Conterminous US</td>
+    <td>1.0 second SA (1.0 Hz)</td>
+    <td>5% in 50 years</td>
+    <td>[ PNG | PDF ]</td>
+  </tr>
+</table>
+
+</div>
+<div class='column two-of-five'>
+
+<h2>Select Hazard Curves</h2>
+
+<a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/sf.pdf" title="Download full size San Francisco"
+  ><img src="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/sf.png" alt="San Francisco"/></a>
+<a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/la.pdf" title="Download full size Los Angeles"
+  ><img src="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/la.png" alt="Los Angeles"/></a>
+<a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/memphis.pdf" title="Download full size Memphis"
+  ><img src="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/memphis.png" alt="Memphis"/></a>
+<a href="curves/charleston.pdf" title="Download full size Charleston"
+  ><img src="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/charleston.png" alt="Charleston"/></a>
+<a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/seattle.pdf" title="Download full size Seattle"
+  ><img src="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/maps/curves/seattle.png" alt="Seattle"/></a>
+
+</div>
+</div>
---
+++ src/htdocs/hazards/hazmaps/conterminous/index.php
@@ -0,0 +1,173 @@
+<?php
+  // Author: Lisa Wald
+  // Contact:
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'United States - Lower 48';
+      $NAVIGATION = true;
+      include 'template.inc.php';
+    }
+?>
+
+<h2 id="2016">2016</h2>
+
+<p>
+  The U.S. Geological Survey (USGS) has produced a 1-year seismic hazard forecast for 2016 for the Central and Eastern United States (CEUS) that includes contributions from both induced and natural earthquakes. The model assumes that earthquake rates calculated from several different time windows will remain relatively stationary and can be used to forecast earthquake hazard and damage intensity for the year 2016. This assessment is the first step in developing an operational earthquake forecast for the CEUS, and the analysis could be revised with updated seismicity and model parameters. Consensus input models consider alternative earthquake catalog durations, smoothing parameters, maximum magnitudes, and ground motion estimates, and represent uncertainties in earthquake occurrence and diversity of opinion in the science community.
+</p>
+<p>
+  Ground shaking seismic hazard for 1-percent probability of exceedance in 1 year reaches 0.6 g (as a fraction of standard gravity [g]) in northern Oklahoma and southern Kansas, and about 0.2 g in the Raton Basin of Colorado and New Mexico, in central Arkansas, and in north-central Texas near Dallas. Near some areas of active induced earthquakes, hazard is higher than in the 2014 USGS National Seismic Hazard Model (NHSM) by more than a factor of 3; the 2014 NHSM did not consider induced earthquakes. In some areas, previously observed induced earthquakes have stopped, so the seismic hazard reverts back to the 2014 NSHM. Increased seismic activity, whether defined as induced or natural, produces high hazard. Conversion of ground shaking to seismic intensity indicates that some places in Oklahoma, Kansas, Colorado, New Mexico, Texas, and Arkansas may experience damage if the induced seismicity continues unabated. The chance of having Modified Mercalli Intensity (MMI) VI or greater (damaging earthquake shaking) is 5–12 percent per year in north-central Oklahoma and southern Kansas, similar to the chance of damage caused by natural earthquakes at sites in parts of California.
+</p>
+<ul>
+  <li>
+    <a href="http://pubs.usgs.gov/of/2015/1070/">Incorporating Induced Seismicity in the 2014 United States National Seismic Hazard Model—Results of 2014 Workshop and Sensitivity Studies</a> (USGS OFR 2015-1070)
+  </li>
+  <li>
+    <a href="/hazards/induced/">2016 One-Year Seismic Hazard Model from Induced and Natural Earthquakes</a>
+  </li>
+</ul>
+
+<h2 id="2014">2014</h2>
+
+<p>The 2014 U.S. Geological Survey (USGS) National Seismic Hazard Maps display earthquake ground motions for various probability levels across the United States and are applied in seismic provisions of building codes, insurance rate structures, risk assessments, and other public policy. The updated maps represent an assessment of the best available science in earthquake hazards and incorporate new findings on earthquake ground shaking, faults, seismicity, and geodesy. The USGS National Seismic Hazard Mapping Project developed these maps by incorporating information on potential earthquakes and associated ground shaking obtained from interaction in science and engineering workshops involving hundreds of participants, review by several science organizations and State surveys, and advice from expert panels and a Steering Committee. The new probabilistic hazard maps represent an update of the seismic hazard maps; previous versions were developed by Petersen and others (2008) and Frankel and others (2002), using the methodology developed Frankel and others (1996). Algermissen and Perkins (1976) published the first probabilistic seismic hazard map of the United States which was updated in Algermissen and others (1990).</p>
+
+<p>The National Seismic Hazard Maps are derived from seismic hazard curves calculated on a grid of sites across the United States that describe the annual frequency of exceeding a set of ground motions. Data and maps from the 2014 U.S. Geological Survey National Seismic Hazard Mapping Project are available for download below. Maps for available periods (0.2 s, 1 s, PGA) and specified annual frequencies of exceedance can be calculated from the hazard curves. Figures depict probabilistic ground motions with a 2 percent probability of exceedance. Spectral accelerations are calculated for 5 percent damped linear elastic oscillators. All ground motions are calculated for site conditions with Vs30=760 m/s, corresponding to NEHRP B/C site class boundary.</p>
+
+<ul>
+  <li><a href="2014/images/HazardMap2014_lg.jpg"/>Simplified 2014 Hazard Map (PGA, 2% in 50 years)</a></li>
+  <li>Maps: Conterminous US
+    <ul>
+          <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/2014/2014pga2pct.pdf">PGA 2% in 50 yrs</a>(1.3MB PDF)</li>
+          <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/2014/2014pga10pct.pdf">PGA 10% in 50 yrs</a>(1.1MB PDF)</li>
+          <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/2014/2014_5Hz2pct.pdf">5 Hz 2% in 50 yrs</a>(1.3MB PDF)</li>
+          <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/2014/2014_5Hz10pct.pdf">5 Hz 10% in 50 yrs</a>(1.0MB PDF)</li>
+          <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/2014/2014_1Hz2pct.pdf">1 Hz 2% in 50 yrs</a>(1.0MB PDF)</li>
+          <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/2014/2014_1Hz10pct.pdf">1 Hz 10% in 50 yrs</a>(0.8MB PDF)</li>
+    </ul>
+  </li>
+
+  <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/2014/data/">Data</a>
+  </li>
+
+  <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/2014/USpga250.zip">GIS Shapefiles</a> (PGA, 2% in 50yrs) (5MB Zip)</a>
+  </li>
+
+  <li><a href="https://github.com/usgs/nshmp-haz-fortran">Source code</a>
+  </li>
+
+  <li><a href="https://github.com/usgs/nshmp-haz-catalogs">Catalogs</a>
+  </li>
+
+  <li><a href="http://geohazards.usgs.gov/cfusion/hazfaults_2014_search/query_main.cfm">Faults Database Search</a>
+  </li>
+
+  <li><a href="/hazards/qfaults/map/hazfault2014.html">Fault Source Map</a></li>
+
+  <li><strong><a href="http://pubs.usgs.gov/of/2014/1091/">Hazard Model Documentation</a> (USGS OFR 2014-1091)</strong></li>
+  <li><a href="http://pubs.usgs.gov/of/2013/1293/
+">Geodetic and Geologic Slip Model Documentation</a> (USGS OFR 2014-1293)</li>
+  <li><a href="http://pubs.usgs.gov/sim/3325/">Seismic-Hazard Maps for the Conterminous United States, 2014</a> (USGS Scientific Investigations Map 3325)</li>
+</ul>
+
+<h3>Additional Information</h3>
+
+<p>The California portion of the 2104 NSHMP is based on the Uniform California Earthquake Rupture Forecast version 3 (UCERF3). The model was developed by Working Group on California Earthquake Probabilities (WGCEP). For more information see:</p>
+  <ul>
+    <li><a href="http://pubs.usgs.gov/of/2013/1165/">Uniform California Earthquake Rupture Forecast, Version 3 (UCERF3)-The Time-Independent Model</a> (USGS OFR 2013-1165)</li>
+    <li><a href="http://www.wgcep.org/">Working Group on California Earthquake Probabilities (WGCEP)</a></li>
+  </ul>
+
+<h2 id="2008">2008</h2>
+
+<p>The 2008 U.S. Geological Survey (USGS) National Seismic Hazard Maps display earthquake ground motions for various probability levels across the United States and are applied in seismic provisions of building codes, insurance rate structures, risk assessments, and other public policy. This update of the maps incorporates new findings on earthquake ground shaking, faults, seismicity, and geodesy. The resulting maps are derived from seismic hazard curves calculated on a grid of sites across the United States that describe the frequency of exceeding a set of ground motions.
+
+The USGS National Seismic Hazard Mapping Project developed these maps by incorporating information on potential earthquakes and associated ground shaking obtained from interaction in science and engineering workshops involving hundreds of participants, review by several science organizations and State surveys, and advice from two expert panels.
+
+The new probabilistic hazard maps represent an update of the 2002 seismic hazard maps developed by Frankel and others (2002), which used the methodology developed for the 1996 version of the maps (Frankel and others, 1996). Algermissen and Perkins (1976) published the first probabilistic seismic hazard map of the United States which was updated in Algermissen and others (1990). The national seismic maps represent our assessment of the “best available science” in earthquake hazards estimation for the United States.</p>
+
+<ul>
+  <li><a href="2008/maps/">Maps</a></li>
+  <li><a href="2008/data/">Data</a></li>
+  <li><a href="2008/software/">Source Code</a></li>
+  <li><a href="2008/catalogs/">Catalogs</a></li>
+  <li><a href="http://geohazards.usgs.gov/cfusion/hazfaults_2008_search">Faults Database Search</a></li>
+  <li><a href="/hazards/qfaults/map/hazfault2008.html">Fault Source Map</a></li>
+  <li><a href="http://pubs.usgs.gov/of/2008/1128/" target="_blank"
+    >Documentation for the 2008 Update of the United States National Seismic Hazard Maps</a> (USGS OFR 2008-1128)</li>
+</ul>
+
+<h3>Additional Information</h3>
+<ul>
+    <li><a href="http://pubs.usgs.gov/fs/2008/3018/">2008 United States National Seismic Hazard Maps</a> - (USGS Fact Sheet 2008-3018)</li>
+    <li><a href="http://pubs.usgs.gov/sim/3195/">Seismic-Hazard Maps for the Conterminous United States, 2008</a> (USGS Scientific Investigations Map 3195)</li>
+  <li><a href="http://pubs.usgs.gov/of/2007/1437/l/of2007-1437l.pdf"
+    >Appendix L: Cascadia Subduction Zone</a> (USGS OFR 2007-1437L)</li>
+  <li><a href="http://pubs.usgs.gov/fs/2009/3071/">Earthquake Hazard in the New Madrid Seismic Zone Remains a Concern</a> (USGS Fact Sheet 2009-3071)
+  </li>
+  <li><a href="http://pubs.usgs.gov/of/2007/1437/">The Uniform California Earthquake Rupture Forecast, Version 2 (UCERF2)</a> (USGS OFR 2007-1437)
+  </li>
+  <li><a href="http://www.wgcep.org/">Working Group on California Earthquake Probabilities</a></li>
+    <li><a href="2008/99HQGR0098.pdf" title="2001, P. Sommerville et al. Final Report">Ground Motion Attenuation Relations for the Central and Eastern United States</a> (USGS-funded)
+    </li>
+    <li><a href="2008/update_200805.php">Revision I, May 2008</a></li>
+    <li><a href="2008/update_200812.php">Revision II, December 2008</a> (The Basis for <a href="/hazards/designmaps/usdesign.php">Design Maps</a>)</li>
+    <li><a href="2008/update_201001/">Revision III, January 2010</a></li>
+
+
+</ul>
+
+<h2 id="2002">2002</h2>
+
+<p>Following the release of the 2002 United States hazard maps and data, two subsequent updates were released. The first in April of 2003 and the second in October of 2003. Descriptions of each release can be found below. What is currently made available on our web site reflects the updated data and image files.</p>
+
+<ul>
+  <li><a href="2002/maps/">Maps</a></li>
+  <li><a href="2002/data/">Data</a></li>
+  <li><a href="2002/software/">Source Code</a></li>
+  <li><a href="http://geohazards.usgs.gov/cfusion/hazfaults_2002_search">Faults Database Search</a></li>
+  <li><a href="/hazards/qfaults/map/hazfault2002.html">Fault Source Map</a></li>
+  <li><a href="2002/documentation/">Documentation</a></li>
+</ul>
+
+<h3>Additional Information</h3>
+<ul>
+  <li><a href="http://pubs.usgs.gov/of/2002/ofr-02-420/">Documentation for the 2002 Update of the National Seismic Hazard Maps</a> (USGS OFR 02-420)</li>
+  <li><a href="http://geopubs.wr.usgs.gov/open-file/of02-123/">Implosion, Earthquake, and Explosion Recordings from the 2000 Seattle Kingdome Seismic Hazards Investigation of Puget Sound (SHIPS), Washington</a> (USGS OFR 02-123)</li>
+  <li><a href="http://pubs.usgs.gov/of/2002/ofr-02-0073/ofr-02-0073.html">Investigation of an Earthquake Swarm near Trinidad, Colorado, August-October 2001</a> (USGS OFR 02-0073)</li>
+  <li><a href="2002/update_2002apr.php">Revision I, April 2003</a></li>
+  <li><a href="2002/update_2002oct.php">Revision II, October 2003</a></li>
+</ul>
+
+<h2 id="1996">1996</h2>
+
+<p>
+  The computations for these maps and data used 50 year return periods on firm rock at 760 m/sec. All of the maps were prepared by combining hazard derived from spatially- smoothed historic seismicity with hazard from fault-specific sources. For more information please read our full documentation.
+</p>
+<p>
+  Although these maps have been used by the U.S. Geological Survey, no warranty, expressed or implied is made by the USGS as to the accuracy of the maps and related material nor shall the fact of distribution constitute any such warranty, and no responsibility is assumed by the USGS in connection therewith.
+</p>
+
+<ul>
+  <li><a href="1996/maps.php">Maps</a></li>
+  <li><a href="1996/data.php">Data</a></li>
+  <li><a href="1996/software.php">Source Code</a></li>
+  <li><a href="1996/documentation.php">Documentation</a></li>
+</ul>
+
+<h3>Additional Information</h3>
+<ul>
+  <li>
+    <a href="http://pubs.usgs.gov/fs/1996/fs183-96/fs183-96.pdf"
+    >Hazard Maps Help Save Lives and Property</a> (USGS Fact Sheet 96-183)</a>
+  </li>
+  <li>
+    <a href="http://pubs.usgs.gov/of/1996/ofr-96-0723/">Site-Response Maps for the Los Angeles Region Based on Earthquake Ground Motions</a> (USGS OFR 96-723)
+  </li>
+  <li>
+    <a href="http://pubs.er.usgs.gov/usgspubs/ofr/ofr97464">Preparation of Earthquake Catalogs for the National Seismic-Hazard Maps : Contiguous 48 States</a> (USGS OFR 97-464)
+  </li>
+  <li>
+    <a href="http://pubs.er.usgs.gov/usgspubs/ofr/ofr97131">Seismic-Hazard Maps for the Conterminous United States</a> (USGS OFR 97-131)
+  </li>
+  <li>
+    <a href="http://pubs.er.usgs.gov/usgspubs/ofr/ofr97130">Seismic-Hazard Maps for California, Nevada, and Western Arizona/Utah</a> (USGS OFR 97-130_)
+  </li>
+</ul>
