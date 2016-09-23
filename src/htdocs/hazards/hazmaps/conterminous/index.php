70f7520be19bd97d1911e61419064d8f769e0a45
lisa-usgs <lisa@usgs.gov>
2016-09-23 09:19:38 -0600

    hazards us work

--- src/htdocs/hazards/hazmaps/conterminous/index.php
+++ src/htdocs/hazards/hazmaps/conterminous/index.php
@@ -27,10 +27,12 @@
 
 <h2 id="2014">2014</h2>
 
-<p>The 2014 U.S. Geological Survey (USGS) National Seismic Hazard Maps display earthquake ground motions for various probability levels across the United States and are applied in seismic provisions of building codes, insurance rate structures, risk assessments, and other public policy. The updated maps represent an assessment of the best available science in earthquake hazards and incorporate new findings on earthquake ground shaking, faults, seismicity, and geodesy. The USGS National Seismic Hazard Mapping Project developed these maps by incorporating information on potential earthquakes and associated ground shaking obtained from interaction in science and engineering workshops involving hundreds of participants, review by several science organizations and State surveys, and advice from expert panels and a Steering Committee. The new probabilistic hazard maps represent an update of the seismic hazard maps; previous versions were developed by Petersen and others (2008) and Frankel and others (2002), using the methodology developed Frankel and others (1996). Algermissen and Perkins (1976) published the first probabilistic seismic hazard map of the United States which was updated in Algermissen and others (1990).</p>
-
-<p>The National Seismic Hazard Maps are derived from seismic hazard curves calculated on a grid of sites across the United States that describe the annual frequency of exceeding a set of ground motions. Data and maps from the 2014 U.S. Geological Survey National Seismic Hazard Mapping Project are available for download below. Maps for available periods (0.2 s, 1 s, PGA) and specified annual frequencies of exceedance can be calculated from the hazard curves. Figures depict probabilistic ground motions with a 2 percent probability of exceedance. Spectral accelerations are calculated for 5 percent damped linear elastic oscillators. All ground motions are calculated for site conditions with Vs30=760 m/s, corresponding to NEHRP B/C site class boundary.</p>
-
+<p>
+  The 2014 U.S. Geological Survey (USGS) National Seismic Hazard Maps display earthquake ground motions for various probability levels across the United States and are applied in seismic provisions of building codes, insurance rate structures, risk assessments, and other public policy. The updated maps represent an assessment of the best available science in earthquake hazards and incorporate new findings on earthquake ground shaking, faults, seismicity, and geodesy. The USGS National Seismic Hazard Mapping Project developed these maps by incorporating information on potential earthquakes and associated ground shaking obtained from interaction in science and engineering workshops involving hundreds of participants, review by several science organizations and State surveys, and advice from expert panels and a Steering Committee. The new probabilistic hazard maps represent an update of the seismic hazard maps; previous versions were developed by Petersen and others (2008) and Frankel and others (2002), using the methodology developed Frankel and others (1996). Algermissen and Perkins (1976) published the first probabilistic seismic hazard map of the United States which was updated in Algermissen and others (1990).
+</p>
+<p>
+  The National Seismic Hazard Maps are derived from seismic hazard curves calculated on a grid of sites across the United States that describe the annual frequency of exceeding a set of ground motions. Data and maps from the 2014 U.S. Geological Survey National Seismic Hazard Mapping Project are available for download below. Maps for available periods (0.2 s, 1 s, PGA) and specified annual frequencies of exceedance can be calculated from the hazard curves. Figures depict probabilistic ground motions with a 2 percent probability of exceedance. Spectral accelerations are calculated for 5 percent damped linear elastic oscillators. All ground motions are calculated for site conditions with Vs30=760 m/s, corresponding to NEHRP B/C site class boundary.
+</p>
 <ul>
   <li><a href="2014/images/HazardMap2014_lg.jpg"/>Simplified 2014 Hazard Map (PGA, 2% in 50 years)</a></li>
   <li>Maps: Conterminous US
@@ -69,7 +71,9 @@
 
 <h3>Additional Information</h3>
 
-<p>The California portion of the 2104 NSHMP is based on the Uniform California Earthquake Rupture Forecast version 3 (UCERF3). The model was developed by Working Group on California Earthquake Probabilities (WGCEP). For more information see:</p>
+<p>
+  The California portion of the 2104 NSHMP is based on the Uniform California Earthquake Rupture Forecast version 3 (UCERF3). The model was developed by Working Group on California Earthquake Probabilities (WGCEP). For more information see:
+</p>
   <ul>
     <li><a href="http://pubs.usgs.gov/of/2013/1165/">Uniform California Earthquake Rupture Forecast, Version 3 (UCERF3)-The Time-Independent Model</a> (USGS OFR 2013-1165)</li>
     <li><a href="http://www.wgcep.org/">Working Group on California Earthquake Probabilities (WGCEP)</a></li>
@@ -77,59 +81,56 @@
 
 <h2 id="2008">2008</h2>
 
-<p>The 2008 U.S. Geological Survey (USGS) National Seismic Hazard Maps display earthquake ground motions for various probability levels across the United States and are applied in seismic provisions of building codes, insurance rate structures, risk assessments, and other public policy. This update of the maps incorporates new findings on earthquake ground shaking, faults, seismicity, and geodesy. The resulting maps are derived from seismic hazard curves calculated on a grid of sites across the United States that describe the frequency of exceeding a set of ground motions.
-
-The USGS National Seismic Hazard Mapping Project developed these maps by incorporating information on potential earthquakes and associated ground shaking obtained from interaction in science and engineering workshops involving hundreds of participants, review by several science organizations and State surveys, and advice from two expert panels.
-
-The new probabilistic hazard maps represent an update of the 2002 seismic hazard maps developed by Frankel and others (2002), which used the methodology developed for the 1996 version of the maps (Frankel and others, 1996). Algermissen and Perkins (1976) published the first probabilistic seismic hazard map of the United States which was updated in Algermissen and others (1990). The national seismic maps represent our assessment of the “best available science” in earthquake hazards estimation for the United States.</p>
-
+<p>
+  The 2008 U.S. Geological Survey (USGS) National Seismic Hazard Maps display earthquake ground motions for various probability levels across the United States and are applied in seismic provisions of building codes, insurance rate structures, risk assessments, and other public policy. This update of the maps incorporates new findings on earthquake ground shaking, faults, seismicity, and geodesy. The resulting maps are derived from seismic hazard curves calculated on a grid of sites across the United States that describe the frequency of exceeding a set of ground motions.
+</p>
+<p>
+  The USGS National Seismic Hazard Mapping Project developed these maps by incorporating information on potential earthquakes and associated ground shaking obtained from interaction in science and engineering workshops involving hundreds of participants, review by several science organizations and State surveys, and advice from two expert panels.
+</p>
+<p>
+  The new probabilistic hazard maps represent an update of the 2002 seismic hazard maps developed by Frankel and others (2002), which used the methodology developed for the 1996 version of the maps (Frankel and others, 1996). Algermissen and Perkins (1976) published the first probabilistic seismic hazard map of the United States which was updated in Algermissen and others (1990). The national seismic maps represent our assessment of the “best available science” in earthquake hazards estimation for the United States.
+</p>
 <ul>
-  <li><a href="2008/maps/">Maps</a></li>
-  <li><a href="2008/data/">Data</a></li>
-  <li><a href="2008/software/">Source Code</a></li>
-  <li><a href="2008/catalogs/">Catalogs</a></li>
-  <li><a href="http://geohazards.usgs.gov/cfusion/hazfaults_2008_search">Faults Database Search</a></li>
-  <li><a href="/hazards/qfaults/map/hazfault2008.html">Fault Source Map</a></li>
-  <li><a href="http://pubs.usgs.gov/of/2008/1128/" target="_blank"
-    >Documentation for the 2008 Update of the United States National Seismic Hazard Maps</a> (USGS OFR 2008-1128)</li>
+  <li><a href="2008/maps.php">Maps</a> - Revision III</li>
+  <li><a href="2008/data.php">Data</a> - Revision III</li>
+  <li><a href="2008/software/">Source Code - ?????</a></li>
+  <li><a href="2008/catalogs.php">Catalogs</a></li>
+  <li><a href="http://pubs.usgs.gov/of/2008/1128/">Documentation for the 2008 Update of the United States National Seismic Hazard Maps</a> (USGS OFR 2008-1128)</li>
 </ul>
 
 <h3>Additional Information</h3>
 <ul>
-    <li><a href="http://pubs.usgs.gov/fs/2008/3018/">2008 United States National Seismic Hazard Maps</a> - (USGS Fact Sheet 2008-3018)</li>
-    <li><a href="http://pubs.usgs.gov/sim/3195/">Seismic-Hazard Maps for the Conterminous United States, 2008</a> (USGS Scientific Investigations Map 3195)</li>
-  <li><a href="http://pubs.usgs.gov/of/2007/1437/l/of2007-1437l.pdf"
-    >Appendix L: Cascadia Subduction Zone</a> (USGS OFR 2007-1437L)</li>
+  <li><a href="2008/update200805.php">Revision I, May 2008</a></li>
+  <li><a href="2008/update200812.php">Revision II, December 2008</a> (the basis for the <a href="/hazards/designmaps/usdesign.php">Design Maps</a>)</li>
+  <li><a href="2008/update201001.php">Revision III, January 2010</a></li>
+
+  <li><a href="http://pubs.usgs.gov/fs/2008/3018/">2008 United States National Seismic Hazard Maps</a> - (USGS Fact Sheet 2008-3018)</li>
+  <li><a href="http://pubs.usgs.gov/sim/3195/">Seismic-Hazard Maps for the Conterminous United States, 2008</a> (USGS Scientific Investigations Map 3195)</li>
+  <li><a href="http://pubs.usgs.gov/of/2007/1437/l/of2007-1437l.pdf">Appendix L: Cascadia Subduction Zone</a> (USGS OFR 2007-1437L)</li>
   <li><a href="http://pubs.usgs.gov/fs/2009/3071/">Earthquake Hazard in the New Madrid Seismic Zone Remains a Concern</a> (USGS Fact Sheet 2009-3071)
   </li>
   <li><a href="http://pubs.usgs.gov/of/2007/1437/">The Uniform California Earthquake Rupture Forecast, Version 2 (UCERF2)</a> (USGS OFR 2007-1437)
   </li>
   <li><a href="http://www.wgcep.org/">Working Group on California Earthquake Probabilities</a></li>
-    <li><a href="2008/99HQGR0098.pdf" title="2001, P. Sommerville et al. Final Report">Ground Motion Attenuation Relations for the Central and Eastern United States</a> (USGS-funded)
-    </li>
-    <li><a href="2008/update_200805.php">Revision I, May 2008</a></li>
-    <li><a href="2008/update_200812.php">Revision II, December 2008</a> (The Basis for <a href="/hazards/designmaps/usdesign.php">Design Maps</a>)</li>
-    <li><a href="2008/update_201001/">Revision III, January 2010</a></li>
-
-
+  <li><a href="2008/99HQGR0098.pdf" title="2001, P. Sommerville et al. Final Report">Ground Motion Attenuation Relations for the Central and Eastern United States</a> (USGS-funded)
+  </li>
 </ul>
 
 <h2 id="2002">2002</h2>
 
-<p>Following the release of the 2002 United States hazard maps and data, two subsequent updates were released. The first in April of 2003 and the second in October of 2003. Descriptions of each release can be found below. What is currently made available on our web site reflects the updated data and image files.</p>
+<p>
+  Following the release of the 2002 United States hazard maps and data, two subsequent updates were released. The first in April of 2003 and the second in October of 2003. Descriptions of each release can be found below. <strong>What is currently made available on our web site reflects the updated data and image files.</strong>
+</p>
 
 <ul>
-  <li><a href="2002/maps/">Maps</a></li>
-  <li><a href="2002/data/">Data</a></li>
-  <li><a href="2002/software/">Source Code</a></li>
-  <li><a href="http://geohazards.usgs.gov/cfusion/hazfaults_2002_search">Faults Database Search</a></li>
-  <li><a href="/hazards/qfaults/map/hazfault2002.html">Fault Source Map</a></li>
-  <li><a href="2002/documentation/">Documentation</a></li>
+  <li><a href="2002/maps.php">Maps</a></li>
+  <li><a href="2002/data.php">Data</a></li>
+  <li>Source Code is not available.</li>
+  <li><a href="http://pubs.usgs.gov/of/2002/ofr-02-420/">Documentation for the 2002 Update of the National Seismic Hazard Maps</a> (USGS OFR 02-420)</li>
 </ul>
 
 <h3>Additional Information</h3>
 <ul>
-  <li><a href="http://pubs.usgs.gov/of/2002/ofr-02-420/">Documentation for the 2002 Update of the National Seismic Hazard Maps</a> (USGS OFR 02-420)</li>
   <li><a href="http://geopubs.wr.usgs.gov/open-file/of02-123/">Implosion, Earthquake, and Explosion Recordings from the 2000 Seattle Kingdome Seismic Hazards Investigation of Puget Sound (SHIPS), Washington</a> (USGS OFR 02-123)</li>
   <li><a href="http://pubs.usgs.gov/of/2002/ofr-02-0073/ofr-02-0073.html">Investigation of an Earthquake Swarm near Trinidad, Colorado, August-October 2001</a> (USGS OFR 02-0073)</li>
   <li><a href="2002/update_2002apr.php">Revision I, April 2003</a></li>
@@ -148,8 +149,8 @@ The new probabilistic hazard maps represent an update of the 2002 seismic hazard
 <ul>
   <li><a href="1996/maps.php">Maps</a></li>
   <li><a href="1996/data.php">Data</a></li>
-  <li><a href="1996/software.php">Source Code</a></li>
-  <li><a href="1996/documentation.php">Documentation</a></li>
+  <li>Source Code is not available.</li>
+  <li><a href="http://pubs.usgs.gov/of/1996/532/">National Seismic Hazard Maps: Documentation June 1996</a> (OFR 96-532) and <a href="ftp://hazards.cr.usgs.gov/web/nshm/conterminous/1996/documentation/US1996Figures.zip">Figures from Open-File Report</a> (6 MB Zip file)</li>
 </ul>
 
 <h3>Additional Information</h3>
