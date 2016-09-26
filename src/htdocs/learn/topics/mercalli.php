2c32426074f8cbebc2f1b55a06e1ed5acc4439d3
tkalin-usgs <tkalin@usgs.gov>
2016-09-26 10:51:57 -0600

    Some more learntopics work
    Former-commit-id: 28cbbaeacbfc599ab844a09d82d4ecb18a626573

---
+++ src/htdocs/learn/topics/mercalli.php
@@ -0,0 +1,157 @@
+<?php
+      //	Author:	Tiffany Kalin
+      //	Contact:
+      //	Last	modified:	09/26/2016
+      if	(!isset($TEMPLATE))	{
+      $TITLE	=	'The Modified Mercalli Intensity Scale';
+      $NAVIGATION	= true;
+      include	'template.inc.php';
+      }
+?>
+<p>
+The effect of an earthquake on the Earth's
+surface is called the intensity. The intensity
+scale consists of a series of certain key
+responses such as people awakening, movement
+of furniture, damage to chimneys, and
+finally - total destruction. Although numerous
+<i> intensity scales</i> have been developed over
+the last several hundred years to evaluate
+the effects of earthquakes, the one currently
+used in the United States is the Modified
+Mercalli (MM) Intensity Scale. It was developed
+in 1931 by the American seismologists
+Harry Wood and Frank Neumann. This scale,
+composed of increasing levels of intensity
+that range from imperceptible shaking to
+catastrophic destruction, is designated by
+Roman numerals. It does not have a mathematical
+basis; instead it is an arbitrary
+ranking based on observed effects.
+</p>
+<p>
+The Modified Mercalli Intensity value
+assigned to a specific site after an earthquake
+has a more meaningful measure of
+severity to the nonscientist than the magnitude
+because intensity refers to the effects
+actually experienced at that place.
+</p>
+<p>
+The <strong>lower</strong> numbers of the intensity scale
+generally deal with the manner in which the
+earthquake is felt by people. The <b>higher</b>
+numbers of the scale are based on observed
+structural damage. Structural engineers
+usually contribute information for assigning
+intensity values of VIII or above.
+</p>
+<p>
+The following is an abbreviated description of
+the levels of Modified Mercalli intensity.
+</p>
+
+<table>
+	<thead>
+		<tr>
+			<th>Intensity</th>
+			<th>Shaking</th>
+			<th>Description/Damage</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td style="background-color:#fff;">I</td>
+			<td>Not felt</td>
+			<td>Not felt except by a very few under especially
+					favorable conditions.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#dfe6fe;">II</td>
+			<td>Weak</td>
+			<td>Felt only by a few persons at rest,especially on upper floors of
+					buildings.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#82f9fb;">III</td>
+			<td>Weak</td>
+			<td>Felt quite noticeably by persons indoors,
+					especially on upper floors of buildings.
+					Many people do not recognize it as an
+					earthquake. Standing motor cars may
+					rock slightly.  Vibrations similar to the
+					passing of a truck. Duration estimated.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#7efbdf;">IV</td>
+			<td>Light</td>
+			<td>Felt indoors by many, outdoors by few
+					during the day. At night, some awakened.
+					Dishes, windows, doors disturbed; walls
+					make cracking sound. Sensation like
+					heavy truck striking building. Standing
+					motor cars rocked noticeably.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#95f879;">V</td>
+			<td>Moderate</td>
+			<td>Felt by nearly everyone; many awakened.
+					Some dishes, windows broken. Unstable
+					objects overturned. Pendulum clocks
+					may stop.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#f7f835;">VI</td>
+			<td>Strong</td>
+			<td>Felt by all, many frightened. Some heavy
+					furniture moved; a few instances of fallen
+					plaster. Damage slight.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#fdca2c;">VII</td>
+			<td>Very strong</td>
+			<td>Damage negligible in buildings of good
+					design and construction; slight to moderate
+					in well-built ordinary structures;
+					considerable damage in poorly built or
+					badly designed structures; some
+					chimneys broken.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#ff701f;">VIII</td>
+			<td>Severe</td>
+			<td>Damage slight in specially designed
+					structures; considerable damage in
+					ordinary substantial buildings with
+					partial collapse. Damage great in poorly built
+					structures. Fall of chimneys, factory
+					stacks, columns, monuments, walls.
+					Heavy furniture overturned.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#ec2516;">IX</td>
+			<td>Violent</td>
+			<td>Damage considerable in specially designed
+					structures; well-designed frame
+					structures thrown out of plumb. Damage
+					great in substantial buildings, with partial
+					collapse. Buildings shifted off foundations.</td>
+		</tr>
+		<tr>
+			<td style="background-color:#c81e11;">X</td>
+			<td>Extreme</td>
+			<td>Some well-built wooden structures
+					destroyed; most masonry and frame
+					structures destroyed with foundations.
+					Rails bent.</td>
+		</tr>
+	</tbody>
+</table>
+<HR>
+
+<p>
+Abridged from <i><a href="http://pubs.usgs.gov/gip/earthq4/severitygip.html">The Severity of an Earthquake</a></i>, a U. S. Geological Survey General Interest Publication. U.S. GOVERNMENT PRINTING OFFICE: 1989-288-913
+</p>
+<p>
+This publication is one of a series of <a href="http://pubs.er.usgs.gov/browse/usgs-publications/GIP">general interest publications</a> prepared by the U.S. Geological Survey to provide information about the earth sciences, natural resources, and the environment.
+</p>
