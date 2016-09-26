deb3408e1347ea60a15d942a5d449b639a317cea
tkalin-usgs <tkalin@usgs.gov>
2016-09-26 09:10:06 -0600

    Initial hazards work
    Former-commit-id: 0da5635ee3e6ce59274b58affd5b163dfe0a80e0

--- src/htdocs/hazards/hazmaps/prvi/2003/data/index.php
+++ src/htdocs/hazards/hazmaps/prvi/2003/data/index.php
@@ -1,167 +1,150 @@
 <?php
-	if (!isset($TEMPLATE)) {
-		$TITLE = 'Puerto Rico Data Files';
-		$STYLES = "
-			#tbldownloads { margin:0 auto; width:544px; }
-			#tbldownloads td, #tbldownloads th {
-				color:#333333;
-				padding:3px 8px;
-			}
-			#tbldownloads th { font-weight:bold; border-bottom:1px solid #666666; }
-			#tbldownloads td.downloads {font-size:.8em;padding-bottom:.2em;}
-			#formats p {font-size:.8em;}
-			#formats ul {
-				list-style-position:inside;
-				font-size:.9em;
-				margin:0;
-				padding:0;
-			}
-			#formats li {margin:0;padding:0}
-			#formats label {font-family:Monospace;font-weight:bold;}
-			#formats ul span {display:block;margin-left:5px;font-size:.8em;}
-			code {font-family:Monospace;}
-		";
-		$WIDGETS = 'jquery';
-		$SCRIPTS = '/template/js/jquery.tablesorter.js';
-
-		function parseType($filename) {
-			if (strpos($filename, 'hc')) {
-				return 'Hazard Curves';
-			} else if (strpos($filename, 'gm')) {
-				return 'Ground Motion';
-			} else {
-				return 'Unknown';
-			}
-		}
-		function parseSA($filename) {
-			if (strpos($filename, '0p3')) {
-				return '0.3 Second (3.33Hz)';
-			} else if (strpos($filename, '1p0')) {
-				return '1.0 Second (1Hz)';
-			} else if (strpos($filename, '0p2')) {
-				return '0.2 Second (5Hz)';
-			} else if (strpos($filename, '0p0')) {
-				return 'Peak Ground Acceleration';
-			} else {
-				return 'Not Applicable';
-			}
-		}
-		function parsePE($filename) {
-			if (strpos($filename, '2p50')) {
-				return '2&#37; in 50 Years';
-			} else if (strpos($filename, '10p50')) {
-				return '10&#37; in 50 Years';
-			} else {
-				return 'Not Applicable';
-			}
-		}
-		function getDownloadsMarkup($filename) {
-			$basedir = str_replace("\n", '', `pwd`);
-			$txtfile = $filename;
-			$binfile = str_replace('.txt', '.bin', $filename);
-			return sprintf('[ <a href="%s" title="%s">TEXT</a> | ' .
-			               '<a href="%s" title="%s">BINARY</a> ]',
-								$txtfile, simpleFileSize("${basedir}/${txtfile}"),
-								$binfile, simpleFileSize("${basedir}/${binfile}")
-							);
-		}
-		function getRowMarkup($filename) {
-			return sprintf('<tr><td class="type">%s</td><td class="sa">%s</td>' .
-					'<td class="pe">%s</td><td class="downloads">%s</td></tr>',
-					parseType($filename), parseSA($filename), parsePE($filename),
-					getDownloadsMarkup($filename)
-				);
-		}
-		include_once $_SERVER['DOCUMENT_ROOT'] . '/template/template.inc.php';
-	}
-
-	$files = glob('downloads/*.txt');
-
-	if (!empty($files)) {
+      //	Author:	Tiffany Kalin
+      //	Contact:
+      //	Last	modified:	09/21/2016
+      if	(!isset($TEMPLATE))	{
+      $TITLE	=	'Puerto Rico Data Files';
+      $NAVIGATION	= true;
+      include	'template.inc.php';
+      }
 ?>
-<div class="column seven">
-<h2>Download Available Files</h2>
-<p>
-	Data files are presented in two formats, text and binary. Both formats for
-	a given file contain the same information. The text file is white-space
-	delimited and can easily be imported into your favorite spreadsheet
-	application (i.e. Microsoft Excel&trade;) for further analysis. The binary
-	format is more useful for programatic access from custom pieces of software.
-</p>
-<table cellpadding="0" cellspacing="0" border="0" id="tbldownloads">
-<thead>
-	<tr>
-		<th scope="col" class="type">File Contents</th>
-		<th scope="col" class="sa">Spectral Acceleration</th>
-		<th scope="col" class="period">Return Period</th>
-		<th scope="col" class="downloads">Downloads</th>
-	</tr>
-</thead>
-<tbody>
-	<?php foreach($files as $file) { print getRowMarkup($file); } ?>
-</tbody>
-</table>
-<p style="color:#666666;font-size:.8em;margin:0;padding:0;text-align:right;">
-	Click a file header to re-sort table listing by that column.
-</P>
-</div>
-<?php
-	} else {
-		print '<p class="error">No files currently available.</p>';
-	}
-?>
-<div id="formats" class="column three">
-	<h2>More About File Formats</h2>
-	<p>
-		<strong>Text File Formats</strong><br />
-		These files are simple white-space delimited files. The format should be
-		farily easy to decipher without much guidance.
-	</p>
-	<p>
-		<strong>Binary File Formats</strong><br />
-		These files were created from the text files and have the same format. In
-		order to help with programatic reading of these files there is additional
-		header information at the top of these files. Header information contains:
-	</p>
-	<ul>
-		<li>
-			<label>FileType (int)</label>
-			<span>0 &rarr; Hazard Curve File</span>
-			<span>1 &rarr; Ground Motion File</span>
-		</li>
-		<li><label>Minimum Latitude (double)</label></li>
-		<li><label>Maximum Latitude (double)</label></li>
-		<li><label>Minimum Longitude (double)</label></li>
-		<li><label>Maximum Longitude (double)</label></li>
-		<li>
-			<label>Grid Spacing (double)</label>
-			<span>
+<div class="row">
+	<div class="column two-of-three">
+		<h2>Download Available Files</h2>
+		<p>
+			Data files are presented in two formats, text and binary. Both formats for
+			a given file contain the same information. The text file is white-space
+			delimited and can easily be imported into your favorite spreadsheet
+			application (i.e. Microsoft Excel&trade;) for further analysis. The binary
+			format is more useful for programatic access from custom pieces of software.
+		</p>
+		<table>
+			<thead>
+				<tr>
+					<th>File Contents</th>
+					<th>Spectral Acceleration</th>
+					<th>Return Period</th>
+					<th>Downloads</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Ground Motion</td>
+					<td>0.2 Second (5Hz)</td>
+					<td>10% in 50 Years</td>
+					<td>[ <a href="downloads/pr_gm_0p2s_10p50.txt" title="555 kB">TEXT</a> | <a href="downloads/pr_gm_0p2s_10p50.bin" title="127 kB">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Ground Motion</td>
+					<td>0.3 Second (3.33Hz)</td>
+					<td>10% in 50 Years</td>
+					<td>[ <a href="downloads/pr_gm_0p3s_10p50.txt" title="555 kB">TEXT</a> | <a href="downloads/pr_gm_0p3s_10p50.bin" title="127 kB">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Ground Motion</td>
+					<td>1.0 Second (1Hz)</td>
+					<td>10% in 50 Years</td>
+					<td>[ <a href="downloads/pr_gm_1p0s_10p50.txt" title="555 kB">TEXT</a> | <a href="downloads/pr_gm_1p0s_10p50.bin" title="127 kB">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Ground Motion</td>
+					<td>Peak Ground Acceleration</td>
+					<td>10% in 50 Years</td>
+					<td>[ <a href="downloads/pr_gm_0p0s_10p50.txt" title="555 kB">TEXT</a> | <a href="downloads/pr_gm_0p0s_10p50.bin" title="127 kB">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Ground Motion</td>
+					<td>0.2 Second (5Hz)</td>
+					<td>2% in 50 Years</td>
+					<td>[ <a href="downloads/pr_gm_0p2s_2p50.txt" title="555 kB">TEXT</a> | <a href="downloads/pr_gm_0p2s_2p50.bin" title="127 kB">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Ground Motion</td>
+					<td>0.3 Second (3.33Hz)</td>
+					<td>2% in 50 Years</td>
+					<td>[ <a href="downloads/pr_gm_0p3s_2p50.txt" title="555 kB">TEXT</a> | <a href="downloads/pr_gm_0p3s_2p50.bin" title="127 kB">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Ground Motion</td>
+					<td>1.0 Second (1Hz)</td>
+					<td>2% in 50 Years</td>
+					<td>[ <a href="downloads/pr_gm_1p0s_2p50.txt" title="555 kB">TEXT</a> | <a href="downloads/pr_gm_1p0s_2p50.bin" title="127 kB">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Ground Motion</td>
+					<td>Peak Ground Acceleration</td>
+					<td>2% in 50 Years</td>
+					<td>[ <a href="downloads/pr_gm_0p0s_2p50.txt" title="555 kB">TEXT</a> | <a href="downloads/pr_gm_0p0s_2p50.bin" title="127 kB">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Hazard Curves</td>
+					<td>0.2 Second (5Hz)</td>
+					<td>Not Applicable</td>
+					<td>[ <a href="downloads/pr_hc_0p2s.txt" title="3 Mb">TEXT</a> | <a href="downloads/pr_hc_0p2s.bin" title="2 Mb">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Hazard Curves</td>
+					<td>0.3 Second (3.33Hz)</td>
+					<td>Not Applicable</td>
+					<td>[ <a href="downloads/pr_hc_0p3s.txt" title="3 Mb">TEXT</a> | <a href="downloads/pr_hc_0p3s.bin" title="2 Mb">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Hazard Curves</td>
+					<td>1.0 Second (1Hz)</td>
+					<td>Not Applicable</td>
+					<td>[ <a href="downloads/pr_hc_1p0s.txt" title="3 Mb">TEXT</a> | <a href="downloads/pr_hc_1p0s.bin" title="2 Mb">BINARY</a> ]</td>
+				</tr>
+				<tr>
+					<td>Hazard Curves</td>
+					<td>Peak Ground Acceleration</td>
+					<td>Not Applicable</td>
+					<td>[ <a href="downloads/pr_hc_0p0s.txt" title="3 Mb">TEXT</a> | <a href="downloads/pr_hc_0p0s.bin" title="2 Mb">BINARY</a> ]</td>
+				</tr>
+			</tbody>
+		</table>
+		<!--<p style="color:#666666;font-size:.8em;margin:0;padding:0;text-align:right;">
+			Click a file header to re-sort table listing by that column.
+		</p>-->
+	</div>
+	<div class="column one-of-three">
+		<h2>More About File Formats</h2>
+		<p>
+			<strong>Text File Formats</strong><br />
+			These files are simple white-space delimited files. The format should be
+			farily easy to decipher without much guidance.
+		</p>
+		<p>
+			<strong>Binary File Formats</strong><br />
+			These files were created from the text files and have the same format. In
+			order to help with programatic reading of these files there is additional
+			header information at the top of these files. Header information contains:
+		</p>
+		<ul>
+			<li>
+				FileType (int)<br/>
+				0 &rarr; Hazard Curve File<br/>
+				1 &rarr; Ground Motion File
+			</li>
+			<li>Minimum Latitude (double)</li>
+			<li>Maximum Latitude (double)</li>
+			<li>Minimum Longitude (double)</li>
+			<li>Maximum Longitude (double)</li>
+			<li>
+				Grid Spacing (double)<br/>
 				The decimal degree increment between adjacent latitudes/longitudes
-			</span>
-		</li>
-		<li>
-			<label>Num Curve Points (int)</label>
-			<span>
+			</li>
+			<li>
+				Num Curve Points (int)<br/>
 				The number of points per hazard curve. This value is only present
-				in hazard curve files.
-			</span>
-		</li>
-	</ul>
-	<p>
-		For convenience we provide sample binary file readers for potential 
-		developers to examine the source and get a better idea of how to read the
-		file format. <a href="PRParser.jar" >Download our reader</a> (requires 
-		<a target="_blank" href="http://www.java.com">Java</a>&trade;). To use 
-		this application, open a command line terminal and type: <code>java -jar 
-		PRParser.jar &lt;datafile&gt;</code>.
+					in hazard curve files.
+			</li>
+		</ul>
+		<p>
+			For convenience we provide sample binary file readers for potential
+			developers to examine the source and get a better idea of how to read the
+			file format. <a href="PRParser.jar" >Download our reader</a> (requires
+			<a target="_blank" href="http://www.java.com">Java</a>&trade;). To use
+			this application, open a command line terminal and type: <code>java -jar
+			PRParser.jar &lt;datafile&gt;</code>.
+	</div>
 </div>
-<script type="text/javascript">/* <![CDATA[ */
-$(document).ready(function(_event) {
-	$('#tbldownloads').tablesorter({
-		headers:{
-			3:{sorter:false}
-		},
-		sortList:[[0,0],[2,0],[1,0]]
-	});
-});
-/* ]]> */</script>
--- src/htdocs/hazards/hazmaps/prvi/2003/maps/index.php
+++ src/htdocs/hazards/hazmaps/prvi/2003/maps/index.php
@@ -1,101 +1,87 @@
 <?php
-	if (!isset($TEMPLATE)) {
-		$TITLE = 'Maps';
-		$STYLES = "
-			#tblfiles td, #tblfiles th { color: #333333; padding:3px 8px; }
-			#tblfiles th { font-weight:bold; border-bottom:1px solid #333333; }
-			#tblfiles th.header {cursor:pointer;}
-			#tblfiles td.downloads {font-size:.9em;}
-			.column.four img {float:left;}
-		";
-		$WIDGETS = 'jquery';
-		$SCRIPTS = '/template/js/jquery.tablesorter.js';
-
-		function fileMarkup($file) {
-			return sprintf('<tr><td class="sa">%s</td><td class="pe">%s</td>' .
-				'<td class="downloads">%s</td></tr>', getSa($file), getPe($file),
-				getDownloads($file)
-			);
-		}
-		function getSa($file) {
-			if (strpos($file, '0p0')) {
-				return 'Peak Ground Acceleration';
-			} else if (strpos($file, '0p2')) {
-				return '0.2 second SA (5 Hz)';
-			} else if (strpos($file, '0p3')) {
-				return '0.3 second SA (3.33 Hz)';
-			} else if (strpos($file, '1p0')) {
-				return '1.0 second SA (1.0 Hz)';
-			} else {
-				return 'Unknown';
-			}
-		}
-		function getPe($file) {
-			if (strpos($file, '2p50')) {
-				return '2&#37; in 50 years';
-			} else if (strpos($file, '10p50')) {
-				return '10&#37; in 50 years';
-			} else {
-				return 'Unknown';
-			}
-		}
-		function getDownloads($file) {
-			$png = $file;
-			$pdf = str_replace('.png', '.pdf', $file);
-			$dir = str_replace("\n", '', `pwd`);
-			return sprintf('[ <a href="%s" title="%s">PNG</a> | <a href="%s" ' .
-				'title="%s">PDF</a> ]', $png, simpleFileSize("${dir}/${png}"),
-				$pdf, simpleFileSize("${dir}/${png}")
-			);
-		}
-
-		$files = glob('images/*.png');
-		include_once $_SERVER['DOCUMENT_ROOT'] . '/template/template.inc.php';
-	}
+      //	Author:	Tiffany Kalin
+      //	Contact:
+      //	Last	modified:	09/21/2016
+      if	(!isset($TEMPLATE))	{
+      $TITLE	=	'Maps';
+      $NAVIGATION	= true;
+      include	'template.inc.php';
+      }
 ?>
-
-<div class="column six">
-	<?php if (!empty($files)) { /* We have files to show, show them. */ ?>
-	<h2>Mapped Ground Motion Hazard Values</h2>
-	<table cellpadding="0" cellspacing="0" border="0" id="tblfiles">
-		<thead>
-			<tr>
-				<th scope="col" class="sa">Spectral Acceleration</th>
-				<th scope="col" class="pe">Probability of Exceedance</th>
-				<th scope="col" class="downloads">File Format</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach($files as $file) { print fileMarkup($file); } ?>
-		</tbody>
-	</table>
-	<?php } else { /* No files to show. Say so. */ ?>
-	<p class="error">There are currently no maps available for download.</p>
-	<?php } /* END: if(!empty($files)) */ ?>
+<div class="row">
+	<div class="column two-of-three">
+		<h2>Mapped Ground Motion Hazard Values</h2>
+		<table>
+			<thead>
+				<tr>
+					<th>Spectral Acceleration</th>
+					<th>Probability of Exceedance</th>
+					<th>File Format</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>0.2 second SA (5 Hz)</td>
+					<td>10% in 50 years</td>
+					<td>[ <a href="images/PuertoRico_2003_0p2s_10p50.png" title="33 kB">PNG</a> | <a href="images/PuertoRico_2003_0p2s_10p50.pdf" title="33 kB">PDF</a> ]</td>
+				</tr>
+				<tr>
+					<td>0.3 second SA (3.33 Hz)</td>
+					<td>10% in 50 years</td>
+					<td>[ <a href="images/PuertoRico_2003_0p3s_10p50.png" title="32 kB">PNG</a> | <a href="images/PuertoRico_2003_0p3s_10p50.pdf" title="32 kB">PDF</a> ]</td>
+				</tr>
+				<tr>
+					<td>1.0 second SA (1.0 Hz)</td>
+					<td>10% in 50 years</td>
+					<td>[ <a href="images/PuertoRico_2003_1p0s_10p50.png" title="30 kB">PNG</a> | <a href="images/PuertoRico_2003_1p0s_10p50.pdf" title="30 kB">PDF</a> ]</td>
+				</tr>
+				<tr>
+					<td>Peak Ground Acceleration</td>
+					<td>10% in 50 years</td>
+					<td>[ <a href="images/PuertoRico_2003_0p0s_10p50.png" title="31 kB">PNG</a> | <a href="images/PuertoRico_2003_0p0s_10p50.pdf" title="31 kB">PDF</a> ]</td>
+				</tr>
+				<tr>
+					<td>0.2 second SA (5 Hz)</td>
+					<td>2% in 50 years</td>
+					<td>[ <a href="images/PuertoRico_2003_0p2s_2p50.png" title="32 kB">PNG</a> | <a href="images/PuertoRico_2003_0p2s_2p50.pdf" title="32 kB">PDF</a> ]</td>
+				</tr>
+				<tr>
+					<td>0.3 second SA (3.33 Hz)</td>
+					<td>2% in 50 years</td>
+					<td>[ <a href="images/PuertoRico_2003_0p3s_2p50.png" title="32 kB">PNG</a> | <a href="images/PuertoRico_2003_0p3s_2p50.pdf" title="32 kB">PDF</a> ]</td>
+				</tr>
+				<tr>
+					<td>1.0 second SA (1.0 Hz)</td>
+					<td>2% in 50 years</td>
+					<td>[ <a href="images/PuertoRico_2003_1p0s_2p50.png" title="32 kB">PNG</a> | <a href="images/PuertoRico_2003_1p0s_2p50.pdf" title="32 kB">PDF</a> ]</td>
+				</tr>
+				<tr>
+					<td>Peak Ground Acceleration</td>
+					<td>2% in 50 years</td>
+					<td>[ <a href="images/PuertoRico_2003_0p0s_2p50.png" title="33 kB">PNG</a> | <a href="images/PuertoRico_2003_0p0s_2p50.pdf" title="33 kB">PDF</a> ]</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="column one-of-three">
+		<h2>Select Hazard Curves</h2>
+		<a href="curves/sanjuan.pdf" title="Download full size San Juan">
+			<img src="curves/sanjuan.png" alt="San Juan"/>
+		</a>
+		<a href="curves/mayaguez.pdf" title="Download full size Mayaguez">
+			<img src="curves/mayaguez.png" alt="Mayaguez"/>
+		</a>
+		<a href="curves/ponce.pdf" title="Download full size Ponce">
+			<img src="curves/ponce.png" alt="Ponce"/>
+		</a>
+		<a href="curves/charlotteamalie.pdf" title="Download full size Charlotte Amalie">
+			<img src="curves/charlotteamalie.png" alt="Charlotte Amalie"/>
+		</a>
+		<a href="curves/christiansted.pdf" title="Download full size Christiansted">
+			<img src="curves/christiansted.png" alt="Christiansted"/>
+		</a>
+		<a href="curves/swpr.pdf" title="Download full size Southwest Puerto Rico">
+			<img src="curves/swpr.png" alt="Southwest Puerto Rico"/>
+		</a>
+	</div>
 </div>
-<div class="column four">
-	<h2>Select Hazard Curves</h2>
-
-	<a href="curves/sanjuan.pdf" title="Download full size San Juan"
-		><img src="curves/sanjuan.png" alt="San Juan"/></a>
-	<a href="curves/mayaguez.pdf" title="Download full size Mayaguez"
-		><img src="curves/mayaguez.png" alt="Mayaguez"/></a>
-	<a href="curves/ponce.pdf" title="Download full size Ponce"
-		><img src="curves/ponce.png" alt="Ponce"/></a>
-	<a href="curves/charlotteamalie.pdf" 
-		title="Download full size Charlotte Amalie"
-		><img src="curves/charlotteamalie.png" alt="Charlotte Amalie"/></a>
-	<a href="curves/christiansted.pdf" title="Download full size Christiansted"
-		><img src="curves/christiansted.png" alt="Christiansted"/></a>
-	<a href="curves/swpr.pdf" title="Download full size Southwest Puerto Rico"
-		><img src="curves/swpr.png" alt="Southwest Puerto Rico"/></a>
-
-</div>
-
-<script type="text/javascript">/* <![CDATA[ */
-$(document).ready(function(_event) {
-	$('#tblfiles').tablesorter({
-		headers:{2:{sorter:false}},sortList:[[1,0],[0,0]]
-	});
-});
-/* ]]> */</script>
