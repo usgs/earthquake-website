c911a2468b4306a19c1839201a3ed3ba0a3bb5dd
lisa-usgs <lisa@usgs.gov>
2016-09-27 21:23:36 -0600

    brawley seismic zone activity statement

---
+++ src/htdocs/misc/2016-09-27.php
@@ -0,0 +1,35 @@
+<?php
+  // Author: Lisa Wald
+  // Contact: Donyelle Davis
+    if (!isset($TEMPLATE)) {
+      $TITLE = 'USGS Summary for Swarm Activity Near Bombay Beach';
+      $NAVIGATION = true;
+      include 'template.inc.php';
+    }
+?>
+
+<figure class="right">
+  <img src="images/2016-09-27.gif">
+  <figcaption>
+    Earthquakes in the Brawley seismic zone as of the evening of 09/27/2016.
+  </figcaption>
+</figure>
+
+<p>
+  An earthquake swarm near Bombay Beach, California, started on 2016 Sept. 26 at 4:03 am (PDT)  in the Brawley Seismic Zone, which lies near the southern terminus of the San Andreas Fault.
+</p>
+<p>
+  The swarm includes 142 events so far (as of 26 Sep 2016, 11:31PM PDT) in the magnitude range M1.4 to M4.3. Relocations of these events show that they are occurring in the depth range 4 to 9 km.
+</p>
+<p>
+  The earthquakes are occurring near a set of north-northeast trending cross-faults in the Salton Sea. The cross-faults are part of a fault network that connect the southernmost end of the San Andreas fault with the Imperial fault. Some of the cross-faults are oriented such that they add stress to the San Andreas fault and the San Jacinto fault system when they rupture in small earthquakes like those in the ongoing swarm.
+</p>
+
+<h2>Earthquake Swarms and Probabilities</h2>
+
+<p>
+  Swarm-like activity in this region has occurred in the past, so this week’s activity, in and of itself, is not necessarily cause for alarm.
+</p>
+<p>
+  Preliminary calculations indicate that, as of 10:00 am (PDT) Sept. 27, 2016, there is a 0.03%-1% chance (1 in 3000 to 1 in 100) of a magnitude 7 or greater earthquake being triggered on the Southern San Andreas fault within the next next seven days through October 4, with the likelihood decreasing over time. This probability range is estimated using several models developed in California to assess foreshock/aftershock probabilities.
+</p>
