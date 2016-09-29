0ae988bb57a77d70d7902fdc8e7c5bbf9b90b989
Eric Martinez <emartinez@usgs.gov>
2016-09-29 08:28:11 -0600

    Added a config for telemetry web service endpoint.

--- src/htdocs/monitoring/operations/conf/config.inc.php
+++ src/htdocs/monitoring/operations/conf/config.inc.php
@@ -32,3 +32,5 @@
   // $NETOPS_HELIPLOT_URL . '/{station_code}_24hr.png' --> small heliplot image
   // $NETOPS_HELIPLOT_URL . '/{station_code}_24hr.html' --> heliplot markup
   $NETOPS_HELIPLOT_URL = $NETOPS_DATA_BASEURL . '/Seismic_Data/telemetry_data';
+
+  $TELEMETRY_URL = $NETOPS_WEBSITE_BASEURL . '/telemetry.json';
