8da75d47a0dd44f2300076460263fea6f9e01257
lisa-usgs <lisa@usgs.gov>
2016-09-30 09:40:14 -0600

    updates to nepec reports and meetings pages
    Former-commit-id: 7920bbcc751808c228c6f8b5348cf111e73db05c

--- src/htdocs/aboutus/nepec/reports/index.php
+++ src/htdocs/aboutus/nepec/reports/index.php
@@ -1,7 +1,6 @@
 <?php
   // Author: Lisa Wald
   // Contact: Bill Leith
-  // Last modified: 06/21/2016
   if (!isset($TEMPLATE)) {
   $TITLE = 'NEPEC Reports';
   $NAVIGATION = true;
@@ -10,27 +9,39 @@
 ?>
 
 <ul class='no-style separator'>
-	<li><a href="NEPEC_advice_OEF_Dec2015.pdf">
-		Recommendations from NEPEC on USGS research and development activities in the area of operational earthquake forecasting.</a>
-	</li>
-	<p></p>
 
-	<li><a href="NEPEC_Chair_letter_25Sept2014_scanned.pdf">
-		Letter to USGS Director with summary of recent NEPEC activities and recommendations</a>
-	</li>
-	<p></p>
-
-	<li><a href="NEPEC_letter_WGCEP_Jan07.pdf">Advice from NEPEC on the development of a Uniform California Earthquake Rupture Forecast (UCERF)</a> by the Working Group on California Earthquake Probabilities (WGCEP) and NEPEC's role in review of the UCERF report.
-	</li>
-	<p></p>
-
-	<li><a href="NEPEC_advice_CSEP_May09.pdf">
-		Advice from NEPEC on the Collaboratory for Study of Earthquake Predictability (CSEP)</a>, its relevance for USGS, and potential avenues of future development.</a>
-	</li>
-	<p></p>
+  <li>
+    <h4>September 2016</h4>
+    <a href="NEPEC_prediction_statement_Sep2016.pdf">Recommendations from NEPEC on proper procedures for posing earthquake predictions and testing earthquake prediction methods</a>
+  </li>
 
 	<li>
-		<a href="NEPEC_NMSZ_expert_panel_report.pdf">Report of the Independent Expert Panel on New Madrid Seismic Zone Earthquake Hazards</a> as approved by NEPEC on April 16, 2011.<br />
-		<a href="NEPEC_LettertoMcNutt4-18-11.pdf">Cover letter</a> accompanying report from NEPEC Chair Terry Tullis to
+    <h4>December 2015</h4>
+    <a href="NEPEC_advice_OEF_Dec2015.pdf">
+		Recommendations from NEPEC on USGS research and development activities in the area of operational earthquake forecasting</a>
 	</li>
+
+	<li>
+    <h4>September 2014</h4>
+    <a href="NEPEC_Chair_letter_25Sept2014_scanned.pdf">
+		Letter to USGS Director with summary of recent NEPEC activities and recommendations</a>
+	</li>
+
+  <li>
+    <h4>April 2011</h4>
+    <a href="NEPEC_NMSZ_expert_panel_report.pdf">Report of the Independent Expert Panel on New Madrid Seismic Zone Earthquake Hazards</a><br />
+    <a href="NEPEC_LettertoMcNutt4-18-11.pdf">Cover letter accompanying report</a> from NEPEC Chair Terry Tullis
+  </li>
+
+  <li>
+    <h4>May 2009</h4>
+    <a href="NEPEC_advice_CSEP_May09.pdf">
+		Advice from NEPEC on the Collaboratory for Study of Earthquake Predictability (CSEP)</a>, its relevance for USGS, and potential avenues of future development</a>
+	</li>
+
+	<li>
+    <h4>January 2007</h4>
+    <a href="NEPEC_letter_WGCEP_Jan07.pdf">Advice from NEPEC on the development of a Uniform California Earthquake Rupture Forecast (UCERF)</a> by the Working Group on California Earthquake Probabilities (WGCEP) and NEPEC's role in review of the UCERF report
+	</li>
+
 </ul>
