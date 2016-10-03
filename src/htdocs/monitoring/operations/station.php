a52a89f90c362496900af643aeea36c74905a569
Eric Martinez <emartinez@usgs.gov>
2016-10-03 16:09:36 -0600

    Allowing numbers as well.

--- src/htdocs/monitoring/operations/station.php
+++ src/htdocs/monitoring/operations/station.php
@@ -6,8 +6,8 @@
   $station = param('station');
 
   if ($network != null && $station != null &&
-      preg_match('/^[a-zA-Z]+$/', $network) &&
-      preg_match('/^[a-zA-Z]+$/', $station)) {
+      preg_match('/^[a-zA-Z0-9]+$/', $network) &&
+      preg_match('/^[a-zA-Z0-9]+$/', $station)) {
     header("Location: ${NETOPS_BASEPATH}/stations/${network}/${station}/");
   } else {
     header('HTTP/1.0 404 Not Found');
