8953c47820aef9ac92bc389fc638957d1c503e6c
jdbrown-usgs <jdbrown@usgs.gov>
2016-10-07 07:27:36 -0600

    copied files over and replaced tabs with spaces
    Former-commit-id: 82ae58885c11bd72401986ee3c2068f7f5490b0a

---
+++ src/htdocs/hazards/designmaps/datasets/codes/irc_2000.inc.php
@@ -0,0 +1,20 @@
+<section>
+  <h3 id="irc-2000_2003">2000/03 IRC</h3>
+  <div>
+    <section>
+      <h4>Conterminous US</h4>
+      <ul>
+        <li>
+          Mapped Seismic Design Categories (mapped values)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/2000_2003_IRC-Design_Categories.kmz"
+            title="1001 kB">KML</a> | <a
+            href="<?php print $base; ?>/geo/2000_2003_IRC-Design_Categories.shape.zip"
+            title="627 kB">ArcGIS&trade; Shapefiles</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+  </div>
+</section>
---
+++ src/htdocs/hazards/designmaps/datasets/codes/usgs_1996.inc.php
@@ -0,0 +1,37 @@
+<section>
+  <h3 id="usgs-1996">ASCE 7-98/02, 2000/03 IBC</h3>
+  <div>
+    <section>
+      <h4>Conterminous US</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.1&deg; gridded data,
+          24.6&deg;–50&deg; N, 125&deg;–65&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/1997-US-MCE-R1a.txt"
+            title="5 Mb">Text</a> | <a
+            href="<?php print $base; ?>/geo/1997-US-MCE-R1a.rnd"
+            title="3 Mb">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h4>California/Nevada</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.01&deg; gridded data,
+          32&deg;–43&deg; N, 125&deg;–111&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/1997-CANV-MCE-R2.txt"
+            title="60 Mb">Text</a> | <a
+            href="<?php print $base; ?>/geo/1997-CANV-MCE-R2.rnd"
+            title="35 Mb">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+  </div>
+</section>
