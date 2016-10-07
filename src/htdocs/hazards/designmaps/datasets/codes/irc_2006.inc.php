82ae58885c11bd72401986ee3c2068f7f5490b0a
jdbrown-usgs <jdbrown@usgs.gov>
2016-10-07 07:27:36 -0600

    copied files over and replaced tabs with spaces

---
+++ src/htdocs/hazards/designmaps/datasets/codes/irc_2006.inc.php
@@ -0,0 +1,20 @@
+<section>
+  <h3 id="irc-2006_2009">2006/09 IRC</h3>
+  <div>
+    <section>
+      <h4>Conterminous US</h4>
+      <ul>
+        <li>
+          Mapped Seismic Design Categories (mapped values)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/2006_2009_IRC-Design_Categories.kmz"
+            title="1 Mb">KML</a> | <a
+            href="<?php print $base; ?>/geo/2006_2009_IRC-Design_Categories.shape.zip"
+            title="1 Mb">ArcGIS&trade; Shapefiles</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+  </div>
+</section>
---
+++ src/htdocs/hazards/designmaps/datasets/codes/usgs_2002.inc.php
@@ -0,0 +1,134 @@
+<section>
+  <h3 id="usgs-2002">ASCE 7-05, 2006/09 IBC</h3>
+  <div>
+    <section>
+      <h4>Conterminous US</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.05&deg; gridded data,
+          24.6&deg;-50&deg; N, 125&deg;-65&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/2003-US-MCE-R1a.txt"
+            title="23 Mb">Text</a>
+            | <a
+            href="<?php print $base; ?>/geo/2003-US-MCE-R1a.rnd"
+            title="13 Mb">Binary</a>]
+          </span>
+        </li>
+    </ul>
+    </section>
+    <section>
+      <h4>Alaska</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.1&deg; gridded data,
+          50&deg;-72&deg; N, 170&deg; E - 130&deg; W)
+          <span class="download-links">
+            [Download as <a
+              href="<?php print $base; ?>/geo/1997-AK-MCE-R1a.txt"
+              title="5 Mb">Text</a> | <a
+              href="<?php print $base; ?>/geo/1997-AK-MCE-R1a.rnd"
+              title="3 Mb">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h4>California/Nevada</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.01&deg; gridded data,
+          32&deg;-42&deg; N, 125&deg;-115&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/2003-CANV-MCE-R1a.txt"
+            title="39 Mb">Text</a> | <a
+            href="<?php print $base; ?>/geo/2003-CANV-MCE-R1a.rnd"
+            title="22 Mb">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h4>Hawaii</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.02&deg; gridded data,
+          18&deg;-23&deg; N, 161&deg;-154&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/1998-HI-MCE-R1a.txt"
+            title="3 Mb">Text</a> | <a
+            href="<?php print $base; ?>/geo/1998-HI-MCE-R1a.rnd"
+            title="2 Mb">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h4>Intermountain West</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.01&deg; gridded data,
+          40&deg;-45&deg; N, 112&deg;-110&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/2003-SLC-MCE-Ra.txt"
+            title="5 Mb">Text</a> | <a
+            href="<?php print $base; ?>/geo/2003-SLC-MCE-Ra.rnd"
+            title="3 Mb">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h4>New Madrid Seismic Zone</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.01&deg; gridded data,
+          35&deg;-38&deg; N, 92&deg;-88&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/2003-CEUS-MCE-R1a.txt"
+            title="4 Mb">Text</a> | <a
+            href="<?php print $base; ?>/geo/2003-CEUS-MCE-R1a.rnd"
+            title="2 Mb">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h4>Coastal Pacific Northwest</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.01&deg; gridded data,
+          41&deg;-49&deg; N, 125&deg;-123&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/2003-PacNW-MCE-R1a.txt"
+            title="6 Mb">Text</a> | <a
+            href="<?php print $base; ?>/geo/2003-PacNW-MCE-R1a.rnd"
+            title="3 Mb">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h4>Puerto Rico/U.S. Virgin Islands</h4>
+      <ul>
+        <li>
+          0.2s, 1.0s MCE Ground Motion (0.05&deg; gridded data,
+          16&deg;-21&deg; N, 70&deg;-62&deg; W)
+          <span class="download-links">
+            [Download as <a
+            href="<?php print $base; ?>/geo/2003-PRVI-MCE-R1a.txt"
+            title="608 kB">Text</a> | <a
+            href="<?php print $base; ?>/geo/2003-PRVI-MCE-R1a.rnd"
+            title="381 kB">Binary</a>]
+          </span>
+        </li>
+      </ul>
+    </section>
+  </div>
+</section>
