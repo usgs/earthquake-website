bff26279dd34c71b0c29bb5d58feec3e7eac77bf
jdbrown-usgs <jdbrown@usgs.gov>
2016-10-11 13:58:44 -0600

    updated layout so that it works better in a responsive enviroment
    Former-commit-id: b11570d484b9364b6b870375a96255fc710ecc0c

--- src/htdocs/hazards/designmaps/datasets/codes/usgs_1996.inc.php
+++ src/htdocs/hazards/designmaps/datasets/codes/usgs_1996.inc.php
@@ -2,34 +2,39 @@
   <h3 id="usgs-1996">ASCE 7-98/02, 2000/03 IBC</h3>
   <div>
     <section>
-      <h4>Conterminous US</h4>
-      <ul>
+      <ul class="no-style">
         <li>
-          0.2s, 1.0s MCE Ground Motion (0.1&deg; gridded data,
-          24.6&deg;–50&deg; N, 125&deg;–65&deg; W)
-          <span class="download-links">
-            [Download as <a
-            href="<?php print $base; ?>/geo/1997-US-MCE-R1a.txt"
-            title="5 Mb">Text</a> | <a
-            href="<?php print $base; ?>/geo/1997-US-MCE-R1a.rnd"
-            title="3 Mb">Binary</a>]
+          <span class="download-title">Conterminous US</span>
+          <span class="download-text">
+            0.2s, 1.0s MCE Ground Motion (0.1&deg; gridded data,
+            24.6&deg;–50&deg; N, 125&deg;–65&deg; W)
           </span>
+          <ul class="download-format">
+            <li>
+              [Download as <a
+              href="<?php print $base; ?>/geo/1997-US-MCE-R1a.txt"
+              title="5 Mb">Text</a> | <a
+              href="<?php print $base; ?>/geo/1997-US-MCE-R1a.rnd"
+              title="3 Mb">Binary</a>]
+            </li>
+          </ul>
         </li>
-      </ul>
-    </section>
-    <section>
-      <h4>California/Nevada</h4>
-      <ul>
+
         <li>
-          0.2s, 1.0s MCE Ground Motion (0.01&deg; gridded data,
-          32&deg;–43&deg; N, 125&deg;–111&deg; W)
-          <span class="download-links">
-            [Download as <a
-            href="<?php print $base; ?>/geo/1997-CANV-MCE-R2.txt"
-            title="60 Mb">Text</a> | <a
-            href="<?php print $base; ?>/geo/1997-CANV-MCE-R2.rnd"
-            title="35 Mb">Binary</a>]
+          <span class="download-title">California/Nevada</span>
+          <span class="download-text">
+            0.2s, 1.0s MCE Ground Motion (0.01&deg; gridded data,
+            32&deg;–43&deg; N, 125&deg;–111&deg; W)
           </span>
+          <ul class="download-format">
+            <li>
+              [Download as <a
+              href="<?php print $base; ?>/geo/1997-CANV-MCE-R2.txt"
+              title="60 Mb">Text</a> | <a
+              href="<?php print $base; ?>/geo/1997-CANV-MCE-R2.rnd"
+              title="35 Mb">Binary</a>]
+            </li>
+          </ul>
         </li>
       </ul>
     </section>
