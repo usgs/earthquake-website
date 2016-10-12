b0b607cbdffc391f895f8ea95bd706f180d516ab
tkalin-usgs <tkalin@usgs.gov>
2016-10-12 10:01:26 -0600

    hayward_paleo, creep.php, nmsz pages

--- src/htdocs/learn/topics/hayward_paleo/_navigation.inc.php
+++ src/htdocs/learn/topics/hayward_paleo/_navigation.inc.php
@@ -1,14 +1,14 @@
 <?php
   $section = "/learn";
 
-  /*echo "<a href='/learn/' class='up-one-level'>Learn</a>";
+  echo "<a href='/learn/' class='up-one-level'>Learn</a>";
 
-  print navItem(,"Paleoseismology and the Hayward Fault");
+  print navItem("/learn/topics/hayward_paleo/index.php","Paleoseismology and the Hayward Fault");
 
-  print navItem(,"New Technology for Tracking Ancient Earthquakes");
+  print navItem("/learn/topics/hayward_paleo/newtech.php","New Technology for Tracking Ancient Earthquakes");
 
-  print navItem(,"Tule Pond Paleoseismology Photo Gallery");
+  print navItem("/learn/topics/hayward_paleo/slideshow","Tule Pond Paleoseismology Photo Gallery");
 
-  print	navItem(,"Additional Resources");*/
+  print	navItem("/learn/topics/hayward_paleo/references.php","Additional Resources");
 
 ?>
--- src/htdocs/learn/topics/hayward_paleo/index.php
+++ src/htdocs/learn/topics/hayward_paleo/index.php
@@ -14,20 +14,39 @@
 </p>
 
 <p>
-  Most trenches across the Hayward fault were excavated to meet the requirements of the <a href="http://www.consrv.ca.gov/CGS/rghm/ap/index.htm">Alquist-Priolo Earthquake Fault Zoning Act</a>, to insure that structures are not built over active fault traces. Other trenches have been used by scientists to learn about the fault's capability as a source of earthquakes. Earthquake geologists probe in to the fault's past behavior using the tools of paleoseismology. Using radiocarbon analysis to date these past earthquakes,  scientists have shown that these large earthquakes occur roughly every 100 to 200 years on the Hayward fault.
+  Most trenches across the Hayward fault were excavated to meet the requirements of the <a href="http://www.conservation.ca.gov/cgs/rghm/ap">Alquist-Priolo Earthquake Fault Zoning Act</a>, to insure that structures are not built over active fault traces. Other trenches have been used by scientists to learn about the fault's capability as a source of earthquakes. Earthquake geologists probe in to the fault's past behavior using the tools of paleoseismology. Using radiocarbon analysis to date these past earthquakes,  scientists have shown that these large earthquakes occur roughly every 100 to 200 years on the Hayward fault.
 </p>
 
 <p>
-  On the <a href="map.html"> Hayward fault</a>, perhaps the best and most accessible trench site is at <a href="topo_map.html" rel="ibox&amp;width=335&amp;height=610" width=330,height=494>Tule Pond</a>, also known as Tyson's Lagoon, just south of the Fremont BART (Bay Area Rapid Transit) station in Freemont, California. At this site, the 1868 M<sub>w</sub>6.8 earthquake is revealed in the trench by intensely-contorted, near-surface layers of silty sand and clay. A series of photos</font> take you through <a href="slideshow/index.php#dig"> digging of the trench</a> at Tule Pond.
+  On the Hayward fault, perhaps the best and most accessible trench site is at Tule Pond, also known as Tyson's Lagoon, just south of the Fremont BART (Bay Area Rapid Transit) station in Freemont, California. At this site, the 1868 M<sub>w</sub>6.8 earthquake is revealed in the trench by intensely-contorted, near-surface layers of silty sand and clay. A series of photos</font> take you through <a href="slideshow/index.php#dig"> digging of the trench</a> at Tule Pond.
 </p>
 
 <figure>
-  <img src="images/2010Final-12Evts.jpg " alt="recurrence interval" width="784" height="552" />
+  <img src="images/2010Final-12Evts.jpg" alt="recurrence interval"/>
 </figure>
 
+<div class="row">
+  <div class="column one-of-two">
+    <figure class="left" style="max-width:299px">
+      <img src="images/topo_map.gif" alt="Topo map"/>
+      <figcaption>
+        Aerial photograph, Tule Pond (Tyson Lagoon), Fremont, CA.
+      </figcaption>
+    </figure>
+  </div>
+  <div class="column one-of-two">
+    <figure class="left" style="max-width:494px">
+      <img src="images/map.gif" alt=""/>
+      <img src="images/map_key.gif" alt=""/>
+      <figcaption>
+        Location of trench site and other points along the Hayward Fault. Currently known extent of 1868 surface rupture shown as green highlight band; dashed where subsurface extent of rupture is inferred. Starts indicate localities discussed in <a href="http://pubs.usgs.gov/of/1999/0318/">Open-File report 99-318: Timing of Paleoearthquakes on the Hayward Fault </a>. Creeping trance extends at least 68 km from P, Point Pinole, to AC, Agua Caliente Creek. Trenching sites: MV, Mira Vista (Hayward Fault Paleoearthquake Group, 1997); MT, Montclair (Lienkamper et al., 1995); MH, Masonic Home (Lienkamper and Borchardt, 1996); TP, Tule Pond (Williams et. al., 1993); Other localities: RM, Rocky Mound key triangulation point of Yu and Segall, (1996); CR,, near Calaveras Reservoir, the branching point of Northern Calaveras fault from fast-creeping Southern Calaveras fault. (Working Group on Northern California Earthquake Potential, 1996.)
+      </figcaption>
+    </figure>
+  </div>
+</div>
 <!-- NAVIGATION at bottom for ARTICLE -->
 <p>
-  <a class='next' href="weknow.php">
+  <a class='next' href="newtech.php">
     <strong>NEXT</strong>
   <span class='material-icons next'>&#xe5c8;</span>
   </a>
