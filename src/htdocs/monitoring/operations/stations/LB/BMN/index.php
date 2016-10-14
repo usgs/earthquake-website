6b217a5877e16f470ec8a537e69a0ab9204d74e4
Austin A Holland <aaholland@usgs.gov>
2016-10-14 15:06:23 -0600

    BMN removed no longer an ANSS BB site
    Former-commit-id: 037820652da81e2ca4e7aea44af82573fa00c71b

--- src/htdocs/monitoring/operations/stations/LB/BMN/index.php
+++
@@ -1,6 +0,0 @@
-<?php include '../../../_station.php'; ?>
-
-<div class="site-geology">
-  <h3>Site Geology</h3>
-  <p>Located in Miocene aged Bates Mountain Tuff, rich in sanadine, quartz, and plagioclase. Roughly 24 million years old.</p>
-</div>
