77e74b55d10df38e0a3538a7e35cd5062f2dd629
tkalin-usgs <tkalin@usgs.gov>
2016-10-20 14:30:35 -0600

    Initial hazards/urban work
    Former-commit-id: f0cf04748e792fa5ec9d777c90420a9238fa2c77

---
+++ src/htdocs/hazards/urban/index.php
@@ -0,0 +1,181 @@
+<?php
+      //	Author:	Tiffany Kalin
+      //	Contact:
+      //	Last	modified:	10/13/2016
+      if	(!isset($TEMPLATE))	{
+      $TITLE	=	'Urban Seismic Hazards';
+      $NAVIGATION	= true;
+      include	'template.inc.php';
+      }
+?>
+
+<h3>Urban Regions :</h3>
+<nav class="jumplist">
+	<a href="#ca">California</a>
+	<a href="#ceus">Central and Eastern US</a>
+	<a href="#pacnw">Pacific Northwest</a>
+</nav>
+
+<h2 id="ca">California</h2>
+<ul class="no-style linklist">
+  <li>
+    <a href="/regional/nca/alameda/">
+      <h4>Liquefaction Hazard in Oakland, CA</h4>
+      <img src="images/oakland.gif" alt=""/>
+    </a>
+    <p>
+      The liquefaction hazard map
+      predicts the approximate percentage of each designated area that will have
+      surface manifestations of liquefaction during an M7.1 earthquake on the
+      Hayward fault. An earthquake of this magnitude is expected if the whole
+      Hayward fault ruptures in a single event (Working Group on California
+      Earthquake Probabilities, 1999). This event dominates the deaggregated
+      hazard near the eastern shore of San Francisco Bay.
+    </p>
+  </li>
+  <li>
+    <a href="/regional/nca/soiltype/">
+      <h4>Liquefaction Hazard in Oakland, CA</h4>
+      <img src="images/soiltype.gif" alt=""/>
+    </a>
+    <p>
+      Soil Type and Shaking Hazard in the S.F. Bay Area', 'The type of soil at
+      a site affects the intensity of shaking there. Learn about this effect and
+      use interactive maps to look up the soil type at any site in the San
+      Francisco Bay Area.
+    </p>
+  </li>
+  <li>
+    <a href="http://geomaps.wr.usgs.gov/sfgeo/liquefaction/">
+      <h4>Liquefaction Susceptibility Map for San Francisco Bay Region</h4>
+      <img src="images/sfliquifaction.gif" alt=""/>
+    </a>
+    <p>
+      Shows the distribution of rock and sediment having
+      different vulnerabilities to liquefaction when shaken by earthquakes,
+      together with explanation of the process and examples of its effects.
+    </p>
+  </li>
+  <li>
+    <a href="http://www.abag.ca.gov/bayarea/eqmaps/">
+      <h4>San Francisco Bay Area Hazard Maps</h4>
+      <img src="images/abag.gif" alt=""/>
+    </a>
+    <p>
+      Association of Bay Area Governments
+      offers shaking maps, liquefaction maps, predictive transportation
+      disruption maps, and more.
+    </p>
+  </li>
+  <li>
+    <a href="http://www.conservation.ca.gov/cgs/shzp/Pages/Index.aspx">
+      <h4>California Seismic Hazard Maps</h4>
+      <img src="images/ca.gif" alt=""/>
+    </a>
+    <p>
+      In 1880 the California
+      Legistlature established the State Mining Bureau which has evolved during
+      its 127 years of continuous service into the modern California Geological
+      Survey (CGS).
+    </p>
+  </li>
+</ul>
+
+<h2 id="ceus">Central and Eastern US</h2>
+<ul class="no-style linklist">
+  <li>
+    <a href="images/ceus-seis-haz.pdf">
+      <h4>Central and Eastern U.S. Hazard and Seismicity Map</h4>
+      <img src="images/ceus-seis-haz.gif" alt=""/>
+    </a>
+    <p>
+      This map shows earthquakes (circles) greater than magnitude 3.0 since 1974
+      plotted on the <a href="/hazards/hazmaps/conterminous/">2014 USGS National
+      Seismic Hazard Map for the central and eastern United States</a>. Warmer
+      colors on this map indicate areas of higher hazard. Larger earthquakes are
+      represented by larger circles. (PDF)
+    </p>
+  </li>
+  <li>
+    <a href="memphis/">
+      <h4>Memphis, Shelby County Earthquake Hazards Mapping</h4>
+      <img src="images/memphis.gif" alt=""/>
+    </a>
+    <p>
+      Memphis, Shelby County, Tennessee, is located where damaging earthquakes
+      are only moderately likely, but the consequences of earthquakes, mainly
+      from the New Madrid seismic zone, can be very high. This densely populated
+      urban area is built on a 1-kilometer-thick sequence of sediments deposited
+      in a trough known as the Mississippi embayment. This thick pile of
+      sediments significantly affects earthquake ground motions. We, the
+      authors, have generated a suite of seismic hazard maps for a
+      six-quadrangle area in Memphis, Shelby County, Tennessee, that accounts
+      for these effects. These maps and their derivative products represent the
+      collaborative efforts of the U.S. Geological Survey (USGS) and its
+      partners.
+    </p>
+  </li>
+  <li>
+    <a href="evansville.php">
+      <h4>Evansville Area Earthquake Hazards Mapping</h4>
+      <img src="images/evansville.gif" alt=""/>
+    </a>
+    <p>
+      In tri-state Evansville area of Indiana, Kentucky, and Illinois the USGS
+      is partnering with a local working group to produce urban earthquake
+      hazard maps for the community.
+    </p>
+  </li>
+  <li>
+    <a href="st_louis.php">
+      <h4>St. Louis Area Earthquake Hazards Mapping</h4>
+      <img src="images/stlouis.gif" alt=""/>
+    </a>
+    <p>
+      In the St. Louis area of Missouri and Illinois the USGS is partnering
+      with a local working group to produce urban earthquake hazard maps for the
+      community.
+    </p>
+  </li>
+</ul>
+
+<h2 id="pacnw">Pacific Northwest</h2>
+<ul class="no-style linklist">
+  <li>
+    <a href="seattle.php">
+      <h4>Seattle Urban Seismic Hazard Maps</h4>
+      <img src="images/seattle.gif" width="144px" alt=""/>
+    </a>
+    <p>
+      The USGS has produced a new series
+      of earthquake hazard maps for the City of Seattle. These &ldquo;urban
+      seismic hazard&rdquo; maps provide a much higher-resolution view of the
+      potential for strong earthquake shaking than previously available. This
+      new view is particularly important for Seattle, which sits atop a
+      sedimentary basin that strongly affects the patterns of earthquake ground
+      shaking and therefore, of potential damage.
+    </p>
+  </li>
+  <li>
+    <a href="http://geomaps.wr.usgs.gov/pacnw/lifeline/index.html">
+      <h4>Lifelines and earthquake hazards in the greater Seattle area</h4>
+      <img src="images/lifelines.gif" alt=""/>
+    </a>
+    <p>
+      A Pacific Northwest Urban Corridor Geologic Mapping
+      Project of the Western Earth Surface Processes Team.
+    </p>
+  </li>
+  <li>
+    <a href="http://geomaps.wr.usgs.gov/pacnw/map.html">
+      <h4>Pacific Northwest Urban Mapping &amp; Geologic Hazards</h4>
+      <img src="images/geomap.gif" alt=""/>
+    </a>
+    <p>
+      The urban corridor lies within the Puget-Willamtte lowland, a
+      seismically active forearc basin overlying the Cascadia subduction zone -
+      Pacific Northwest Urban Corridor Geologic Mapping Project of the Western
+      Earth Surface Processes Team
+    </p>
+  </li>
+</ul>
