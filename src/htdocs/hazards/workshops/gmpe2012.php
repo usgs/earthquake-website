47e1b4737cf06442e2b3c2eb079accdfb5e3dcc2
lisa-usgs <lisa@usgs.gov>
2016-10-21 14:19:15 -0600

    most workshop pages done

---
+++ src/htdocs/hazards/workshops/gmpe2012.php
@@ -0,0 +1,94 @@
+<?php
+// Author: Lisa Wald
+// Contact: Mark Peterson
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Ground Motion Prediction Equations (GMPEs) Workshop - 2012';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<h2>December 12-13, 2012</h2>
+
+<p><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/http://ihouse.berkeley.edu/">I-House, Berkeley, CA</a><br />
+Hosted by the Pacific Earthquake Engineering Research Center</p>
+
+<p>The links in the following agendas are to PDF files of participants that have granted permission to view their presentations. </p>
+
+<p><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/GMPE_Workshop_Agenda.pdf">Workshop Agenda (PDF)</a></p>
+
+<h3>December 12th</h3>
+
+<ul>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM01_Petersen.gmworkshop-berkeley-newsm.pdf">Introduction</a> - Mark Petersen</li>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM02_Rezaeian1_CEUS_GMPEs_20121207sm.pdf">CEUS GMPEs considered for 2014 maps</a> - Sanaz Rezaeian</li>
+	<li>Underlying parameters and issues in development of CEUS GMPEs:
+		<ul>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM03_Boore_path_stress_tradeoff_rotd50_v1.0(dmb)sm.pdf">Trade-off between Stress-Drop and Geometric-Spreading</a> - Dave Boore</li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM04_FrankelGeomSpreadingTalkDec2012sm.pdf">Geometric spreading from coda-normalized S-wave amplitudes</a> - Art Frankel</li>
+			<li>Regionalization and its effects on ground motions, Gulf Coast issues, and Geometric-Spreading from Virginia aftershocks - <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM05_Chapman-usgs-12-12-2012mccsm.pdf">Martin Chapman</a> &amp; <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM06_Mooney_USGS_CMPE_Dec_12_2012sm.pdf">Walter Mooney</a></li>
+			<li>Kappa, Conversion from hard rock to 760m/s - <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM07_Boore_conversion_from_vhr_to_bcsm.pdf">Dave Boore</a> &amp; <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM08_Abrahamson-2012_dec12_kappasm.pdf">Norm Abrahamson</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM09_Atkinson_t12_USGS_GMPEssm.pdf">GMPEs in Canadian National Hazard Maps</a> - Gail Atkinson</li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_AM10_WANG%20-USGS-GMPE2012sm.pdf">Constraints on GMPEs using Wenchuan data</a> - Zhenming Wang</li>
+			<li>Discussion</li>
+		</ul>
+	</li>
+	<li>Residual studies using NGA-East database of recorded ground motions:
+		<ul>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM01_Cramer_NGAEastUSGSworkshop2012-12-12residsm.pdf">Total residuals</a> - Chris Cramer</li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM02_Goulet_CEUS_ResidualsCompsm.pdf">Accounting for mixed-effects</a> - Christine Goulet</li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM03_Youngs-Overview_EPRI_(2004_2006)Review_12_12_2012sm.pdf"/>Overview of EPRI 2004.06 review project</a> - Bob Youngs</li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM04_Moschetti_GM_NSHMP_201212sm.pdf">Sensitivity of hazard maps to CEUS GMPEs</a> - Morgan Moschetti</li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM05_Petersen-Sensitivity%20for%20WUS%20gm%20models-2012sm.pdf">Discussion on weighting and other issues for CEUS GMPEs</a> - Mark Petersen</li>
+		</ul>
+	</li>
+	<li>Subduction Zone:
+		<ul>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM06_Petersen-GM-wkshop-Subductionsm.pdf">Introduction</a> - Mark Petersen</li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM07_Abrahamson_Dec_12_2012_BCHydroGMPEsm.pdf">BChydro GMPE</a> - Norm Abrahamson</li>
+			<li>Other GMPEs - Gail Atkinson | <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM08b_Youngs_etal_1997sm.pdf">Bob Youngs</a> | <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/WED_PM08c_Zhao_USGSWorkshopsm.pdf">Zhao</a></li>
+			<li>Sensitivity of hazard maps to GMPEs - Mark Petersen</li>
+			<li>Discussion on weighting and other issues for Subduction Zone GMPEs? - Mark Petersen</li>
+		</ul>
+	</li>
+</ul>
+
+<h3>December 13th</h3>
+
+<ul>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM01_Rezaeian2_WUS_GMPEs_Introsm.pdf"/>Introduction</a> - Sanaz Rezaeian</li>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM02_Bozorgnia_NGA_USGS_workshop_Dec_13_2012sm.pdf">Summary of NGA-W2 project and database</a> - Yousef Bozorgnia</li>
+	<li>NGA-W2 GMPEs: (Developers introduce their new models and describe why they should be implemented or replace their previous models in the NSHMp.)
+		<ul>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM03a_Abrahamson_usgs_Dec13_AS12nga_west2sm.pdf">Abrahamson-Silva</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM03b_Boore_BSSA12_USGS_meeting_12-13Dec12sm.pdf">Boore, Stewart</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM03c_CAMPBELL-BOZORGNIA_NGA_West2_13DEC12_USGS_Workshop3sm.pdf">Campbell-Bozorgnia</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM03d_Chiou&Youngs_2012_Update_Dec13_2012sm.pdf">Chiou-Youngs</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM03e_Idriss_USGS_Dec_12sm.pdf">Idriss</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM03f_Gregor-CompGMPE_USGSsm.pdf">Gregor</a></li>
+		</ul>
+	</li>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM04_Youngs_NGAW2_Epistemic_Study_RrYsm.pdf">Additional epistemic uncertainty in NGA-W2</a> - Bob Youngs</li>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM05_2012_Graizer_Kalkan_GMPE_v4sm.pdf">Graizer-Kalkan GMPE</a> - Vladimir Graizer</li>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_AM06_Petersen-Sensitivity%20for%20WUS%20gm%20models-2012sm.pdf">Sensitivity of hazard maps to WUS GMPEs &amp; Discussion on weighting and other issues for WUS GMPEs</a> - Mark Petersen</li>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM02_Stewart-USGS-2012-12-13-Site%20ampsm.pdf">New studies on site amplification factors</a> - Jonathan Stewart</li>
+	<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM03_SPUDICH_NSHMP_Talkv3sm.pdf">New studies on directivity modeling</a> - Paul Spudich</li>
+
+	<li>Discussions &amp; Summary of Workshop:
+		<ul>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM04_Discussion_Rezaeian3_20121213sm.pdf">Sanaz Rezaeian</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM05_Discussion_Wong-USGS%20WUS-CEUS%20GMPE-1sm.pdf">Ivan Wong</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM06_Discussion_BOORE%20rotd50_v1.0(dmb)sm.pdf">Dave Boore</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM07_Discussion_Baker_USGS_directionalitysm.pdf">Jack Baker</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM08_Discussion_BOZORGNIA_Campbell_BACKBONE_GMPE_Dec13_2012sm.pdf">Yousef Bozorgnia</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM09_Discussion_CAMPBELL%20HEM%20Comparison_13DEC12_USGS_Workshopsm.pdf">Ken Campbell</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM10_Discussion_Anderson_FukushimaHamadori_briefsm.pdf">John Anderson</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM11_Discussion_HONGJUN%20SI_20121213NHAMP%20WSsm.pdf">Hongjun</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM12_Discussion_Kamai_USGS_Discussion_GK_bumpsm.pdf">Ronnie Kamai / Norm Abrahamson</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM13_Discussion_BOZORGNIA_inelastic_spectrasm.pdf">Yousef Bozorgnia</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM14_Discussion_Marrone_Suggest%20PSHA%20Evaluation%20for%20Expected%20CEUS%20Spectral%20Peaksm.pdf">Jim Marrone</a></li>
+			<li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/GMPE2012/THUR_PM15_Discussion_SuHao_USGS_workshop_EngViewsm.pdf">Su Hau</a></li>
+		</ul>
+	</li>
+
+</ul>
---
+++ src/htdocs/hazards/workshops/pacnw2006.php
@@ -0,0 +1,75 @@
+<?php
+// Author: Lisa Wald
+// Contact: Mark Peterson
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Pacific Northwest Workshop - 2006';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<h2>March 28-29, 2006 - Jackson Federal Building, Downtown Seattle</h2>
+<p>
+  The links in the following agenda are to PDF files of participants
+  that have granted permission to view their presentations at the CEUS
+  workshop.
+</p>
+
+<h3>March 28th</h3>
+
+<h4>Introduction: Overview of Scientific Issues: Why Are We Here?</h4>
+
+<a href="Weaver.pdf">Weaver</a> and <a href="FrankelDay1.pdf">Frankel</a></p>
+
+<h4>Faults in the Puget Sound region and Elsewhere in Western Washington</h4>
+
+ (new findings on the following faults:Seattle (<a href="Kelseyworkshop.pdf" title="Kelsey" target="_blank">Kelsey</a>),Tacoma (Sherrod), Canyon River (<a href="WalshCanyonRiver.pdf" title="Walsh" target="_blank">Walsh</a>), Little River (<a href="NelsonLittleRiver.pdf" title="Nelson" target="_blank">Nelson</a>), Boulder Creek (Sherrod), Saddle Mountain (<a href="HughesHazard2006.pdf" title="Hughes" target="_blank">Hughes</a>), Calawah (<a href="McCrory-CALAWAH-ed.pdf" title="McCrory" target="_blank">McCrory</a>).</p>
+
+<p>New Seattle fault zone model: <a href="Brocher.3.28.pdf" title="Brocher" target="_blank">Brocher</a></p>
+
+<p>Magnitude estimate for 900 A.D. Seattle fault earthquake:          <a href="Muller.pdf" title="Muller" target="_blank">Muller</a></p>
+
+<p>Summary of recent high-resolution imaging results ( Seattle, Tacoma, Olympia faults): <a href="Pratt.pdf" title="Pratt" target="_blank">Pratt</a></p>
+
+<p>South Whidbey Island Fault experience: <a href="Blakely_presentationSWIF.pdf" title="Blakely" target="_blank">Blakely</a>, <a href="FrankelDay1.pdf" title="Frankel" target="_blank">Frankel</a></p>
+
+<p><strong>Faults in the Portland region and elsewhere in western Oregon</strong>(new results; discussion of Portland Hills, East Bank, and Oatfield faults): Madin, Wells, Blakely</p>
+
+<p><strong>Use of GPS measurements in hazard assessment</strong>. Mapping crustal deformation: <a href="McCaffrey.pdf" title="McCaffrey" target="_blank">McCaffrey</a>. Estimating long-term earthquake rates using strain rates from GPS, <a href="Mazzotti.pdf" title="Mazzotti" target="_blank">Mazzotti</a>, status of GPS observations and episodic tremor and slip<strong></strong>( <a href="Melbourne.pdf" title="Melbourne" target="_blank">Melbourne</a>).</p>
+
+<p><strong>Cascadia subduction zone</strong>: development of recurrence rate-magnitude distribution for great earthquakes M8-9 (Nelson, Goldfinger), time-dependent recurrence models (Petersen, <a href="FrankelDay1.pdf" title="Frankel" target="_blank">Frankel</a>, <a href="AdamsCascCondProbUSGS06.pdf" title="Adams" target="_blank">Adams</a>), location of eastern edge of subduction zone (<a href="LaForgeNew.pdf" title="LaForge" target="_blank">LaForge</a>, <a href="McCaffrey.pdf" title="McCaffrey" target="_blank">McCaffrey</a>), episodic tremor/slip implications for eastern edge of locked zone ( Rogers)</p>
+
+
+
+
+<h3>March 29</h3>
+
+<p><strong>Faults in eastern Washington, including Hanford area</strong>: <a href="Rohay revised.pdf" title="Rohay" target="_blank">Rohay</a></p>
+
+<p><strong>Faults in eastern Oregon</strong>: Madin</p>
+
+<p><strong>Faults in southwest British Columbia:</strong><a href="Cassidy.Leech.River.pdf" title="Cassidy" target="_blank">Cassidy</a></p>
+
+<p><strong>Intra slab earthquakes</strong>, potential for western Oregon: Wong; geometry of slab under Puget Sound: <a href="McCrory-NISQUALLY-Ed.pdf" title="McCrory" target="_blank">McCrory</a>; quantifying hazard for Portland: <a href="FrankelDay2.pdf" title="Frankel" target="_blank">Frankel</a></p>
+
+<p><strong>Ground motion attenuation relations</strong>:</p>
+
+<p> Subduction zone interface and intra slab earthquakes: Atkinson, <a href="GregorPacNW.pdf" title="Gregor " target="_blank">Gregor </a></p>
+<p> Effect of Moho reflections and focusing on ground motions: Rogers</p>
+<p> Crustal faults: Next Generation of Attenuation Relations (NGA): <a href="Abrahamson_nga_summary.pdf" title="Abrahamson" target="_blank">Abrahamson</a></p>
+
+<p> Effects of NGA on national maps, <a href="FrankelDay2.pdf" title="Frankel" target="_blank">Frankel</a></p>
+
+<p><strong>Discussion of engineering</strong><strong> issues</strong> by representatives of the Structural Engineers Associations of Washington and Oregon (<a href="Valley-060329.pdf" title="Valley" target="_blank">Valley</a>, Webber)</p>
+
+<p><strong>Brief description of other products </strong>(<a href="FrankelDay2.pdf" title="Frankel" target="_blank">Frankel</a>):</p>
+
+<p>Probability of earthquake occurrence</p>
+
+<p> Seattle urban hazard maps, plans for Portland; </p>
+
+<p>scenario ground-motion maps for earthquakes on specific faults</p>
+
+<p><strong>Summary of possible changes to national maps based on workshop, with discussion of research priorities</strong></p>
+
+<p>5 PM<strong> adjourn meeting </strong></p>
