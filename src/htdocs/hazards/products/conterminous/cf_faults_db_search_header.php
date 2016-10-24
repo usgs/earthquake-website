99b8d30ebdd15b9c1c10162c934e1ab0a3db3c8a
lisa-usgs <lisa@usgs.gov>
2016-10-24 15:20:16 -0600

    moving cf-header files

---
+++ src/htdocs/hazards/products/conterminous/cf_faults_db_search_header.php
@@ -0,0 +1,7 @@
+<?php
+
+	$TITLE =  htmlspecialchars($_GET["year"]) . " National Seismic Hazard Maps - Source Parameters";
+	$HEAD = '<base href="http://earthquake.usgs.gov/"/>';
+  include 'template.inc.php';
+
+	exit();
