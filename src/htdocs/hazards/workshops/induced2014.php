7a432afebf5730d1a8595cf00bf5b830797bd547
lisa-usgs <lisa@usgs.gov>
2016-10-24 11:05:28 -0600

    finished workshop webpages
    Former-commit-id: bfad21c22f2b810ce1f7e242b621cb39e1155755

---
+++ src/htdocs/hazards/workshops/induced2014.php
@@ -0,0 +1,117 @@
+<?php
+// Author: Lisa Wald
+// Contact: Mark Peterson
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Workshop on Hazard from Induced Seismicity';
+    $NAVIGATION = true;
+    $HEAD = '<link rel="stylesheet" href="../styles.css"/>';
+    include 'template.inc.php';
+  }
+?>
+
+<img class="right" src="images/oklahoma-geological-survey.gif" alt="oklahoma-geological-survey"/>
+
+<h2>Co-hosted by the Oklahoma Geological Survey and the USGS</h2>
+
+<p>Midwest City, OK<p>
+
+<p>
+  <strong>November 17th 2014:</strong> Field trip<br/>
+  <strong>November 18th 2014:</strong> Workshop<br/>
+  <strong>November 19th 2014:</strong> Internal meeting to discuss workshop content
+</p>
+
+<h2>Purpose</h2>
+
+<p>
+  The workshop developed the methodology and models to describe the hazard from induced seismicity for the United States.
+</p>
+<p>
+  The National Seismic Hazard Maps (NSHM) affect public safety and influence building and insurance costs, totaling several hundred billion dollars per year. Currently, suspected non-tectonic earthquakes are removed from the hazard calculations for the NSHM. The hazard calculations for about 10 states, including Oklahoma, will be affected by taking into account suspected non-tectonic earthquakes.
+</p>
+<p>
+  Scientists from State Geological Surveys, academia, the USGS presented their initial findings from including induced seismicity into the hazard calculations.
+</p>
+<h3>Discussions</h3>
+<ol>
+  <li>input into the hazard maps,</li>
+  <li>impacts of the decisions,</li>
+  <li>regulatory and industry issues, and</li>
+  <li>hazard products.</li>
+</ol>
+<p>
+  We invited regulators, industry representatives, scientists, engineers, and the public to provide insight for this new hazard model.
+</p>
+
+<h3>November 17​, 3 to 6 pm​: Field Trip, Departing from the Reed Conference Center front entrance</h3>
+
+<p>
+  (half-day to allow for same day arrival if possible)<br/>
+  Address: 5800 Will Rogers Rd, Midwest City, OK 73110, Phone:(405)741-7333
+</p>
+<p>
+  Overview of oil and gas activity in central Oklahoma and tour of disposal wells including high-volume disposal wells in the Oklahoma City metropolitan area.
+</p>
+
+<h3>November 18: Workshop on Hazard from Induced Seismicity</h3>
+
+<ul>
+  <li><h4>Welcome and Background</h4>
+    <ul>
+      <li>Welcome -- Oklahoma Geological Survey – Holland, Keller</li>
+      <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/induced2014/Petersen-intro.pdf">Welcome USGS</a> – Rubinstein, Anderson, and Petersen</li>
+      <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/induced2014/Ellsworth.pdf">Overview of Induced seismicity</a> - Ellsworth</li>
+      <li>A Case Study Of Seismicity In Azle, Texas – DeShon</li>
+    </ul>
+  </li>
+
+  <li><h4>Part 1: The National Seismic Hazard Model and Potentially Induced Seismicity</h4>
+    <ul>
+      <li>How Is The NSHM Put Together? What Are The Key Factors That Influence Hazard?  -- Mueller</li>
+      <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/induced2014/McGarr.pdf">Is Mmax Different For Induced Seismicity?</a> -- McGarr</li>
+      <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/induced2014/Cramer.pdf">Ground Motions And Source Properties Of Natural Vs Induced Earthquakes</a> -- Cramer</li>
+      <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/induced2014/Hough.pdf">Shaking From Induced Earthquakes</a> – Hough
+      <li>Hazard Sensitivity To Rate Models, Smoothing, And Mmax - Moschetti</li>
+    </ul>
+  </li>
+
+  <li><h4>Part 2: The Logic Tree</h4>
+    <ul>
+      <li>Overview Of The Logic Tree To Assess Hazard From Varying Earthquake Rates -- Rubinstein</li>
+      <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/induced2014/Petersen-logictree.pdf">The Potentially Induced Seismicity Model: Sensitivity To Parameters, Future Seismicity, and the Straw-man Model</a> – Petersen</li>
+      <li>Discussion</li>
+    </ul>
+  </li>
+
+    <ul>
+      <li><h4>Special Lunch Presentation:</h4> Can Stress Measurements Help Determine Whether Induced Seismicity Is likely? - Zoback</li>
+    </ul>
+  </li>
+
+  <li><h4>Part 3: Other Methods & Government Panel</h4>
+    <ul>
+      <li><a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/induced2014/Holland.pdf">One Application Of A Traffic Light System</a> – Holland</li>
+      <li>Another Option -- Operational Earthquake Forecasting – Field</li>
+      <li>Discussion</li>
+      <li>Government Agency Panel And Discussion</li>
+    </ul>
+  </li>
+
+  <li><h4>Industry Panel and Wrap-up</h4>
+    <ul>
+      <li>Industry panel</li>
+      <li>Wrap-up, Next Steps, & Comments</li>
+    </ul>
+  </li>
+
+</ul>
+
+<h3>November 19: Internal meeting</h3>
+<ul>
+  <li>USGS, State Geological Surveys, and NSHMP Steering Committee meeting to discuss workshop content.</li>
+  <li>Steering Committee/ Advisory Panel Meeting</li>
+</ul>
+
+<hr/>
+
+<p class="privacy"><a href="http://www.usgs.gov/privacy.html">USGS Privacy Policy</a></p>
--- src/htdocs/hazards/workshops/pacnw2006.php
+++ src/htdocs/hazards/workshops/pacnw2006.php
@@ -17,59 +17,81 @@
 
 <h3>March 28th</h3>
 
-<h4>Introduction: Overview of Scientific Issues: Why Are We Here?</h4>
+<h4>Introduction: Overview of Scientific Issues: Why Are We Here? (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Weaver.pdf">Weaver</a> and <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/FrankelDay1.pdf">Frankel</a>)</h4>
 
-<a href="Weaver.pdf">Weaver</a> and <a href="FrankelDay1.pdf">Frankel</a></p>
+<h4>Faults in the Puget Sound Region and Elsewhere in Western Washington</h4>
+<ul>
+  <li>New Findings on the Following Faults:
+    <ul>
+      <li>Seattle (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Kelseyworkshop.pdf">Kelsey</a>)</li>
+      <li>Tacoma (Sherrod)</li>
+      <li>Canyon River (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/WalshCanyonRiver.pdf">Walsh</a>)</li>
+      <li>Little River (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/NelsonLittleRiver.pdf">Nelson</a>)</li>
+      <li>Boulder Creek (Sherrod)</li>
+      <li>Saddle Mountain (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/HughesHazard2006.pdf">Hughes</a>)</li>
+      <li>Calawah (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/McCrory-CALAWAH-ed.pdf">McCrory</a>)</li>
+    </ul>
+  </li>
+  <li>New Seattle fault zone model (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Brocher.3.28.pdf">Brocher</a>)</li>
+  <li>Magnitude estimate for 900 A.D. Seattle fault earthquake (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Muller.pdf">Muller</a>)</li>
+  <li>Summary of recent high-resolution imaging results (Seattle, Tacoma, Olympia faults) <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Pratt.pdf">Pratt</a>)</li>
+  <li>South Whidbey Island Fault experience (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Blakely_presentationSWIF.pdf">Blakely</a>, <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/FrankelDay1.pdf">Frankel</a>)</li>
+</ul>
 
-<h4>Faults in the Puget Sound region and Elsewhere in Western Washington</h4>
+<h4>Faults in the Portland Region and Elsewhere in Western Oregon</h4>
+<ul>
+  <li>New results; discussion of Portland Hills, East Bank, and Oatfield faults (Madin, Wells, Blakely)</li>
+</ul>
 
- (new findings on the following faults:Seattle (<a href="Kelseyworkshop.pdf" title="Kelsey" target="_blank">Kelsey</a>),Tacoma (Sherrod), Canyon River (<a href="WalshCanyonRiver.pdf" title="Walsh" target="_blank">Walsh</a>), Little River (<a href="NelsonLittleRiver.pdf" title="Nelson" target="_blank">Nelson</a>), Boulder Creek (Sherrod), Saddle Mountain (<a href="HughesHazard2006.pdf" title="Hughes" target="_blank">Hughes</a>), Calawah (<a href="McCrory-CALAWAH-ed.pdf" title="McCrory" target="_blank">McCrory</a>).</p>
+<h4>Use of GPS Measurements in Hazard Assessment</h4>
+<ul>
+  <li>Mapping crustal deformation (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/McCaffrey.pdf">McCaffrey</a>)</li>
+  <li>Estimating long-term earthquake rates using strain rates from GPS (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Mazzotti.pdf">Mazzotti</a>)</li>
+  <li>Status of GPS observations and episodic tremor and slip (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Melbourne.pdf">Melbourne</a>)</li>
+</ul>
 
-<p>New Seattle fault zone model: <a href="Brocher.3.28.pdf" title="Brocher" target="_blank">Brocher</a></p>
-
-<p>Magnitude estimate for 900 A.D. Seattle fault earthquake:          <a href="Muller.pdf" title="Muller" target="_blank">Muller</a></p>
-
-<p>Summary of recent high-resolution imaging results ( Seattle, Tacoma, Olympia faults): <a href="Pratt.pdf" title="Pratt" target="_blank">Pratt</a></p>
-
-<p>South Whidbey Island Fault experience: <a href="Blakely_presentationSWIF.pdf" title="Blakely" target="_blank">Blakely</a>, <a href="FrankelDay1.pdf" title="Frankel" target="_blank">Frankel</a></p>
-
-<p><strong>Faults in the Portland region and elsewhere in western Oregon</strong>(new results; discussion of Portland Hills, East Bank, and Oatfield faults): Madin, Wells, Blakely</p>
-
-<p><strong>Use of GPS measurements in hazard assessment</strong>. Mapping crustal deformation: <a href="McCaffrey.pdf" title="McCaffrey" target="_blank">McCaffrey</a>. Estimating long-term earthquake rates using strain rates from GPS, <a href="Mazzotti.pdf" title="Mazzotti" target="_blank">Mazzotti</a>, status of GPS observations and episodic tremor and slip<strong></strong>( <a href="Melbourne.pdf" title="Melbourne" target="_blank">Melbourne</a>).</p>
-
-<p><strong>Cascadia subduction zone</strong>: development of recurrence rate-magnitude distribution for great earthquakes M8-9 (Nelson, Goldfinger), time-dependent recurrence models (Petersen, <a href="FrankelDay1.pdf" title="Frankel" target="_blank">Frankel</a>, <a href="AdamsCascCondProbUSGS06.pdf" title="Adams" target="_blank">Adams</a>), location of eastern edge of subduction zone (<a href="LaForgeNew.pdf" title="LaForge" target="_blank">LaForge</a>, <a href="McCaffrey.pdf" title="McCaffrey" target="_blank">McCaffrey</a>), episodic tremor/slip implications for eastern edge of locked zone ( Rogers)</p>
-
-
+<h4>Cascadia subduction zone</h4>
+<ul>
+  <li>Development of recurrence rate-magnitude distribution for great earthquakes M8-9 (Nelson, Goldfinger)</li>
+  <li>Time-dependent recurrence models (Petersen, <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/FrankelDay1.pdf">Frankel</a>, <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/AdamsCascCondProbUSGS06.pdf">Adams</a>)</li>
+  <li>Location of eastern edge of subduction zone (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/LaForgeNew.pdf">LaForge</a>, <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/McCaffrey.pdf">McCaffrey</a>)</li>
+  <li>Episodic tremor/slip implications for eastern edge of locked zone ( Rogers)</li>
+</ul>
 
 
 <h3>March 29</h3>
 
-<p><strong>Faults in eastern Washington, including Hanford area</strong>: <a href="Rohay revised.pdf" title="Rohay" target="_blank">Rohay</a></p>
+<h4>Faults in Eastern Washington, Including Hanford Area (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Rohay revised.pdf">Rohay</a>)</h4>
 
-<p><strong>Faults in eastern Oregon</strong>: Madin</p>
+<h4>Faults in Eastern Oregon (Madin)</h4>
 
-<p><strong>Faults in southwest British Columbia:</strong><a href="Cassidy.Leech.River.pdf" title="Cassidy" target="_blank">Cassidy</a></p>
+<h4>Faults in Southwest British Columbia (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Cassidy.Leech.River.pdf">Cassidy</a>)</h4>
 
-<p><strong>Intra slab earthquakes</strong>, potential for western Oregon: Wong; geometry of slab under Puget Sound: <a href="McCrory-NISQUALLY-Ed.pdf" title="McCrory" target="_blank">McCrory</a>; quantifying hazard for Portland: <a href="FrankelDay2.pdf" title="Frankel" target="_blank">Frankel</a></p>
+<h4>Intra-Slab Earthquakes</h4>
+<ul>
+  <li>Potential for western Oregon (Wong)</li>
+  <li>Geometry of slab under Puget Sound (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/McCrory-NISQUALLY-Ed.pdf">McCrory</a>)</li>
+  <li> Quantifying hazard for Portland (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/FrankelDay2.pdf">Frankel</a>)</li>
+</ul>
 
-<p><strong>Ground motion attenuation relations</strong>:</p>
+<h4>Ground Motion Attenuation Relations</h4>
+<ul>
+  <li>Subduction zone interface and intra slab earthquakes (Atkinson, <a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/GregorPacNW.pdf">Gregor</a>)</li>
+  <li>Effect of Moho reflections and focusing on ground motions (Rogers)</li>
+  <li>Crustal faults: Next Generation of Attenuation Relations (NGA) (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Abrahamson_nga_summary.pdf">Abrahamson</a>)</li>
+  <li>Effects of NGA on national maps (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/FrankelDay2.pdf">Frankel</a>)</li>
+</ul>
 
-<p> Subduction zone interface and intra slab earthquakes: Atkinson, <a href="GregorPacNW.pdf" title="Gregor " target="_blank">Gregor </a></p>
-<p> Effect of Moho reflections and focusing on ground motions: Rogers</p>
-<p> Crustal faults: Next Generation of Attenuation Relations (NGA): <a href="Abrahamson_nga_summary.pdf" title="Abrahamson" target="_blank">Abrahamson</a></p>
+<h4>Discussion of Engineering Issues</h4>
+<ul>
+  <li>Representatives of the Structural Engineers Associations of Washington and Oregon (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/Valley-060329.pdf">Valley</a>, Webber)</li>
+</ul>
 
-<p> Effects of NGA on national maps, <a href="FrankelDay2.pdf" title="Frankel" target="_blank">Frankel</a></p>
+<h4>Brief Description of Other Products (<a href="ftp://hazards.cr.usgs.gov/web/nshm/workshops/PACNW2006/FrankelDay2.pdf">Frankel</a>)</h4>
+<ul>
+  <li>Probability of earthquake occurrence</li>
+  <li>Seattle urban hazard maps, plans for Portland</li>
+  <li>Scenario ground-motion maps for earthquakes on specific faults</li>
+</ul>
 
-<p><strong>Discussion of engineering</strong><strong> issues</strong> by representatives of the Structural Engineers Associations of Washington and Oregon (<a href="Valley-060329.pdf" title="Valley" target="_blank">Valley</a>, Webber)</p>
-
-<p><strong>Brief description of other products </strong>(<a href="FrankelDay2.pdf" title="Frankel" target="_blank">Frankel</a>):</p>
-
-<p>Probability of earthquake occurrence</p>
-
-<p> Seattle urban hazard maps, plans for Portland; </p>
-
-<p>scenario ground-motion maps for earthquakes on specific faults</p>
-
-<p><strong>Summary of possible changes to national maps based on workshop, with discussion of research priorities</strong></p>
-
-<p>5 PM<strong> adjourn meeting </strong></p>
+<h4>Summary of Possible Changes to National Maps Based on Workshop, With Discussion of Research Priorities</h4>
