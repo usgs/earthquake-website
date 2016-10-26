63155676a9140432a9e712c211eec7360fe94bbd
lisa-usgs <lisa@usgs.gov>
2016-10-26 11:56:20 -0600

    changed links to large MP4 files

--- src/htdocs/data/cus_seisvelmodel/M7.7simulation.php
+++ src/htdocs/data/cus_seisvelmodel/M7.7simulation.php
@@ -1,7 +1,6 @@
 <?php
   // Author: Lisa Wald
   // Contact: Rob Williams, rawilliams@usgs.gov
-  // Last modified: 07/18/2016
     if (!isset($TEMPLATE)) {
       $TITLE = 'Computer Simulation of a Magnitude 7.7 Earthquake in the New Madrid Seismic Zone';
       $NAVIGATION = true;
@@ -10,5 +9,5 @@
 ?>
 
 <p>
-  <video src="M7.7simulation-south.mp4" poster="images/M7.7simulation.gif" width="834" height="884" controls></video>
+  <video src="/sitedata/nshm/urban/M7.7simulation-south.mp4" poster="images/M7.7simulation.gif" width="834" height="884" controls></video>
 </p>
