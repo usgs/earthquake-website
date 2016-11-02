5c550ae5142db9e37bf5837539257c45cf475098
tkalin-usgs <tkalin@usgs.gov>
2016-11-02 10:13:13 -0600

    Initial work on some learn pages

---
+++ src/htdocs/learn/topics/eqmonitoring/comparison.php
@@ -0,0 +1,45 @@
+<?php
+      //	Author:	Tiffany Kalin
+      //	Contact:
+      //	Last	modified:	11/1/2016
+      if	(!isset($TEMPLATE))	{
+      $TITLE	=	'A Comparison of Analog and Digital Seismograms';
+      $NAVIGATION	= true;
+      include	'template.inc.php';
+      }
+?>
+<!--<h2>A Comparison of Analog and Digital Seismograms</h2>-->
+<p>
+	Here is an image of an analog recording of an earthquake.The relatively flat lines are periods of quiescence and the large and squiggly line is an earthquake.<br />
+	<br />
+  <img src="images/classic_seis.gif" alt="Image of a classic analog seismogram"/><br />
+  <br />
+  Below is a digital seismogram. The data is stored electronically, easy to access and manipulate, and much more accurate and detailed than the analog recordings.<br />
+  <br />
+  <img src="images/digital_seis.gif" alt="Image of a Digital seismograph"/>
+</p>
+
+<h2>Seismograms: Analog vs. Digital </h2>
+<p>
+	Earthquake research has assisted engineers in determining better construction and design of retrofitting of homes and buildings that can withstand the shaking that earthquakes generate. Earthquake information, such as location, magnitude, and shaking distribution, is immediately available within minutes after an earthquake to everyone via broadcast media or the internet.
+</p>
+
+<!--
+<hr />
+<div align="center">
+	<a href="eq-mon-9.php">Previous Page</a>
+	<a href="eq-mon-11.php">Next Page</a>
+	<br />
+	<a href="index.php">TOC</a> |
+	<a href="eq-mon-1.php">1</a> |
+	<a href="eq-mon-2.php">2</a> |
+	<a href="eq-mon-3.php">3</a> |
+	<a href="eq-mon-4.php">4</a> |
+	<a href="eq-mon-4.php">5</a> |
+	<a href="eq-mon-6.php">6</a> |
+	<a href="eq-mon-7.php">7</a> |
+	<a href="eq-mon-8.php">8</a> |
+	<a href="eq-mon-9.php">9</a> |
+	10 |
+	<a href="eq-mon-11.php">11</a>
+</div>-->
---
+++ src/htdocs/learn/topics/eqmonitoring/index.php
@@ -0,0 +1,98 @@
+<?php
+	if ( ! isset( $TEMPLATE ) ) {
+		/**
+			PAGE TITLE
+			
+			You must Change the Page Title
+		*/
+		$TITLE = "Earthquake Monitoring";
+		
+		/**
+		
+			CUSTOM PAGE CONTACT
+			
+			 By default the link at the bottom of each page will send email to Lisa, who can forward the email as appropriate
+			
+			 To have a custom page contact $CONTACT must have a usgsEmailID
+		*/
+			// $CONTACT = "rhea@usgs.gov";
+		
+		/**
+			CUSTOM STYLESHEETS
+			
+			 Before creating a custom stylesheet for one page, talk to the web team to determine if these
+			 styles should be included in the global scripts first
+			
+			 To include custom stylesheets $STYLESHEETS must contain a comma delimited list of absolute paths to the stylesheet
+		*/
+		//	$STYLESHEETS = "/css/stylesheet1.css,/css/stylesheet2.css";
+		
+		/**
+			CUSTOM JAVASCRIPT
+		
+			 Before creating a custom javascript for one page, talk to the web team to determine if this
+			 functionality should be included in the global scripts first
+			
+			 To include custom javascripts $SCRIPTS must contain a comma delimited list of absolute paths to the stylesheet
+		*/
+		//	$SCRIPTS = "/js/script1.js,/js/script2.js";
+	
+	
+		/**
+			CHOOSE A TEMPLATE
+		
+			 WARNING: Only one template should be uncommented!
+		*/
+		// Two Column
+		include $_SERVER['DOCUMENT_ROOT'] . "/template/template.inc.php";
+		
+		// One Column
+		//include $_SERVER['DOCUMENT_ROOT'] . "/template/template.onecolumn.inc.php";
+	}
+?>  <h2>Why EQ Monitoring?</h2>
+		  <p>Each year there are several thousand earthquakes throughout the world. There are about 60 each day in California alone! With the grown population and infrastructure (high-rises, bridges, apartments, pipelines, communication towers, etc.) earthquakes pose a greater hazard to our lives and comminutes than ever before. A few hundred years ago, even large earthquakes could go unnoticed but even now a small earthquake is often felt by thousands of people. Before we learn about earthquake monitoring, we need to learn a few things about earthquakes and what is actually monitored. </p>
+		  <h2>Table of Contents</h2>          
+		  <h3><a href="eq-mon-1.php">EQ Monitoring Basics</a></h3>
+          <h3><a href="eq-mon-2.php">Putting it All Together</a></h3>
+          <ul>
+            <li>
+              <h4><a href="eq-mon-3.php">Magnitude vs. Intensity</a></h4>
+            </li>
+            <li>
+              <h4><a href="eq-mon-4.php">EQ's and Buildings</a></h4>
+            </li>
+          </ul>
+          <h3><a href="eq-mon-5.php">EQ Monitoring History</a></h3>
+          <ul>
+            <li>
+              <h4><a href="eq-mon-6.php">History of Seismometry</a></h4>
+            </li>
+            <li>
+              <h4><a href="eq-mon-7.php">History Timeline</a></h4>
+            </li>
+            <li>
+              <h4><a href="eq-mon-8.php">Analog Seismometers</a></h4>
+            </li>
+          </ul>
+          <h3>EQ Monitoring Today</h3>
+          <ul>
+            <li>
+              <h4><a href="eq-mon-9.php">Digital Instruments</a></h4>
+            </li>
+            <li>
+              <h4><a href="eq-mon-10.php">Seismograms: Analog vs. Digital</a></h4>
+            </li>
+          </ul>
+          <h3><a href="eq-mon-11.php">Additional Reading</a></h3>
+          <hr />
+          <div align="center">              <a href="eq-mon-1.php">Next Page</a>  <br />
+            TOC |            <a href="eq-mon-1.php">1</a> | <a href="eq-mon-2.php">2</a> | 
+			  <a href="eq-mon-2.php"></a><a href="eq-mon-3.php">3</a> | 
+			  <a href="eq-mon-2.php"></a> <a href="eq-mon-4.php">4</a> | 
+			  <a href="eq-mon-2.php"></a> <a href="eq-mon-5.php">5</a> | 
+			  <a href="eq-mon-2.php"></a> <a href="eq-mon-6.php">6</a> | 
+			  <a href="eq-mon-2.php"></a> <a href="eq-mon-7.php">7</a> | 
+			  <a href="eq-mon-2.php"></a> <a href="eq-mon-8.php">8</a> | 
+			  <a href="eq-mon-2.php"></a> <a href="eq-mon-9.php">9</a> | 
+			  <a href="eq-mon-2.php"></a> <a href="eq-mon-10.php">10</a> | 
+		  <a href="eq-mon-2.php"></a> <a href="eq-mon-11.php">11</a></div>
