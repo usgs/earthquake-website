8b541eaa9ce78cc6e07c8178f100501dba55371d
lisa-usgs <lisa@usgs.gov>
2016-11-02 13:35:20 -0600

    added more figures to safz article

--- src/htdocs/learn/topics/safz-paleo/determining.php
+++ src/htdocs/learn/topics/safz-paleo/determining.php
@@ -4,35 +4,53 @@
   if (!isset($TEMPLATE)) {
     $TITLE = 'Establishing the Story of Past Earthquakes on the San Andreas Fault to Inform the Future - working title';
     $NAVIGATION = true;
-    $HEAD = '<link rel="stylesheet" href="../styles.css"/>';
+    $HEAD = '<link rel="stylesheet" href="../styles.css"/>
+            <link rel="stylesheet" href="styles.css"/>';
     include 'template.inc.php';
   }
 ?>
-
-<figure class='right'>
-  <img src='images/???' alt=''/>
-  <figcaption>
-    ???
-  </figcaption>
-</figure>
 
 <h2>Determining the Age of a Paleoearthquake</h2>
 <p>
   How do you go from the observation of a prehistoric earthquake in the trench wall to the age of an earthquake? You’ll want this to understand how long ago the most recent one happened, how old the earthquake before that one is, and so on through the sequence of disrupted sediments.
 </p>
+
+<figure>
+  <img src='images/trenchwall.jpg' alt='photo of trench wall on left, sketch with layers outlined on the right'/>
+  <figcaption>
+    Fault produced during large magnitude earthquake when organic rich layer 4 was at the ground surface. Note that silt and sand layer  5 is thicker on the left side of the fault, where it filled in the step (scarp) made by motion on the fault.  Layers 5 and 6 are not affected by the fault.  The age of the earthquake is determined by the age of layers above and below the fault tip.
+  </figcaption>
+</figure>
+
+<figure class='right charcoal'>
+  <img src='images/charcoal.jpg' alt='photo of trench wall with a piece of charcoal in a layer'/>
+  <figcaption>
+    Piece of charcoal in silt and sand layer 1 used in radiocarbon dating.
+  </figcaption>
+</figure>
+
 <p>
   There are many different ways that dates are extracted from samples gathered at paleoearthquake sites.  The dating technique that’s familiar to most people, and is the most common, is radiocarbon dating.  Radiocarbon dating allows you to determine the age of material contains carbon, which is almost anything that was once a living plant or animal.  Plant remains like seeds and leaves, charcoal from past fires, and shells and soils are all datable materials using radiocarbon.
 </p>
 
-<figure class='right'>
-  <img src='images/???' alt='radiocarbon dating'/>
-  <figcaption>
-    ???
-  </figcaption>
-</figure>
 <p>
   The use of accelerator mass spectrometry, which allows you to measure the number of radiogenic carbon isoptoes and determine the age, has lead to improvements in paleoseismology.  The big advantage is actually that new lab techniques can now resolve the age of very small samples.  This means that a scientist can collect small samples the size of a grain of rice from a geologic feature instead of schlepping large, heavy samples from the field to the lab, and the data are even better, as a wide variety of material can be dated.  The age of each earthquake is bracketed by dating layers above and below the layer deformed by the prehistoric earthquake.
 </p>
+
+<figure class='left results'>
+  <img src='images/calibration.gif' alt='radiocarbon dating'/>
+  <figcaption>
+    The sample is analyzed by AMS and the number of 14C isotopes is determined. When this is calibrated with the calibration curve (blue), the variations in 14C over time can be used to determine the age of the sample in calendar years.  It has a complex shape (grey) due to uncertainties in the laboratory age (green).  The mean radicarbon age of charcoal in layer 1 is 1102 A.D.
+  </figcaption>
+</figure>
+<figure class='left results'>
+  <img src='images/results.gif' alt='radiocarbon dating'/>
+  <figcaption>
+    Radiocarbon ages are determined for all of the layers to establish that the dating is robust and no contamination has occurred.  Note that samples get older with each deeper layer. The earthquake in the panel above occured when layer 4 was at the ground surface. The age of the earthquake is determined from the radiocarbon ages above and below, in this case the top and bottom of layer 4. This earthquake occured between 1457 and 1568 AD.
+  </figcaption>
+</figure>
+
+<br style="clear:left;"/>
 
 <h2>Estimating Paleoearthquake Magnitudes</h2>
 <p>
--- src/htdocs/learn/topics/safz-paleo/finding.php
+++ src/htdocs/learn/topics/safz-paleo/finding.php
@@ -4,7 +4,8 @@
   if (!isset($TEMPLATE)) {
     $TITLE = 'Establishing the Story of Past Earthquakes on the San Andreas Fault to Inform the Future - working title';
     $NAVIGATION = true;
-    $HEAD = '<link rel="stylesheet" href="../styles.css"/>';
+    $HEAD = '<link rel="stylesheet" href="../styles.css"/>
+            <link rel="stylesheet" href="styles.css"/>';
     include 'template.inc.php';
   }
 ?>
@@ -24,24 +25,23 @@
   To find a paleoearthquake, first you need to find a fault.  The standard use of lidar (See Down in the Trenches and Up in the Air - earthquake.usgs.gov/learn/topics/wasatch/collect.php) in geological investigations has made it much easier to find evidence of faults. Lidar can cover a lot more ground than a few geologists hiking around looking for evidence of past earthquakes, and its ability to “see through” the vegetation also allows coverage in areas where it may be difficult or impossible for scientists to access.
 </p>
 
-<figure class='right'>
-  <img src='images/???' alt='trench cross-section'/>
+<figure class='right trench'>
+  <img src='images/trench.gif' alt='trench cross-section'/>
   <figcaption>
-    ???
+    Layers of sediment in a trench wall outlined in color.  The red line curving up from the bottom is the fault.
   </figcaption>
 </figure>
 <p>
   Once you’ve found a fault, you need to be able to see the layers of sediment and soils that have been deposited over time beneath the surface.  Usually this requires digging trenches across the fault.  In the trench you can see sediments and soils that have come down from nearby hills and are deposited in layers, one on top of the other, like a stack of books. When an earthquake occurs along a fault, the layers are disrupted, bent and offset.  As time goes by, new layers are deposited on top. Since layers deposited after the earthquake are not broken, it is clear that the earthquake occurred before they were deposited.  This pattern repeats over the centuries, creating a geological story in the layers and their interruptions.  Viewing a cross-section of deposits near the fault from the surface downward is like reading a timeline backwards from present to past.  The job of a paleoseismologist is to unravel the sequence of events revealed in the cross-section.
 </p>
-<sidebar>
+
   <figure class='right'>
     <img src='images/trees.jpg' alt='dead trees in a tidal marsh'/>
     <figcaption>
-      Dead forest of western red cedar sticking up through a brackish-water tidal marsh at Copalis River on the Washington coast. The forest was killed during rapid subsidence accompanying the most recent Cascadia Subduction Zone earthquake. Tree rings from the dead trees provide evidence that the trees died sometime after the end of the A.D. 1699 growing season and before the beginning of the A.D. 1700 growing season. This age is consistent with the date of this earthquake based on a Japanese tsunami, discussed below. The forest in the background is above the highest tides. (Brian Atwater, USGS)
+      Dead forest of western red cedar sticking up through a brackish-water tidal marsh at Copalis River on the Washington coast. The forest was killed during rapid subsidence accompanying the most recent Cascadia Subduction Zone earthquake. Tree rings from the dead trees provide evidence that the trees died sometime after the end of the A.D. 1699 growing season and before the beginning of the A.D. 1700 growing season. This age is consistent with the date of this earthquake based on a Japanese tsunami. The forest in the background is above the highest tides. (Brian Atwater, USGS)
     </figcaption>
   </figure>
   In addition to trenching, there are some other rather creative techniques for identifying paleoearthquakes. Using dendrochronology, or tree rings, is an interesting way to look for paleoseismic evidence.  Trees that were severely shaken, permanently tilted from shaking, or were inundated by salt water from subsidence, will suffer in health and not grow as quickly as usual for a while after an earthquake.  The subsequent tree ring for that time period will be thinner than usual. In coastal areas like where this photo was taken, dendrochronology can be used to determine when large paleoearthquakes altered the level of the land: during subduction zone earthquakes some parts of the land surface can lower by several feet. Thus forests with trees going along a coast will suddenly die as the roots of the trees are drowned in salt water.
-</sidebar>
 
 <!-- NAVIGATION at bottom for ARTICLE -->
 <p>
