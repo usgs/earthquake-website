d8f718e184fd57fcc55a373cd5b6e93fa38f5445
lisa-usgs <lisa@usgs.gov>
2016-11-03 13:11:44 -0600

    animations with new mp4 files for Learn

---
+++ src/htdocs/learn/animations/index.php
@@ -0,0 +1,275 @@
+<?php
+// Author: Lisa Wald
+// Contact: Lisa Wald
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Animations for Earthquake Terms and Concepts';
+    $NAVIGATION = true;
+    $HEAD = '<link rel="stylesheet" href="index.css"/>';
+    include 'template.inc.php';
+  }
+?>
+
+<div class="row">
+
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/aftershocks.mp4">
+        <h4>Aftershocks, Foreshocks &amp; Mainshocks</h4>
+        <img src="images/aftershocks.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        Aftershocks are earthquakes that follow the largest shock of an earthquake sequence. They are smaller than the mainshock and within 1-2 fault lengths distance from the mainshock fault. Aftershocks can continue over a period of weeks, months, or years. In general, the larger the mainshock, the larger and more numerous the aftershocks, and the longer they will continue. Foreshocks are relatively smaller earthquakes that precede the largest earthquake in a series, which is termed the mainshock. Not all mainshocks have foreshocks.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/amplification.mp4">
+        <h4>Amplification</h4>
+        <img src="images/amplification.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        Shaking at a site may be increased, or amplified, by focusing of seismic energy caused by the materials in basins or by surface topography such as mountains.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/asperity.mp4">
+        <h4>Asperity</h4>
+        <img src="images/asperity.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        An asperity is an area on a fault that is stuck. The earthquake rupture usually begins at an asperity.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/attenuation.mp4">
+        <h4>Attenuation</h4>
+        <img src="images/attenuation.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        When you throw a pebble in a pond, it makes waves on the surface that move out from the place where the pebble entered the water. The waves are largest where they are formed and gradually get smaller as they move away. This decrease in size, or amplitude, of the waves is called attenuation. Seismic waves also become attenuated as they move away from the earthquake source.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/blindthrustfault.mp4">
+        <h4>Blind Thrust Fault</h4>
+        <img src="images/blindthrustfault.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        A thrust fault that does not rupture all the way up to the surface so there is no evidence of it on the ground. It is buried under the uppermost layers of rock in the crust.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/directivity.mp4">
+        <h4>Directivity</h4>
+        <img src="images/directivity.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        Directivity is an effect of a fault rupturing whereby earthquake ground motion in the direction of rupture propagation is more severe than that in other directions from the earthquake source.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/divergent.mp4">
+        <h4>Divergent</h4>
+        <img src="images/divergent.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        A divergent boundary is where two adjacent tectonic plates are moving away from each other.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/elasticrebound.mp4">
+        <h4>Elastic Rebound</h4>
+        <img src="images/elasticrebound.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        Elastic rebound is what happens to the crustal material on either side of a fault during an earthquake. The idea is that a fault is stuck until the strain accumulated in the rock on either side of the fault has overcome the friction making it stick. The rock becomes distorted, or bent, but holds its position until the earthquake occurs, and the rock snaps back into an unstrained position, releasing energy that produces seismic waves.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/elasticrebound.mp4">
+        <h4>Elastic Rebound</h4>
+        <img src="images/elasticrebound.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        Elastic rebound is what happens to the crustal material on either side of a fault during an earthquake. The idea is that a fault is stuck until the strain accumulated in the rock on either side of the fault has overcome the friction making it stick. The rock becomes distorted, or bent, but holds its position until the earthquake occurs, and the rock snaps back into an unstrained position, releasing energy that produces seismic waves.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/horstandgraben.mp4">
+        <h4>Horst and Graben</h4>
+        <img src="images/horstandgraben.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        A horst is an upthrown block lying between two steep-angled fault blocks. A graben is a down-dropped block of the earth's crust resulting from extension, or pulling, of the crust.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/greatcircle.mp4">
+        <h4>Great Circle</h4>
+        <img src="images/greatcircle.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        The shortest path between two points on the surface of a sphere lies along a great circle.  On a 2-dimensional map, this looks like a line, but when it's on a 3-dimensional sphere, it's an arc... part of a circle.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/liquefaction.mp4">
+        <h4>Liquefaction</h4>
+        <img src="images/liquefaction.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        Liquefaction is a process by which water-saturated sediment temporarily loses strength and acts like a fluid... like when you wiggle your toes in the wet sand near the water at the beach. This effect can be caused by earthquake shaking.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/normalfault.mp4">
+        <h4>Normal Fault</h4>
+        <img src="images/normalfault.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        Normal, or Dip-slip, faults are inclined fractures where the blocks have mostly shifted vertically. If the rock mass above an inclined fault moves down, the fault is termed normal, whereas if the rock above the fault moves up, the fault is termed a Reverse fault.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/shadowzone.mp4">
+        <h4>Shadow Zone</h4>
+        <img src="images/shadowzone.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        The shadow zone is the area of the earth from angular distances of 104 to 140 degrees that, for a given earthquake, that does not receive any direct P waves. The shadow zone results from S waves (not shown in animation) being stopped entirely by the liquid core and P waves being bent (refracted) by the liquid core.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/strikeslip.mp4">
+        <h4>Strike-Slip Fault</h4>
+        <img src="images/strikeslip.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        Strike-slip faults are vertical (or nearly vertical) fractures where the blocks have mostly moved horizontally. If the block opposite an observer looking across the fault moves to the right, the slip style is termed right-lateral; if the block moves to the left, the motion is termed left-lateral.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/thrustfault.mp4">
+        <h4>Thrust Fault</h4>
+        <img src="images/thrustfault.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        A thrust fault is a reverse fault with a dip of 45&deg; or less, a very low angle. This animation shows a reverse fault.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/thrustfault.mp4">
+        <h4>Thrust Fault</h4>
+        <img src="images/thrustfault.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        A thrust fault is a reverse fault with a dip of 45&deg; or less, a very low angle. This animation shows a reverse fault which is a steeper-angle fault, but it moves the same way.
+      </p>
+    </li>
+  </ul>
+  <ul class="no-style linklist card">
+    <li>
+      <p>
+        <a href="mp4/wavefront.mp4">
+        <h4>Wavefront</h4>
+        <img src="images/wavefront.png" alt=" icon"  width=150/>
+        </a>
+      </p>
+      <br style="clear:left;"/>
+      <p>
+        The wavefront is the instantaneous boundary between the seismic waves in the earth material, and the material that the seismic energy has not yet reached. As a seismic wave propagates through the earth, the wavefront moves.
+      </p>
+    </li>
+  </ul>
+
+</div>
+
+<h2>More USGS Videos and Animations</h2>
+<ul>
+	<li><a href="http://education.usgs.gov/common/video_animation.htm#earthquakes">earthquakes</a></li>
+	<li><a href="http://education.usgs.gov/common/video_animation.htm#tectonics">plate tectonics</a></li>
+	<li><a href="http://education.usgs.gov/common/video_animation.htm#geology">geology</a></li>
+	<li><a href="http://education.usgs.gov/common/video_animation.htm#tsunamis">tsunamis</a></li>
+</ul>
