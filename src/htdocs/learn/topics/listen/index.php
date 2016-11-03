210f523bc25ea0a9984647450329c39a22962d0a
tkalin-usgs <tkalin@usgs.gov>
2016-11-03 14:21:25 -0600

    Learn topics listen pages

---
+++ src/htdocs/learn/topics/listen/index.php
@@ -0,0 +1,48 @@
+<?php
+      //	Author:	Tiffany Kalin
+      //	Contact:
+      //	Last	modified:	11/2/2016
+      if	(!isset($TEMPLATE))	{
+      $TITLE	=	'Listening to Earthquakes';
+      $NAVIGATION	= true;
+      include	'template.inc.php';
+      }
+?>
+
+<p>
+	<em>Created by Andy Michael, USGS, and Daniel Ross, 12 year old USGS Volunteer for Science.</em>
+</p>
+<p>
+	When we listen to music we hear a wide range of sound frequencies
+  or notes. When we feel an earthquake we feel a wide range
+  of frequencies but unlike music the notes are less distinct.
+  People describing what they felt during an earthquake may
+  call it a "sharp jolt" or a "rolling motion." The sharp jolt
+  is due to high frequencies while the rolling motion is due
+  to low frequencies. What you feel during an earthquake is
+  a product of the earthquake itself, how far you are from the
+  earthquake, and the types of rocks between you and the earthquake.
+  To learn about this we will convert the shaking during earthquakes
+  to sound and then listen to the sounds.
+</p>
+<h3>Converting Shaking to Sound</h3>
+<p>
+  When a loudspeaker produces sound it shakes. During an earthquake
+  the ground shakes. To convert the earthquake shaking to sound
+  we make the loudspeaker shake the same way the ground did. But,
+  if we really did that the sound would be too low for people
+  to hear. So, we speed things up so that the loudspeaker moves
+  back and forth much faster than the ground did and then we can
+  hear it.
+</p>
+<h3>Tips for listening</h3>
+<p>
+	The sound files on these pages are in .wav format. If your browser
+	can't play them back, you will need to <a href="setup.php">install
+	a plugin</a> that will do this.
+</p>
+<p>
+	The sounds on these pages require a good loudspeaker. If
+  your computer doesn't have good speakers (a subwoofer is best)
+  then try using a pair of earphones.
+</p>
---
+++ src/htdocs/learn/topics/listen/rocktype.php
@@ -0,0 +1,53 @@
+<?php
+      //	Author:	Tiffany Kalin
+      //	Contact:
+      //	Last	modified:	11/3/2016
+      if	(!isset($TEMPLATE))	{
+      $TITLE	=	'Rock Type Effects';
+      $NAVIGATION	= true;
+      include	'template.inc.php';
+      }
+?>
+<p>
+	If you haven't done the <a href="distance.php">Distance
+  Effects </a> topic yet, you should do that one first.
+</p>
+<p>
+	When learning about how distance affects shaking we learned
+  that the waves lose energy as they travel. How fast they lose
+  energy depends on the type of rock they are traveling through.
+  For instance, very solid granites will transmit energy better
+  than the chewed up rocks we find in fault zones. It's almost
+  like the waves traveling through the chewed up rocks are traveling
+  further and so they lose more energy and especially more of
+  the high frequencies.
+</p>
+<p>
+	Below are two seismograms, or records of ground shaking,
+  you can listen to. They come from the same earthquake but
+  were recorded on two different seismometers. Both seismometers
+  were about 5 miles from the earthquake, but one is on very
+  solid rock while the other is in the fault zone. That's why
+  they have different pitches. The volume has been adjusted
+  to the same level so concentrate on the pitch to tell the
+  difference. Click the sideways triangle (or play button) to
+  hear the sound.
+</p>
+<h3>Seismometer One</h3>
+<p>
+	<audio controls>
+		<source src="wavs/pst.wav" type="audio/wav"/>
+	Your browser does not support the audio element.
+	</audio>
+</p>
+<h3>Seismometer Two</h3>
+<p>
+	<audio controls>
+		<source src="wavs/pmm.wav" type="audio/wav"/>
+	Your browser does not support the audio element.
+	</audio>
+</p>
+<p>
+	Which seismometer was on the solid rock: <a href="answers/pst.php">One </a>
+  or <a href="answers/pmm.php">Two</a>?
+</p>
