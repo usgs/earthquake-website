3487663fc3b6dee74f8f773ef3ca16c10ab186a2
Scott Haefner <shaefner@usgs.gov>
2016-11-07 15:19:47 -0800

    send email to shane

--- src/htdocs/contactus/menlo/workshop/index.php
+++ src/htdocs/contactus/menlo/workshop/index.php
@@ -75,7 +75,7 @@ if (isSet($_POST['submit'])) { // user submitted form
 
   // Email alert
   $admin = 'shane@usgs.gov';
-  $admin = 'shaefner@usgs.gov';
+  //$admin = 'shaefner@usgs.gov';
   $headers = "MIME-Version: 1.0" . "\r\n";
   $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
   $headers .= "From: shaefner@usgs.gov";
