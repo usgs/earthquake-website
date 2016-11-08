159a7a2e38ee72b8ade8a601a1f1716c86b69906
tkalin-usgs <tkalin@usgs.gov>
2016-11-08 12:25:51 -0700

    Two index.php pages for monitoring sections

---
+++ src/htdocs/monitoring/volunteer/index.php
@@ -0,0 +1,35 @@
+<?php
+      //	Author:	Tiffany Kalin
+      //	Contact:
+      //	Last	modified:	11/3/2016
+      if	(!isset($TEMPLATE))	{
+      $TITLE	=	'Volunteer Monitoring';
+      $NAVIGATION	= true;
+      include	'template.inc.php';
+      }
+?>
+
+<p>
+	Opportunities for you to host a seismometer in your private home, business, public building or school.
+</p>
+
+<ul class="linklist">
+	<li>
+    <a href="/monitoring/netquakes/">
+      <h4>NetQuakes Volunteer Monitoring</h4>
+      <img src="/monitoring/images/citizen.jpg" alt="citizen"/>
+    </a>
+    <p>
+			Instruments installed in private homes, businesses, public buildings and schools in locations around the US and World.
+    </p>
+  </li>
+	<li>
+		<a href="/research/sfbayeffects_waves/volunteer.php">
+			<h4>Volunteer Monitoring - Walnut Creek/Pleasant Hill/Concord, CA, Area Seismograph Array</h4>
+			<img src="/research/sfbayeffects_waves/images/Picture1_thumb.gif" alt="Bay Area Waves"/>
+		</a>
+		<p>
+			The USGS is requesting volunteers in the greater Walnut Creek/Pleasant Hill/Concord area to consider hosting a temporary seismic station for a period of two to three years for the purpose of studying site response in this part of the Bay area. All participation is strictly voluntary.
+		</p>
+	</li>
+</ul>
