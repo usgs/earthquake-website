93638216647eecb68fe1bc877b88ce1dee0bafa9
lisa-usgs <lisa@usgs.gov>
2016-11-09 14:06:23 -0700

    changed default contact to SIS form

--- src/htdocs/contactus/index.php
+++ src/htdocs/contactus/index.php
@@ -3,7 +3,8 @@
 // Contact: Lisa Wald, lisa@usgs.gov
 // Last modified: 11/24/2015
 if (!isset($TEMPLATE)) {
-  $TITLE = 'Contact Us/Regional Science Activities';
+  $TITLE = 'Contacts and Regional Science Activities';
+  $CONTACT = 'lisa@usgs.gov';
   $HEAD = '<link rel="stylesheet" href="/lib/hazdev-svgimagemap.css"></script>' .
   $FOOT = '<script src="/lib/hazdev-svgimagemap.js"></script>' .
       '<script src="index.js"></script>';
@@ -31,26 +32,23 @@ if (!isset($TEMPLATE)) {
     	<li><a href="pasadena/">Pasadena Field Office, Pasadena, CA</a></li>
     	<li><a href="albuquerque/">Albuquerque Seismological Laboratory</a></li>
     </ul>
-    <h3>Questions About Earthquakes</h3>
-    <ul>
-      <li><a href="media.php">Contacts and Information for the Media</a></li>
-      <li><a href="mailto:sis_eq_questions@usgs.gov">Earthquake Questions and Website Feedback</a></li>
-    </ul>
+    <p>&nbsp;</p>
   </div>
 
   <div class="column one-of-two">
-
     <div class="EQoffices"></div>
+
+    <h3>For General Public and Media</h3>
+    <ul>
+      <li><a href="media.php">Media Contacts and Information</a></li>
+      <li>Earthquake Questions or Website Comments - Use the <a href="https://answers.usgs.gov/cgi-bin/gsanswers">Questions or comments?</a> link at the bottom of every webpage.</li>
+    </ul>
 
     <h3>Looking for Something Else?</h3>
     <ul>
       <li><a href="http://store.usgs.gov/">USGS Maps and Publications</a></li>
       <li><a href="http://www.usgs.gov/ohr/">USGS Employment Opportunities</a></li>
       <li><a href="http://www.usgs.gov/">USGS - Everything!</a></li>
-    </ul>
-    <h3>Web Team</h3>
-    <ul>
-      <li><a href="webteam.php">Web Team</a></li>
     </ul>
 
   </div>
