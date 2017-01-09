38280ff0af0462d71d3a44ee80bf751827528013
Scott Haefner <shaefner@usgs.gov>
2017-01-09 12:26:59 -0800

    check if submit button exists

--- src/htdocs/contactus/menlo/workshop/index.js
+++ src/htdocs/contactus/menlo/workshop/index.js
@@ -3,11 +3,12 @@
 (function() {
 
   document.addEventListener('DOMContentLoaded', function() {
-
-    var inputs = document.querySelectorAll('.field input');
+    var inputs,
+        submit;
 
     // add / remove 'focus' class to highlight form field user is currently interacting with
     // also add 'interacted' class to highlight validation issues on only "touched" inputs
+    inputs = document.querySelectorAll('.field input');
     for (var i = 0; i < inputs.length; i ++) {
       inputs[i].addEventListener('focus', function() {
         var div = this.parentNode;
@@ -21,12 +22,15 @@
     }
 
     // add 'interacted' class to all input elements after user tries to submit form
-    document.getElementById('submit').addEventListener('click', function() {
-      for (var i = 0; i < inputs.length; i ++) {
-        var div = inputs[i].parentNode;
-        div.classList.add('interacted');
-      }
-    });
+    submit = document.getElementById('submit');
+    if (submit) {
+      submit.addEventListener('click', function() {
+        for (var i = 0; i < inputs.length; i ++) {
+          var div = inputs[i].parentNode;
+          div.classList.add('interacted');
+        }
+      });
+    }
 
   }, false);
 
