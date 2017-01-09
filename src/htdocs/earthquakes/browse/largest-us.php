f5308712258964bd1e0943898234044503101f71
lisa-usgs <lisa@usgs.gov>
2017-01-09 15:00:13 -0700

    completed top 28 US EQs list but some discrepancies to resolve

--- src/htdocs/earthquakes/browse/largest-us.php
+++ src/htdocs/earthquakes/browse/largest-us.php
@@ -1,9 +1,8 @@
 <?php
   // Author: Lisa Wald
   // Contact: Paul Earle,
-  // Last modified:
   if (!isset($TEMPLATE)) {
-  $TITLE = '10 Largest Earthquakes in the United States';
+  $TITLE = '28 Largest Earthquakes in the United States';
   $NAVIGATION = true;
   include 'template.inc.php';
   }
@@ -12,72 +11,270 @@
 <table>
 
   <tr>
-    <th>&nbsp;</th>
-    <th>Location</th>
-    <th>Date Time (UTC)</th>
+    <th> </th>
     <th>Magnitude</th>
+    <th>Location</th>
+    <th>Date</th>
+    <th>Time (UTC)</th>
+    <th>Latitude</th>
+    <th>Longitude</th>
   </tr>
 
   <tr>
     <td><b>1.</b></td>
-    <td><a href="/earthquakes/eventpage/iscgem869809">1964 Great Alaska Earthquake</a></td>
-    <td>1964 03 28 03:36:14.0&nbsp;UTC</td>
     <td>9.3</td>
+    <td><a href="/earthquakes/events/alaska1964/">1964 Great Alaska Earthquake</a></td>
+    <td>1964-03-28</td>
+    <td>03:36:14</td>
+    <td></td>
+    <td></td>
   </tr>
     <tr>
     <td><b>2.</b></td>
-    <td><a href="/earthquakes/states/events/1700_01_26.php">Cascadia subduction zone</a></td>
-    <td>1700 01 26 &nbsp;UTC</td>
     <td>&#152;9</td>
+    <td>Cascadia Subduction Zone</td>
+    <td>1700-01-26</td>
+    <td></td>
+    <td></td>
+    <td></td>
   </tr>
   <tr>
     <td><b>3.</b></td>
-    <td><a href="/earthquakes/eventpage/iscgem859206">Rat Islands, Alaska</a></td>
-    <td>1965 02 04 05:01&nbsp;UTC</td>
     <td>8.7</td>
+    <td><a href="/earthquakes/eventpage/iscgem859206">Rat Islands, Alaska</a></td>
+    <td>1965-02-04</td>
+    <td>05:01</td>
+    <td>51.251&deg;N</td>
+    <td>178.715&deg;E</td>
   </tr>
   <tr>
     <td><b>4.</b></td>
-    <td><a href="/earthquakes/eventpage/iscgem886030">Andreanof Islands, Alaska</a></td>
-    <td>1957 03 09 14:22:31.9&nbsp;UTC</td>
     <td>8.6</td>
+    <td><a href="/earthquakes/eventpage/iscgem886030">Andreanof Islands, Alaska</a></td>
+    <td>1957-03-09</td>
+    <td>14:22:31</td>
+    <td>51.499&deg;N</td>
+    <td>175.626&deg;W</td>
   </tr>
   <tr>
     <td><b>5.</b></td>
-    <td><a href="/earthquakes/eventpage/iscgem902774">East of Shumagin Islands, Alaska</a></td>
-    <td>1938 11 10 20:18:41.2&nbsp;UTC</td>
     <td> 8.3</td>
+    <td><a href="/earthquakes/eventpage/iscgem902774">Alaska Peninsula</a></td>
+    <td>1938-11-10</td>
+    <td>20:18:41</td>
+    <td>55.178&deg;N</td>
+    <td>158.181&deg;W</td>
   </tr>
   <tr>
     <td><b>6.</b></td>
-    <td><a href="/earthquakes/eventpage/iscgem898313">Unimak Islands, Alaska</a></td>
-    <td>1946 04 01 12:28&nbsp;UTC</td>
     <td>8.6</td>
+    <td><a href="/earthquakes/eventpage/iscgem898313">Unimak Islands, Alaska (S of Alaska)</a></td>
+    <td>1946-04-01</td>
+    <td>12:28</td>
+    <td>53.492&deg;N</td>
+    <td>162.832&deg;W</td>
   </tr>
   <tr>
     <td><b>7.</b></td>
-    <td><a href="/earthquakes/states/events/1899_09_10.php">Yakutat Bay, Alaska</a></td>
-    <td>1899 09 10 21:41&nbsp;UTC</td>
     <td>8.0</td>
+    <td>Yakutat Bay, Alaska</td>
+    <td>1899-09-10</td>
+    <td>21:41</td>
+    <td></td>
+    <td></td>
   </tr>
 
   <tr>
     <td><b>8.</b></td>
-    <td><a href="/earthquakes/eventpage/usp000bg0m">Denali Fault, Alaska</a></td>
-    <td>2002 11 03 22:12&nbsp;UTC</td>
     <td>7.9</td>
+    <td><a href="/earthquakes/eventpage/usp000bg0m">Denali Fault, Alaska (Central Alaska)</a></td>
+    <td>2002-11-03</td>
+    <td>22:12</td>
+    <td>63.517&deg;N</td>
+    <td>147.444&deg;W</td>
   </tr>
   <tr>
     <td><b>9.</b></td>
-    <td><a href="/earthquakes/eventpage/usp0003aq8">Gulf of Alaska, Alaska</a></td>
-    <td>1987 11 30 19:23&nbsp;UTC</td>
     <td>7.9</td>
+    <td><a href="/earthquakes/eventpage/usp0003aq8">Gulf of Alaska, Alaska</a></td>
+    <td>1987-11-30</td>
+    <td>19:23</td>
+    <td>58.679&deg;N</td>
+    <td>142.786&deg;W</td>
   </tr>
   <tr>
     <td><b>10.</b></td>
-    <td><a href="/earthquakes/eventpage/usp0002tmu"> Andreanof Islands, Alaska</a></td>
-    <td>1986 05 07 22:47&nbsp;UTC</td>
     <td>8.0</td>
+    <td><a href="/earthquakes/eventpage/usp0002tmu">Andreanof Islands, Alaska</a></td>
+    <td>1986-05-07</td>
+    <td>22:47</td>
+    <td>51.520&deg;N</td>
+    <td>174.776&deg;W></td>
+  </tr>
+  <tr>
+    <td><b>11.</b></td>
+    <td>7.9</td>
+    <td>Near Cape Yakataga, Alaska</td>
+    <td>1899-09-04</td>
+    <td>00:22</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>12.</b></td>
+    <td>7.9</td>
+    <td>Ka'u District, Island of Hawaii</td>
+    <td>1868-04-03</td>
+    <td>02:25</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>13.</b></td>
+    <td>7.9</td>
+    <td><a href="/earthquakes/events/1857sca/">The Great Fort Tejon, California Earthquake</a></td>
+    <td>1857-01-09</td>
+    <td>16:24</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>14.</b></td>
+    <td>7.8</td>
+    <td><a href="/earthquakes/eventpage/usp000cd1n">Rat Islands, Alaska</a></td>
+    <td>2003-11-17</td>
+    <td>06:43</td>
+    <td>51.146&deg;N</td>
+    <td>178.650&deg;E</td>
+  </tr>
+  <tr>
+    <td><b>15.</b></td>
+    <td>7.8</td>
+    <td><a href="/earthquakes/eventpage/usp0007jcu">Andreanof Islands, Alaska</a></td>
+    <td>1996-06-10</td>
+    <td>04:03</td>
+    <td>51.564&deg;N</td>
+    <td>177.632&deg;W</td>
+  </tr>
+  <tr>
+    <td><b>16.</b></td>
+    <td>7.8</td>
+    <td><a href="/regional/nca/1906/">1906 San Francisco, California Earthquake</a></td>
+    <td>1906-04-18</td>
+    <td>13:12</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>17.</b></td>
+    <td>7.8</td>
+    <td>Imperial Valley, California</td>
+    <td>1892-02-24</td>
+    <td>07:20</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>18.</b></td>
+    <td>7.7</td>
+    <td><a href="http://earthquake.usgs.gov/earthquakes/states/events/1811-1812.php#december_16">New Madrid, Missouri</a></td>
+    <td>1811-12-16</td>
+    <td>08:15</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>19.</b></td>
+    <td>7.7</td>
+    <td><a href="http://earthquake.usgs.gov/earthquakes/states/events/1811-1812.php#december_16">New Madrid, Missouri</a></td>
+    <td>1812-02-07</td>
+    <td>09:45</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>20.</b></td>
+    <td>7.5</td>
+    <td><a href="http://earthquake.usgs.gov/earthquakes/states/events/1811-1812.php#december_16">New Madrid, Missouri</a></td>
+    <td>1812-01-23</td>
+    <td>15:00</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>21.</b></td>
+    <td>7.4</td>
+    <td>Owens Valley, California</td>
+    <td>1872-03-26</td>
+    <td>10:30</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>22.</b></td>
+    <td>7.3</td>
+    <td><a href="/earthquakes/eventpage/ci3031111">Landers, California (N of Yucca Vly)</a></td>
+    <td>1992-06-28</td>
+    <td>11:57</td>
+    <td>34.200&deg;N</td>
+    <td>116.437&deg;W</td>
+  </tr>
+  <tr>
+    <td><b>23.</b></td>
+    <td>7.3</td>
+    <td><a href="/earthquakes/eventpage/iscgem882673">Hebgen Lake, Montana (Yellowstone Natl. Park)</a></td>
+    <td>1959-08-18</td>
+    <td>06:37</td>
+  </tr>
+  <tr>
+    <td><b>24.</b></td>
+    <td>7.3</td>
+    <td><a href="/earthquakes/eventpage/ci3319401">Kern County, California (WNW of Grapevine)</a></td>
+    <td>1952-07-21</td>
+    <td>11:52</td>
+    <td>34.958&deg;N</td>
+    <td>118.998&deg;W</td>
+  </tr>
+  <tr>
+    <td><b>25.</b></td>
+    <td>7.3</td>
+    <td>West of Eureka, California</td>
+    <td>1922-01-31</td>
+    <td>13:17</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>26.</b></td>
+    <td>7.3</td>
+    <td>Charleston, South Carolina</td>
+    <td>1886-09-01</td>
+    <td>02:51</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>27.</b></td>
+    <td>7.3</td>
+    <td>California - Oregon Coast</td>
+    <td>1873-11-23</td>
+    <td>05:00</td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td><b>28.</b></td>
+    <td>7.3</td>
+    <td>North Cascades, Washington</td>
+    <td>1872-12-15</td>
+    <td>05:40</td>
+    <td></td>
+    <td></td>
   </tr>
 
 </table>
+
+<h2>Note</h2>
+<p>
+  Widely differing magnitudes have been computed for some of these earthquakes; the values differ according to the methods and data used. The magnitudes and exact times in the list above may not currently match the magnitudes and times on the specific eventpages in the ComCat earthquake catalog since we are gathering catalog data from many sources.  We're working to determine the authoritative magnitude and time for these events so that the list and eventpages will represent the best information we can determine from the available data.
+</p>
