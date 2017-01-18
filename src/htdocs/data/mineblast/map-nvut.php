f5bb4b0ffa6ef32eef4305885e6eb55f86e0c51a
lisa-usgs <lisa@usgs.gov>
2017-01-18 15:15:58 -0700

    mining explosions pages

---
+++ src/htdocs/data/mineblast/map-nvut.php
@@ -0,0 +1,13 @@
+<?php
+// Author: Lisa Wald
+// Contact: Jim Dewey
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Routine Mining Seismicity, Nevada and Utah, May 1997 - March 2000';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<p>
+  <img src="images/nvut_w.gif" alt="Map of Nevada and Utah"/>
+</p>
--- src/htdocs/hazards/cf_header.php
+++ src/htdocs/hazards/cf_header.php
@@ -1,6 +1,7 @@
 <?php
 if (!isset($TEMPLATE)) {
 	$TITLE =  htmlspecialchars($_GET["name"]);
+  $NAVIGATIOM = true;
 	$HEAD = '<base href="https://earthquake.usgs.gov/hazards/">';
   include 'template.inc.php';
 }
