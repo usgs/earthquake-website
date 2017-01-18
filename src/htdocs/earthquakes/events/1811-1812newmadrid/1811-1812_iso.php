66378d9074417aab426ed549e91de2ba2f085819
tkalin-usgs <tkalin@usgs.gov>
2017-01-18 09:24:30 -0700

    New Madrid Earthquakes 1811-1812 pages

---
+++ src/htdocs/earthquakes/events/1811-1812newmadrid/1811-1812_iso.php
@@ -0,0 +1,30 @@
+<?php
+      //  Author:  Tiffany Kalin
+      //  Contact:
+      //  Last  modified:  1/18/2017
+      if  (!isset($TEMPLATE))  {
+      $TITLE  =  'New Madrid Earthquakes 1811-1812 Isoseismal Map';
+      $NAVIGATION  = true;
+      include  'template.inc.php';
+      }
+?>
+
+<p style="color: #00508F; font-weight: bold">
+  Isoseismal map for
+  the Arkansas earthquake
+  of December 16,1811, 08:15 UTC
+  <br />
+  (first of the 1811-1812 New Madrid series).
+</p>
+<p>
+  <img src="images/1811-1812_iso.gif" alt="Isoseismal map" longdesc="Isoseismal map" />
+</p>
+<p>
+  <small>
+  Abridged from
+  Seismicity of the United States, 1568-1989 (Revised),
+  by Carl W. Stover and Jerry L. Coffman,
+  U.S. Geological Survey Professional Paper 1527,
+  United States Government Printing Office, Washington: 1993.
+  </small>
+</p>
