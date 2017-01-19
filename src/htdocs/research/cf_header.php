de9778ac7b4d57ceddcddba56ab122657892fe83
lisa-usgs <lisa@usgs.gov>
2017-01-19 14:34:42 -0700

    adding left nav links on external grants

--- src/htdocs/research/cf_header.php
+++ src/htdocs/research/cf_header.php
@@ -7,7 +7,9 @@ if (!isset($TEMPLATE)) {
     echo "<a href='/research/external' class='up-one-level'>External Grants</a>",
 
     "<section>",
-
+      navItem("https://geohazards.usgs.gov/cfusion/external_grants/research.cfm", "Funded Research") .
+      navItem("https://geohazards.usgs.gov/cfusion/external_grants/networks.cfm", "Funded Networks") .
+      navItem("https://geohazards.usgs.gov/cfusion/external_grants/monitoring_upgrades.cfm", "ARRA-Funded Monitoring Upgrades") .
       navItem("/research/external/forpis.php", "Info for PIs") .
       navItem("/research/external/contact.php", "Contacts") ,
 
