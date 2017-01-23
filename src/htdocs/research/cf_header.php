8f62bc3817245e7bdca989d0646986c7e6cda4e5
emartinez-usgs <emartinez@usgs.gov>
2017-01-23 13:14:50 -0700

    Specifying navigation in footer. Skipping in header.

--- src/htdocs/research/cf_header.php
+++ src/htdocs/research/cf_header.php
@@ -1,20 +1,8 @@
 <?php
-include_once 'functions.inc.php';
 if (!isset($TEMPLATE)) {
   $TITLE = htmlspecialchars($_GET['name']);
   $HEAD = '<base href="https://earthquake.usgs.gov/research/">';
-  $NAVIGATION = '
-    <a href="/research/external" class="up-one-level">External Grants</a>
-    <section>
-  ' .
-      navItem('https://geohazards.usgs.gov/cfusion/external_grants/research.cfm', 'Funded Research') .
-      navItem('https://geohazards.usgs.gov/cfusion/external_grants/networks.cfm', 'Funded Networks') .
-      navItem('https://geohazards.usgs.gov/cfusion/external_grants/monitoring_upgrades.cfm', 'ARRA-Funded Monitoring Upgrades') .
-      navItem('/research/external/forpis.php', 'Info for PIs') .
-      navItem('/research/external/contact.php', 'Contacts') .
-    '
-  </section>
-    ';
+  $NAVIGATION = false;
   include 'template.inc.php';
 }
-  exit();
+exit();
