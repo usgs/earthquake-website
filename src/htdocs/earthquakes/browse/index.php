6c6670f7896a452c8f36b3f0245597c1dc79d958
lisa-usgs <lisa@usgs.gov>
2017-01-24 09:14:02 -0700

    removing link to top US list until an agreement is reached

--- src/htdocs/earthquakes/browse/index.php
+++ src/htdocs/earthquakes/browse/index.php
@@ -1,7 +1,6 @@
 <?php
   // Author: Lisa Wald
   // Contact: lisa@usgs.gov
-  // Last modified: 03/01/2016
   if (!isset($TEMPLATE)) {
   $current_year = intval(date('Y'));
   $TITLE = 'Earthquake Lists, Maps, and Statistics';
@@ -23,10 +22,11 @@
     <div class="alert">
       <h3>United States</h3>
       <ul class="no-style">
-        <!--  <li><a href="10-largest-us.php">10 Largest</a></li>-->
+      <!-- removed until an agreement is reached on the US List
         <li>
           <a href="/earthquakes/states/10_largest_us.php">Top 20 Largest</a>
         </li>
+      -->
         <li>
           <a href="/earthquakes/map/#%7B%22feed%22%3A%221461353942365%22%2C%22search%22%3A%7B%22id%22%3A%221461353942365%22%2C%22name%22%3A%22Search%20Results%22%2C%22isSearch%22%3Atrue%2C%22params%22%3A%7B%22starttime%22%3A%221900-01-01%2000%3A00%3A00%22%2C%22endtime%22%3A%222016-04-22%2023%3A59%3A59%22%2C%22maxlatitude%22%3A72.299%2C%22minlatitude%22%3A18.01%2C%22maxlongitude%22%3A-66.973%2C%22minlongitude%22%3A-195.645%2C%22minmagnitude%22%3A8%2C%22orderby%22%3A%22time%22%7D%7D%2C%22listFormat%22%3A%22default%22%2C%22sort%22%3A%22newest%22%2C%22basemap%22%3A%22terrain%22%2C%22autoUpdate%22%3Afalse%2C%22restrictListToMap%22%3Atrue%2C%22timeZone%22%3A%22utc%22%2C%22mapposition%22%3A%5B%5B-4.740675384778361%2C-188.7890625%5D%2C%5B77.46602847687329%2C-63.80859374999999%5D%5D%2C%22overlays%22%3A%7B%22plates%22%3Atrue%7D%2C%22viewModes%22%3A%7B%22map%22%3Atrue%2C%22list%22%3Atrue%2C%22settings%22%3Afalse%2C%22help%22%3Afalse%7D%7D">Magnitude 8+</a>
         </li>
