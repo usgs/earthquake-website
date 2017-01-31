efbb2447889315a524994b734c2c24ce2c815388
lisa-usgs <lisa@usgs.gov>
2017-01-31 16:59:42 -0700

    abc

---
+++ src/htdocs/learn/kids/abc/parents/index.php
@@ -0,0 +1,21 @@
+<?php
+// Author: Lisa Wald
+// Contact: Lucy Jones
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Earthquake ABC - Parents Guide';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<img src='/learn/kids/abc/images/eqbanner.gif' alt='Earthquake'/>
+<img src='/learn/kids/abc/images/abcanim.gif' alt='ABC'/>
+<br style="clear:left;"/>
+<img src='/learn/kids/abc/images/trace.gif' alt='seismogram'/>
+
+<h2>A Child's View of Earthquake Facts and Feelings<sup>&copy;</sup></h2>
+<h3>With a Parent's Guide to Earthquakes by Dr. Lucy Jones, USGS</h3>
+
+<?php
+include 'jumpmenu.inc.php';
+?>
