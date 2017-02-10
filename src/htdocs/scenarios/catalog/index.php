1350352128bf501e8711cc94e709bcba02f660ef
lisa-usgs <lisa@usgs.gov>
2017-02-10 09:57:53 -0700

    changed shakecast download link to email, and added direct link to bssc2014 map on scenario catalogs page

--- src/htdocs/scenarios/catalog/index.php
+++ src/htdocs/scenarios/catalog/index.php
@@ -5,8 +5,16 @@ if (!isset($TEMPLATE)) {
   $NAVIGATION = true;
   include 'template.inc.php';
 }
+?>
 
+<a href="map/"><h2>U.S. Map and List of Scenarios for Building Seismic Safety Council 2014 Event Set</h2></a>
+<p>
+  See <strong>bssc2014</strong> catalog link below for information about this scenario collection.
+</p>
 
+<h2>All Scenario Catalogs</h2>
+
+<?php
 include_once '../_functions.inc.php';
 $catalogs = get_json_metadata('.', 'index.json');
 
