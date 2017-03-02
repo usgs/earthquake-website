16a28ac112302b5fce18174df2d3d287d6564a00
Thompson <emthompson@usgs.gov>
2017-03-02 15:25:15 -0700

    New catalog for legacy global shakemaps

---
+++ src/htdocs/scenarios/catalog/gllegacy/index.php
@@ -0,0 +1,11 @@
+<?php
+include '../_catalog.inc.php';
+?>
+
+
+<h2 id="description">Description</h2>
+
+This catalog contains a selection of historical earthquake scenarios produced by the NEIC. Legacy catalogs such as this do not contain the level of detailed documentation required for newly created scenarios. These scenarios are maintained for archival purposes.
+
+
+
