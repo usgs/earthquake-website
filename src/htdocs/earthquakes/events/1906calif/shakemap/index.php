1a24103f105da719bf2719ca0c3805d5131d1aed
Scott Haefner <shaefner@usgs.gov>
2017-04-03 13:04:56 -0700

    initial commit

---
+++ src/htdocs/earthquakes/events/1906calif/shakemap/index.php
@@ -0,0 +1,108 @@
+<?php
+
+if (!isset($TEMPLATE)) {
+  $TITLE = '1906 San Francisco Earthquake ShakeMaps';
+  $HEAD = '<link rel="stylesheet" href="style.css" />';
+  $FOOT = '';
+
+  include 'template.inc.php';
+}
+
+?>
+
+<p><acronym title="Modified Mercali Intensity">MMI</acronym> maps for the 1906 San Francisco Earthquake plotted in ShakeMap format.</p>
+
+<ul class="no-style linklist">
+  <li>
+    <a href="img/1906_Boatwright_intensity.jpg">
+      <h3>California</h3>
+      <img src="img/full_thumb.jpg" alt="ShakeMap thumbnail">
+    </a>
+    <p>MMI ShakeMap of California for the 1906 San Francisco earthquake inferred from Lawson (1908) by Boatwright and Bundock (2005).</p>
+    <ul class="shakemap">
+      <li><a href="download/1906_Boatwright_intensity.ps.zip">Postscript</a></li>
+      <li><a href="download/1906_Boatwright_shape.zip">Shapefile</a></li>
+      <li><a href="download/1906_CalNeva_grid.xyz.zip">Intensity Grid</a></li>
+      <li><a href="download/stationlist.html">Site Table</a></li>
+      <li><a href="download/1906_CalNeva_hazus.zip">Shapefile for HAZUS</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="img/1906_Boatwright_NC_intensity.jpg">
+      <h3>North Coast</h3>
+      <img src="img/nc_thumb.jpg" alt="ShakeMap thumbnail">
+    </a>
+    <p>MMI ShakeMap of the North Coast for the 1906 San Francisco earthquake inferred from Lawson (1908) by Boatwright and Bundock (2005).</p>
+    <ul class="shakemap">
+      <li><a href="download/1906_Boatwright_NC_intensity.ps.zip">Postscript</a></li>
+      <li><a href="download/1906_Boatwright_shape.zip">Shapefile</a></li>
+      <li><a href="download/1906_CalNeva_grid.xyz.zip">Intensity Grid</a></li>
+      <li><a href="download/stationlist.html">Site Table</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="img/1906_Boatwright_SR_intensity.jpg">
+      <h3>North Bay</h3>
+      <img src="img/sr_thumb.jpg" alt="ShakeMap thumbnail">
+    </a>
+    <p>MMI ShakeMap of the North Bay for the 1906 San Francisco earthquake inferred from Lawson (1908) by Boatwright and Bundock (2005).</p>
+    <ul class="shakemap">
+      <li><a href="download/1906_Boatwright_SR_intensity.ps.zip">Postscript</a></li>
+      <li><a href="download/1906_Boatwright_shape.zip">Shapefile</a></li>
+      <li><a href="download/1906_CalNeva_grid.xyz.zip">Intensity Grid</a></li>
+      <li><a href="download/stationlist.html">Site Table</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="img/1906_Boatwright_BA_intensity.jpg">
+      <h3>San Francisco Bay Area</h3>
+      <img src="img/ba_thumb.jpg" alt="ShakeMap thumbnail">
+    </a>
+    <p>MMI ShakeMap of the Bay Area for the 1906 San Francisco earthquake inferred from Lawson (1908) by Boatwright and Bundock (2005).</p>
+    <ul class="shakemap">
+      <li><a href="download/1906_Boatwright_BA_intensity.ps.zip">Postscript</a></li>
+      <li><a href="download/1906_Boatwright_shape.zip">Shapefile</a></li>
+      <li><a href="download/1906_CalNeva_grid.xyz.zip">Intensity Grid</a></li>
+      <li><a href="download/stationlist.html">Site Table</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="img/1906_Boatwright_PN_intensity.jpg">
+      <h3>San Francisco Peninsula</h3>
+      <img src="img/pn_thumb.jpg" alt="ShakeMap thumbnail">
+    </a>
+    <p>MMI ShakeMap of the SF Peninsula for the 1906 San Francisco earthquake inferred from Lawson (1908) by Boatwright and Bundock (2005).</p>
+    <ul class="shakemap">
+      <li><a href="download/1906_Boatwright_PN_intensity.ps.zip">Postscript</a></li>
+      <li><a href="download/1906_Boatwright_shape.zip">Shapefile</a></li>
+      <li><a href="download/1906_CalNeva_grid.xyz.zip">Intensity Grid</a></li>
+      <li><a href="download/stationlist.html">Site Table</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="img/1906_Boatwright_S_intensity.jpg">
+      <h3>Central Coast</h3>
+      <img src="img/s_thumb.jpg" alt="ShakeMap thumbnail">
+    </a>
+    <p>MMI ShakeMap of the Central Coast for the 1906 San Francisco earthquake inferred from Lawson (1908) by Boatwright and Bundock (2005).</p>
+    <ul class="shakemap">
+      <li><a href="download/1906_Boatwright_S_intensity.ps.zip">Postscript</a></li>
+      <li><a href="download/1906_Boatwright_shape.zip">Shapefile</a></li>
+      <li><a href="download/1906_CalNeva_grid.xyz.zip">Intensity Grid</a></li>
+      <li><a href="download/stationlist.html">Site Table</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="img/1906_Scenario_intensity.jpg">
+      <h3>Predicted</h3>
+      <img src="img/1906_scenario_thumb.jpg" alt="ShakeMap thumbnail">
+    </a>
+    <p>MMI ShakeMap of California for the 1906 San Francisco earthquake predicted from Boore et al. (1997) regressions by Working Group 2002.</p>
+    <ul class="shakemap">
+      <li><a href="download/1906_Scenario_intensity.ps">Postscript</a></li>
+      <li><a href="download/1906_Scenario_shape.zip">Shapefile</a></li>
+      <li><a href="download/1906_Scenario_grid.xyz.zip">Intensity Grid</a></li>
+      <li><a href="download/1906_Scenario_intensity.hazus.zip">Shapefile for HAZUS</a></li>
+    </ul>
+  </li>
+</ul>
