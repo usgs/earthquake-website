28ec48d669251f0fe9f9870fb928fbb1ea5eaada
jtaggart-berkeleyseismo <jennifer@seismo.berkeley.edu>
2017-04-25 15:34:30 -0700

    Added Mission, ShakeMaps link, Contributing Networks, changed Regional Partners for NC contributor page.

--- src/htdocs/data/comcat/contributor/nc/index.php
+++ src/htdocs/data/comcat/contributor/nc/index.php
@@ -4,19 +4,37 @@ include '../_contributor.inc.php';
 
 <h2 id="partners">Regional Partners</h2>
 <ul>
-  <li><a href="http://www.seismo.berkeley.edu/seismo" target="_blank">Berkeley Seismological Lab</a></li>
+  <li><a href="http://www.earthquakes.berkeley.edu/" target="_blank">Berkeley Seismological Lab</a></li>
   <li><a href="http://www.conservation.ca.gov/cgs/" target="_blank">California Geological Survey</a></li>
-  <li><a href="http://www.caltech.edu/content/marketing-and-communications" target="_blank">Caltech Media Relations</a></li>
   <li><a href="http://www.seismolab.caltech.edu/" target="_blank">Caltech Seismological Laboratory</a></li>
-  <li><a href="http://www.cisn.org/" target="_blank">CISN - California Integrated Seismic Network</a></li>
+  <li><a href="http://www.cisn.org/" target="_blank">California Integrated Seismic Network</a></li>
+  <li><a href="https://earthquake.usgs.gov/contactus/menlo/" target="_blank">USGS Menlo Park</a></li>
+  <li><a href="https://earthquake.usgs.gov/contactus/pasadena/" target="_blank">USGS Pasadena</a></li>
+</ul>
+
+<h2 id="contributingNetworks"><a href="http://www/ncedc/org/ncss/index.html">Contributing Networks</a></h2>
+<ul>
+  <li>California Department of Water Resources</li>
+  <li>Calpine</li>
+  <li>Lawrence Berkeley Laboratory</li>
+  <li>Pacific Gas &amp; Electric</li>
+  <li>University of California Santa Barbara</li>
+  <li>University of California San Diego</li>
+  <li>University of Nevada, Reno</li>
+  <li>University of Oregon</li>
+  <li>University of Washington</li>
+</ul>
+
+<h2 id="shakeMaps">ShakeMaps</h2>
+<ul>
+  <li><a href="http://www.cisn.org/shakemap.html">ShakeMaps at the CISN</a></li>
 </ul>
 
 <h2 id="mission">Mission</h2>
 <p>
-    The mission of the California Integrated Seismic Network (CISN) is to operate
-     a reliable, modern, statewide system for earthquake monitoring, research,
-     archiving, and distribution of information for the benefit of public safety,
-     emergency response, and loss mitigation. Further, the CISN seeks to mitigate
-     the impact of future earthquakes by collecting, processing, and disseminating
-     critical earthquake information in a timely way.
+    The mission of the California Integrated Seismic Network (CISN) is to operate a reliable, modern, statewide system for 
+    earthquake monitoring, research, archiving, and distribution of information for the benefit of public safety, emergency 
+    response, and loss mitigation. Further, the CISN seeks to mitigate the impact of future earthquakes by collecting, 
+    processing, and disseminating critical earthquake information in a timely way.
 </p>
+
