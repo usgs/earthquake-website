a346bdb2a7e9688ecc62f5f48fc618035c5d1822
lisa-usgs <lisa@usgs.gov>
2017-06-02 14:22:32 -0600

    added more staff to Pasadena staff page

--- src/htdocs/contactus/pasadena/pasadenastaff.php
+++ src/htdocs/contactus/pasadena/pasadenastaff.php
@@ -8,95 +8,167 @@
 
 <h2>Contacts for Southern California</h2>
 <table class="tabular responsive">
-	<thead>
-		<tr>
-		  <th>Name</th>
-		  <th>E-mail</th>
-		</tr>
-	</thead>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>E-mail</th>
+    </tr>
+  </thead>
 
-	<tbody>
-		<tr>
-		  <td>Edna Anjal</td>
-		  <td><a href= "mailto:anjal@usgs.gov">anjal@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Aris Aspiotes</td>
-		  <td><a href= "mailto:aaspiotes@usgs.gov">aaspiotes@usgs.gov</td>
-		</tr>
-		<tr>
-		  <td>Elizabeth Cochran</td>
-		  <td><a href= "mailto:ecochran@usgs.gov">ecochran@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Jason De Cristofaro</td>
-		  <td><a href= "mailto:jdecristofaro@usgs.gov">jdecristofaro@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Sandra Dedeaux</td>
-		  <td><a href= "mailto:sdedeaux@usgs.gov">sdedeaux@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Dan Determan</td>
-		  <td><a href= "mailto:ddeterman@usgs.gov">ddeterman@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Karen Felzer</td>
-		  <td><a href= "mailto:kfelzer@usgs.gov">kfelzer@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Doug Given</td>
-		  <td><a href= "mailto:doug@usgs.gov">doug@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Rob Graves</td>
-		  <td><a href= "mailto:rwgraves@usgs.gov">rwgraves@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Susan Hough</td>
-		  <td><a href= "mailto:hough@usgs.gov">hough@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Ken Hudnut</td>
-		  <td><a href= "mailto:hudnut@usgs.gov">hudnut@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Katherine Kendrick</td>
-		  <td><a href= "mailto:kendrick@usgs.gov">kendrick@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Scott Lydeen</td>
-		  <td><a href= "mailto:scotty@gps.caltech.edu">scotty@gps.caltech.edu</a></td>
-		</tr>
-		<tr>
-		  <td>Morgan Page</td>
-		  <td><a href= "mailto:mpage@usgs.gov">mpage@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Sue Perry</td>
-		  <td><a href= "mailto:scperry@usgs.gov">scperry@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Kate Scharer</td>
-		  <td><a href= "mailto:kscharer@usgs.gov">kscharer@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Stan Schwarz</td>
-		  <td><a href= "mailto:stans@gps.caltech.edu">stans@gps.caltech.edu</a></td>
-		</tr>
-		<tr>
-		  <td>Thurman Dave Sutton</td>
-		  <td><a href= "mailto:dsutton@gps.caltech.edu">dsutton@gps.caltech.edu</a></td>
-		</tr>
-		<tr>
-		  <td>Valerie Thomas</td>
-		  <td><a href= "mailto:vthomas@usgs.gov">vthomas@usgs.gov</a></td>
-		</tr>
-		<tr>
-		  <td>Alan Yong</td>
-		  <td><a href= "mailto:yong@usgs.gov">yong@usgs.gov</a></td>
-		</tr>
-	</tbody>
+  <tbody>
+    <tr>
+      <td>Mark Alvarez</td>
+      <td><a href= "mailto:malvarez@usgs.gov">malvarez@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Edna Anjal</td>
+      <td><a href= "mailto:anjal@usgs.gov">anjal@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Aris Aspiotes</td>
+      <td><a href= "mailto:aaspiotes@usgs.gov">aaspiotes@usgs.gov</td>
+    </tr>
+    <tr>
+      <td>Glenn Biasi</td>
+      <td><a href= "mailto:gbiasi@usgs.gov">gbiasi@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Alexis "Alex" Cadriao</td>
+      <td><a href= "mailto:acadiao@usgs.gov">acadiao@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Elizabeth Cochran</td>
+      <td><a href= "mailto:ecochran@usgs.gov">ecochran@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Eunice Cuadros</td>
+      <td><a href= "mailto:ecuadros@usgs.gov">ecuadros@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Donyelle Davis</td>
+      <td><a href= "mailto:dkdavis@usgs.gov">dkdavis@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Jason De Cristofaro</td>
+      <td><a href= "mailto:jdecristofaro@usgs.gov">jdecristofaro@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Robert deGroot</td>
+      <td><a href= "mailto:rdegroot@usgs.gov">rdegroot@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Sandra Dedeaux</td>
+      <td><a href= "mailto:sdedeaux@usgs.gov">sdedeaux@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Daniel Determan</td>
+      <td><a href= "mailto:ddeterman@usgs.gov">ddeterman@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Sara Dougherty</td>
+      <td><a href= "mailto:sdougherty@usgs.gov">sdougherty@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Karen Felzer</td>
+      <td><a href= "mailto:kfelzer@usgs.gov">kfelzer@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Gary Gann</td>
+      <td><a href= "mailto:agann@usgs.gov">agann@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Doug Given</td>
+      <td><a href= "mailto:doug@usgs.gov">doug@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Rob Graves</td>
+      <td><a href= "mailto:rwgraves@usgs.gov">rwgraves@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Steve Guiwits</td>
+      <td><a href= "mailto:sguiwits@usgs.gov">sguiwits@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Susan Hough</td>
+      <td><a href= "mailto:hough@usgs.gov">hough@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Ken Hudnut</td>
+      <td><a href= "mailto:hudnut@usgs.gov">hudnut@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Buck Jones</td>
+      <td><a href= "mailto:bajones@usgs.gov">bajones@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Katherine Kendrick</td>
+      <td><a href= "mailto:kendrick@usgs.gov">kendrick@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Kate Long</td>
+      <td><a href="mailto:Kate.Long@caloes.ca.gov">Kate.Long@caloes.ca.gov</a></td>
+    </tr>
+    <tr>
+      <td>Scott Lydeen</td>
+      <td><a href= "mailto:scotty@gps.caltech.edu">scotty@gps.caltech.edu</a></td>
+    </tr>
+    <tr>
+      <td>Devin McPhillips</td>
+      <td><a href= "mailto:dmcphillips@usgs.gov">dmcphillips@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>David Nget</td>
+      <td><a href= "mailto:dnget@usgs.gov">dnget@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Morgan Page</td>
+      <td><a href= "mailto:mpage@usgs.gov">mpage@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Sue Perry</td>
+      <td><a href= "mailto:scperry@usgs.gov">scperry@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Jennifer Pfau</td>
+      <td><a href= "mailto:jpfau@usgs.gov">jpfau@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Kate Scharer</td>
+      <td><a href= "mailto:kscharer@usgs.gov">kscharer@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Stan Schwarz</td>
+      <td><a href= "mailto:sschwarz@usgs.gov">sschwarz@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Richard "Josh" Sessoms</td>
+      <td><a href= "mailto:rsessoms@usgs.gov">rsessoms@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Deborah Smith</td>
+      <td><a href= "mailto:deborahsmith@usgs.gov">deborahsmith@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Thurman Dave Sutton</td>
+      <td><a href= "mailto:dsutton@gps.caltech.edu">dsutton@gps.caltech.edu</a></td>
+    </tr>
+    <tr>
+      <td>Valerie Thomas</td>
+      <td><a href= "mailto:vthomas@usgs.gov">vthomas@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Nicholas Van Der Elst</td>
+      <td><a href= "mailto:nvanderelst@usgs.gov">nvanderelst@usgs.gov</a></td>
+    </tr>
+    <tr>
+      <td>Margaret Vinci</td>
+      <td><a href= "mailto:mvinci@gps.caltech.edu">mvinci@gps.caltech.edu</a></td>
+    </tr>
+    <tr>
+      <td>Alan Yong</td>
+      <td><a href= "mailto:yong@usgs.gov">yong@usgs.gov</a></td>
+    </tr>
+  </tbody>
 </table>
 
 <hr/>
@@ -105,23 +177,23 @@
 
 <p><strong>From the 134/210 Freeway:</strong></p>
 <p>
-	Take the 134 East and follow the signs to Pasadena (210E).<br/>
+  Take the 134 East and follow the signs to Pasadena (210E).<br/>
   Exit at Lake Ave. and go right (South) for several city blocks.<br/>
   Turn left on Del Mar.<br/>
   Continue below from Del Mar.
 </p>
 <p><strong>From the 110 Pasadena Freeway:</strong></p>
 <p>
-	Take the 110N until it ends in Pasadena.<br/>
+  Take the 110N until it ends in Pasadena.<br/>
   Keep going straight (on Arroyo which is what the freeway turns into).<br/>
   Turn right (East) on Del Mar.<br/>
   Continue below from Del Mar.<br/>
 </p>
 <p>From Del Mar:</p>
 <p>
-	Turn right on Wilson Ave. Go through the stop sign at San Pasqual, and the
-	office is a few houses down on the right. The address is 525 S. Wilson and the
-	house is yellow with black shutters.<br/>
-	The &quot;Geological Survey&quot;
-	house is #39 on the <a href="http://www.caltech.edu/map">Caltech campus map</a>.
+  Turn right on Wilson Ave. Go through the stop sign at San Pasqual, and the
+  office is a few houses down on the right. The address is 525 S. Wilson and the
+  house is yellow with black shutters.<br/>
+  The &quot;Geological Survey&quot;
+  house is #39 on the <a href="http://www.caltech.edu/map">Caltech campus map</a>.
  </p>
