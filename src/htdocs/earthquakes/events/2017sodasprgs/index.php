4a3431343e3a6e600726d0da936a9c1baac9fe53
lisa-usgs <lisa@usgs.gov>
2017-09-15 10:22:45 -0600

    soda springs initial work

---
+++ src/htdocs/earthquakes/events/2017sodasprgs/index.php
@@ -0,0 +1,111 @@
+<?php
+// Author: Lisa Wald
+// Contact: Bill Yeck
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'M5.3 2017 Soday Springs, Idaho Sequence';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<p>
+  On September 2, 2017, there was a M5.3 earthquake east of Soda Springs, Idaho. That earthquake has kicked off a sustained and highly active aftershock sequence. It caused moderate shaking over a broad area of southeastern Idaho, northern Utah, and western Wyoming.
+</p>
+
+<p>
+  This earthquake has generated a number of other earthquakes (aftershocks), more than we would typically observe for other earthquakes of this size. Because of this, we are providing an aftershock forecast that we'll update regularly.
+</p>
+<p>What we understand, in terms of future larger earthquakes in this area is:
+<ul>
+  <li>When there are more earthquakes, the chance of a large earthquake is greater, which means that the chance of damage is greater.
+  </li>
+  <li>Idaho and Montana get earthquakes. While it is not that common, it is also not surprising to see earthquakes in these areas.
+  </li>
+  <li>Because of the increased probability of damaging earthquakes, we advise people to listen to the advice of your (State/County/City) emergency management office.
+  </li>
+  <li>This sequence may have damaging earthquakes in the future, so remember to: Drop, Cover, and Hold on.
+  </li>
+</ul>
+
+<h2>About the Earthquake</h2>
+<p>
+  The M5.3 earthquake occurred as the result of normal faulting within the shallow crust, on a fault dipping at an intermediate angle either to the west, or to the east. This faulting style is typical of earthquakes located in the Intermountain Seismic Belt (ISB), a prominent north-south-trending zone of seismicity in the Intermountain West, and a region of moderate-to-high seismic hazard. The ISB is characterized by movement along north-trending, east- and west-dipping range-bounding normal faults that accommodate gradual horizontal extension of the Earth’s crust.
+</p>
+<p>
+  Earthquakes occur frequently in the ISB. It is unlikely that this sequence is related to oil and gas operations, or to the Yellowstone volcanic region over 200 km to the northeast.
+</p>
+<h2>Past Earthquakes in This Area</h2>
+<p>
+  South and Central Idaho have experienced at least 14 other M5+ earthquakes within 300 km of the September 2, 2017 earthquake over the preceding century. The largest was the October 28, 1983, <a href="/earthquakes/eventpage/usp0001zbv">M6.9 Borah Peak earthquake</a>, which struck about 250 km to the northwest of the September 2, 2017 earthquake. The Borah Peak earthquake is the largest known to have occurred in Idaho, and resulted in two fatalities, two injuries, and considerable damage in Challis, Idaho.
+</p>
+<h2>Aftershock Forecasts and Aftershock Forecast Scenarios</h2>
+<p>
+  The <a href="/earthquakes/eventpage/us2000aekg">M5.3 Soda Springs</a> Sequence is particularly active, producing more earthquakes on average than other earthquakes of this magnitude. Sequences can be defined as a lot of earthquakes that occur in and around a similar area over a short period of time. Occasional active sequences are not unusual for this geographic region; a number of places get sequences and typically, they diminish in number over time.
+</p>
+<h3>About Aftershock Forecasts</h3>
+<p>
+  Due to the active and ongoing nature of this sequence, we have developed an earthquake forecast for continuing seismicity. No one can predict the exact time or place of any earthquake, including aftershocks. What our earthquake forecasts do is give us an understanding of the chances of having more earthquakes within a given time period. We calculate this earthquake forecast using a statistical analysis based on past earthquakes in similar tectonic environments, as well as the aftershocks recorded to date for this sequence.
+</p>
+<p>
+  Our forecast changes as time passes due to the decay in the frequency of aftershocks, larger aftershocks that reinvigorate the sequence, and changes in forecast modeling based on the earthquake data collected.
+</p>
+<p>
+  According to our forecast (as of Sep 14 at 10am Mountain time), over the next week there is a:
+</p>
+<ul>
+  <li>
+    6-16% chance of one or more earthquakes that are larger than the largest earthquake so far in this sequence (M5.3). It is likely that there will smaller earthquakes, with 0-40 magnitude 3.5 or higher aftershocks, which are large enough to be felt nearby.
+  </li>
+</ul>
+<h3>About Aftershock Forecast Scenarios</h3>
+<p>
+  While no one can scientifically predict earthquakes, we have developed forecast models that can give us a good indication of what could occur in the future. Similar to weather models, our models can change with more data and information. The longer this sequence goes on, the more we learn about what it will do in the future.
+</p>
+<p>
+  We have taken the detailed forecast (see below) and developed the three scenarios that we consider to be the most likely. These scenarios are for the following week only, but earthquakes will continue to be possible at later times. We will update the forecast as more information becomes available.
+</p>
+<p>
+  Here are the three scenarios or possibilities for the next week, based on earthquake forecast models:
+</p>
+<ol>
+  <li>
+    <strong>Scenario #1 (most likely: 80-90% chance):</strong><br/>
+    The sequence will continue to decay over the next week, which means there will be fewer earthquakes. Occasional spikes in activity may be accompanied by additional M4-M5 earthquakes. While all earthquake sequences decay over time, there are several other possible outcomes, which we will now explore.
+  </li>
+  <li>
+    <strong>Scenario #2 (less likely than Scenario One but possible with 10-20% chance):</strong><br />
+     A similar sized or larger earthquake than the M5.3 main shock (link) may occur. This situation is often referred to as a “doublet” when a similar sized earthquake follows the original earthquake that kicked off the sequence. Doublets have occurred in places around the world, but they are not very common.
+  </li>
+  <li>
+    <strong>Scenario #3 (the least likely scenario but still possible: <1% chance):</strong><br />
+    A much larger earthquake than M5.3 could occur, up to and including the M7.0 range. There is a small chance that this sequence could include a much larger earthquake (in which case we would call it a foreshock sequence). We have seen this happen in other places around the world, with the most notable being L’Aquila, Italy in 2009. It is important to understand that this is not the most likely scenario, but we cannot ignore the possibility of this occurring.
+  </li>
+</ol>
+<h2>What You Can Do About the Earthquakes</h2>
+<p>
+  If you feel an earthquake, remember to: Drop, Cover, and Hold On. An earthquake may feel small at the beginning but can build in shaking intensity very quickly, so it is best to take the precaution in small earthquakes, as well as larger ones.  Preparing for earthquakes is also important - to find out more, go here: (links). Earthquakes don’t give us much warning that they are coming, so it is important to be prepared for these to occur.
+</p>
+<h2>What Scientists are Doing to Learn More About These Earthquakes</h2>
+<p>
+  Due to its remote location, there is a lot to be learned yet about this sequence. Scientists and students at Utah State University, University of Idaho, and other institutions are working with us to get a better understanding of the systems and mechanisms causing these earthquakes. We are assisting with a program of research to determine areas that are most effected, any changes in local hot springs, and other issues.
+</p>
+<h2>What Fault is Causing All This Shaking?</h2>
+<p>
+  We aren’t certain which fault these earthquakes are occurringed on, but we have some strong suspects. The specific fault responsible for this earthquake and the subsequent aftershocks is currently under study by the USGS, and scientists at Utah State University.  Science investigation teams are currently assembling to getting out in the field to find out more.
+</p>
+<p>
+  The geology of this area is intriguing and complex. The M5.3 earthquake and subsequent aftershocks occurred beneath the Aspen Range of southeastern Idaho, approximately 15 km (9.3 mi) SE of Soda Springs, Idaho, and ??? km (??? mi) north of Montpelier, Idaho. The basic geology of the region consists of folded and thrust-faulted rocks of Permian to Cretaceous age. These thrust faults shifted these rocks from west to east at approximately 100 million years ago and create the mountains from southeastern Idaho to western Wyoming a region termed the Idaho-Wyoming-Utah fold and thrust belt.  Subsequent to this compression, the area has been subjected to east-west extension that has created active normal faults.
+</p>
+<p>
+  To the southwest, these faults are clearly seen (the Wasatch Fault, East/West Cache Faults and the East/West Bear Lake Faults). The West and East Bear Lake faults probably continue north of the lake. These two faults are responsible for the formation of these valleys, including the valley through which the Bear River flows from Montpelier to Soda Springs.  These normal faults probably started moving several million years ago, and indicators of offset can be seen from Georgetown to east of Bear Lake. This sequence could have occurred on one of these faults.
+</p>
+<p>
+  However, another strong contender fault has an intriguing name: Slug Creek Fault. We don’t know what inspired USGS Geologist G.R. Mansfield, who mapped the fault in 1927, to give a fault such a moniker but the reason this fault is a strong suspect is that this fault is seen in the upper regions of the Aspen Range. This is a similar location as the M5.3. Further, slip to the west along a fault dips to the west and this would explain the focal mechanism of the main shock, meaning the earthquake certainly acts similar to another earthquake on this fault would.
+</p>
+<p>
+  So, there are number of suspects in determining which fault is the main culprit. In the coming weeks and months, we aim to crack this intriguing geological case.
+</p>
+<h2>For More Information</h2>
+<ul>
+  <li></li>
+</ul>
