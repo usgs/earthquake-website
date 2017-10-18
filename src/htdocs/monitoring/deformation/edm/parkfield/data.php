423de84a33444b59295bb325d097b835c377f327
Scott Haefner <shaefner@usgs.gov>
2017-10-18 12:06:09 -0700

    fix broken links; add data files

---
+++ src/htdocs/monitoring/deformation/edm/parkfield/data.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!isset($TEMPLATE)) {
+  $TITLE = 'Data from the Parkfield two-color EDM network';
+  $HEAD = '';
+  $FOOT = '';
+  $NAVIGATION = true;
+
+  include 'template.inc.php';
+}
+
+?>
+
+<h2>Data Files</h2>
+
+<ul>
+  <li><a href="data/bare1.dat">BARE1</a></li>
+  <li><a href="data/buck1.dat">BUCK1</a></li>
+  <li><a href="data/cann3.dat">CANN3</a></li>
+  <li><a href="data/cree2.dat">CREE2</a></li>
+  <li><a href="data/flat2.dat">FLAT2</a></li>
+  <li><a href="data/gold1.dat">GOLD1</a></li>
+  <li><a href="data/hogs2.dat">HOGS2</a></li>
+  <li><a href="data/hunt1.dat">HUNT1</a></li>
+  <li><a href="data/lang1.dat">LANG1</a></li>
+  <li><a href="data/masw1.dat">MASW1</a></li>
+  <li><a href="data/mels1.dat">MELS1</a></li>
+  <li><a href="data/mida1.dat">MIDA1</a></li>
+  <li><a href="data/midd3.dat">MIDD3</a></li>
+  <li><a href="data/mide1.dat">MIDE1</a></li>
+  <li><a href="data/nore1.dat">NORE1</a></li>
+  <li><a href="data/norm1.dat">NORM1</a></li>
+  <li><a href="data/pomm1.dat">POMM1</a></li>
+  <li><a href="data/pomo1.dat">POMO1</a></li>
+  <li><a href="data/tabl1.dat">TABL1</a></li>
+  <li><a href="data/todd2.dat">TODD2</a></li>
+  <li><a href="data/turk1.dat">TURK1</a></li>
+</ul>
