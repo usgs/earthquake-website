b388c1414b06956de53c025a7edbf53f19ade907
lisa-usgs <lisa@usgs.gov>
2017-12-14 13:58:48 -0700

    modifications to Research for Everyone to accomodate more each year

--- src/htdocs/research/everyone/_navigation.inc.php
+++ src/htdocs/research/everyone/_navigation.inc.php
@@ -1,16 +1,13 @@
 <?php
 $section = "/research/everyone";
 
-echo "<a href='/research/everyone' class='up-one-level'>Science for Everyone</a>",
+echo "<a href='/research/everyone/' class='up-one-level'>Science for Everyone</a>".
 
-  "<section>",
+  "<section>".
 
-    navItem("/learn/topics/paleo-intro/", "Inroduction to Paleoseismology: the Past Informs the Future") .
-    navItem("${section}/maine2012/", "Ground-Truthing After the M4.0 Earthquake in Southern Maine, October 16, 2012") .
-    navItem("${section}/nepal2015/", "The M7.8 Nepal Earthquake, 2015 &ndash; A Small Push to Mt. Everest") .
-    navItem("${section}/splays/", "The &quot;Snow Plow Theory&quot; of Early-Arriving Tsuamis") .
-    navItem("${section}/seismology-in-the-city/", "Seismology in the City") .
-    navItem("${section}/wasatch/", "How Big and How Frequent Are Earthquakes on the Wasatch Fault?") ,
+    navItem("${section}/2017/", "2017") .
+    navItem("${section}/2016/", "2016") .
+    navItem("${section}/2015/", "2015") .
 
   "</section>"
   ;
--- src/htdocs/research/everyone/index.php
+++ src/htdocs/research/everyone/index.php
@@ -3,68 +3,31 @@
 // Contact: Lisa Wald
   if (!isset($TEMPLATE)) {
     $TITLE = 'Science for Everyone';
-    $NAVIGATION = false;
-    $HEAD = '<link rel="stylesheet" href="styles.css"/>';
+    include_once 'functions.inc.php';
+    ob_start();
+    include '../_navigation.inc.php';
+    $NAVIGATION = ob_get_clean();
+    $HEAD = '<link rel="stylesheet" href="/research/everyone/styles.css"/>';
     include 'template.inc.php';
   }
 ?>
 
-    <ul class="no-style linklist everyone">
+<!--  Feeds are added to Highlights Feeds for now 12/2017
 
-      <li>
-        <a href="/learn/topics/safz-paleo/">
-          <img class="everyone-img" src="images/trench-xsect.gif" alt="cross-section with layers and a fault outlined"/>
-          <h4>Back to the Future on the San Andreas Fault</h4>
-        </a>
-        <p>What does the science say? Where does the information come from? And what does it mean? Investigating past earthquakes to inform the future.</p>
-      </li>
+<a href="atom.php"><img style="padding-right:16px;" src="/images-home/rssfeed.gif" alt="ATOM feed">Subscribe to Science for Everyone</a>
 
-      <li>
-        <a href="/learn/topics/paleo-intro/">
-          <img class="everyone-img" src="../images/intro-paleo.gif" alt="cartoon of cross-section showing layers with a fault"/>
-          <h4>Introduction to Paleoseismology: The Past Informs the Future</h4>
-        </a>
-        <p>What is paleoseismology? How does it work?</p>
-      </li>
+-->
 
-      <li>
-        <a href="maine2012/">
-          <img class="everyone-img" src="maine2012/images/seis.gif" alt="map showing earthquakes in the Maine area"/>
-          <h4>Ground-Truthing After the M4.0 Earthquake in Southern Maine, October 16, 2012</h4>
-        </a>
-        <p>Old-fashioned interviews are compared to Did You Feel It? responses for this earthquake.</p>
-      </li>
+<h3>2017</h3>
+<!-- Featured Items -->
+  <?php
+  include_once '2017/_features.inc.php';
+  echo $EQ_FEATURES->toHtml(-1);
+  ?>
 
-      <li>
-        <a href="nepal2015/">
-          <img class="everyone-img" src="nepal2015/images/bricks.jpg" alt="collapsed buildings"/>
-          <h4>M7.8 Nepal Earthquake, 2015 – A Small Push to Mt. Everest</h4>
-        </a>
-        <p>A large shallow earthquake moves Mt. Everest 3 cm southwest.</p>
-      </li>
+<h3>Science for Everyone Archives</h3>
 
-      <li>
-        <a href="splays/">
-          <img class="everyone-img" src="splays/images/2004-tsunami.jpg" alt="large water waves coming with people running"/>
-          <h4>The "Snow Plow Theory" of Early-Arriving Tsunamis</h4>
-        </a>
-        <p>What is a splay fault, and how can they affect tsunamis?</p>
-      </li>
-      <li>
-        <a href="seismology-in-the-city/">
-          <img class="everyone-img" src="seismology-in-the-city/images/seis-city-thumb.jpg" alt="map of New York City showing sites of study"/>
-          <h4>Seismology in the City</h4>
-        </a>
-        <p>How seismologists can use noise to see under the ground.</p>
-
-      </li>
-      <li>
-        <a href="wasatch/">
-          <img class="everyone-img" src="wasatch/images/grid.jpg" alt="3 scientists in trench looking at trench wall"/>
-          <h4>How Big and How Frequent Are Earthquakes on the Wasatch Fault?</h4>
-        </a>
-        <p>Paleoseismology along the Wasatch Fault in Utah is helping to estimate the shaking risk to nearby towns.</p>
-      </li>
-    </ul>
-
-</div>
+<ul>
+  <li><a href="2016/">2016</a></li>
+  <li><a href="2015/">2015</a></li>
+</ul>
