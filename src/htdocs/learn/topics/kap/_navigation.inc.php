c54ec513510ba7e1ec41d39bcc21879cd479056e
Scott Haefner <shaefner@usgs.gov>
2018-02-16 15:47:03 -0800

    saving progress

---
+++ src/htdocs/learn/topics/kap/_navigation.inc.php
@@ -0,0 +1,17 @@
+<?php
+  $section = "/learn/topics/kap";
+
+  echo "<a href='/learn/topics' class='up-one-level'>Earthquake Topics</a>",
+
+  "<section>",
+
+    "<header>Kite Aerial Photography</header>",
+
+    navItem("${section}/index.php", "Repeat Photography: George Lawrence&rsquo;s &ldquo;San Francisco in Ruins&rdquo;") .
+    navItem("${section}/carrizo.php", "San Andreas Fault at Wallace Creek") .
+    navItem("/research/parkfield/shake/", "Parkfield Shake Table Exhibit") .
+
+  "</section>"
+  ;
+
+?>
---
+++ src/htdocs/learn/topics/kap/index.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!isset($TEMPLATE)) {
+  $TITLE = 'Repeat Photography: George Lawrence&rsquo;s &ldquo;San Francisco in Ruins&rdquo;';
+  $NAVIGATION = true;
+  $HEAD = '';
+  $FOOT = '';
+
+  include 'template.inc.php';
+}
+
+?>
+
+<p>Just three weeks after the 1906 Earthquake, professional photographer George
+  Lawrence snapped his famous photograph, &#8220;San Francisco in Ruins,&#8221;
+  from a 49-pound camera raised 1000 feet above the bay with a train of Conyne
+  kites.</p>
+
+<p>To honor George Lawrence's feat, and to celebrate the advancements made
+  in earthquake research and awareness, the
+  <a href="http://www.drachen.org/">Drachen Foundation</a> sponsored Kite Aerial
+  Photographer (KAPer) <a href="http://scotthaefner.com/">Scott Haefner</a>,
+  USGS geologist, to re-shoot Lawrence's photo using contemporary kite aerial
+  photography techniques.</p>
+
+<figure>
+  <figcaption>&#8220;San Francisco in Ruins,&#8221; by George Lawrence,
+    documents the damage caused by the
+    <a href="/earthquakes/events/1906calif/18april/">April 18, 1906 M 7.9
+    earthquake</a> on the San Andreas fault. Photo from the
+    <a href="https://www.loc.gov/resource/pan.6a34514">Library
+    of Congress</a>.
+  </figcaption>
+</figure>
