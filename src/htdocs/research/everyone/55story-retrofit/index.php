79b99a99f60c935e8dccb140100faf6fde3d1820
lisa-usgs <lisa@usgs.gov>
2018-03-15 12:02:20 -0600

    initial 55 story building page

---
+++ src/htdocs/research/everyone/55story-retrofit/index.php
@@ -0,0 +1,48 @@
+<?php
+// Author: Lisa Wald
+// Contact: Mehmet Celebi
+  if (!isset($TEMPLATE)) {
+    $TITLE = '55-Story Building – Before and After Earthquake Retrofit';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<figure class='right'>
+<img src="images/bldg-map.jpg" alt=""/>
+<figcaption>
+  Satellite image of the 55-story building in Osaka, Japan.
+</figcaption>
+</figure>
+
+<p>
+  What if the shaking from an earthquake was irrelevant because all our buildings were safe from collapse, and they could keep us safe inside them? That’s one of the goals the research of seismic engineers is trying to accomplish. USGS seismic engineer Mehmet Celebi and several other colleagues in other agencies recently studied the “before” and “after” of a 55-story building in Osaka, Japan that was subjected to two real earthquakes, once before a seismic retrofit and again after the retrofit. This was a rare case that offered a rare opportunity to study the retrofit “before” and “after” of a tall building shaken by two distant earthquakes. The scientists wanted to know how much safer the engineering improvements had made the building for the type of potentially-damaging shaking that is caused by large distant earthquakes.
+</p>
+<p>
+  When an earthquake occurs, energy is radiated out in all directions through the earth’s crust as seismic waves that we feel as shaking.  The shaking behavior includes fast shaking motions like a toddler shaking a rattle, as well as slower shaking that’s more like an ocean swell.  For our purposes here, there are two important facts that are relevant: 1) the fast-shaking (called short-period) energy are dominant  near the earthquake location and tend to die out before they travel very far, but the slow-shaking (long-period) energy is able to travel very far in the earth’s crust before it dies out; and 2) tall buildings are affected more by the long-period energy, and shorter buildings are affected more by the short-period energy.
+</p>
+<figure class='left'>
+<img src="images/retrofit.jpg" alt=""/>
+<figcaption>
+  A schematic drawing showing the locations of the dampers installed in the building for the retrofit.
+</figcaption>
+</figure>
+
+<figure class='left'>
+<img src="images/waveforms.gif" alt=""/>
+<figcaption>
+  The recorded shaking of the building before and after the retrofit. The "BEFORE" waveform is from the M9.0 Tohoku earthquake 767 km away, and the "AFTER" waveform is from the M7.3 Kumamoto earthquake 478 km away. Note that the acceleration before was about 5 times larger than after.
+</figcaption>
+</figure>
+
+<p>
+  In this case, both earthquakes that shook the building were far away, and the long-period waves had a significant effect on the building. The M9.0 Tohoku earthquake on March 11, 2011 (767 km/476 mi away) was recorded on several seismic instruments inside the building, and the recordings showed that the building had undergone severe and extended shaking that had threatened the integrity of the structure. A retrofit was clearly called for.  The retrofit was intended to dampen the shaking of the building so that it would be less damaged and the contents and people inside would not shake as much.  140 dampers and 114 buckling restrained bracers (BRBs) were installed around the building.
+</p>
+<p>
+  On April 24, 2016 the M7.3 Kumamoto earthquake (478 km/297 mi away) again shook the building and again was recorded on the seismic instruments inside.  The good news was that indeed the severity of the shaking was lessened, but the bad news was that the building still experienced enough shaking to make it a risk for occupants in a future large earthquake.  Celebi and colleagues learned that the retrofit engineering helped dampen the shaking of the building, but more or different types of retrofit engineering are needed to completely tackle all the different shaking effects that could cause damage in this tall building in the future.  This knowledge can be used for the engineering and retrofits of similar tall buildings in the future. Currently, in the United States, there are no buildings taller than 35 stories that have been retrofitted.
+</p>
+
+<h2>For More Information</h2>
+<ul>
+  <li><a href="/monitoring/nsmp/buildings/">Earthquake Monitoring of Structures</a></li>
+</ul>
