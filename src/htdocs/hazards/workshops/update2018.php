8d6158647f06dc6110bae5ed260c51c319b10df9
lisa-usgs <lisa@usgs.gov>
2018-03-30 09:46:04 -0600

    added Arts presentation

--- src/htdocs/hazards/workshops/update2018.php
+++ src/htdocs/hazards/workshops/update2018.php
@@ -22,66 +22,66 @@
 <ul>
   <li>Welcome
     <ul>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Petersen_Welcome_Overview_Motivation">Overview and Motivation for the 2018 NSHM Update</a> (181kb) – Mark Petersen</li>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Luco_Motivation_for_Timing_of_2018_NSHM_Update">Motivation for Timing of the 2018 NSHM Update</a> (744kb) – Nico Luco</li>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Rezaeian_User_Needs">Overview of User Needs for the 2018 Update and Beyond</a> (744kb) – Sanaz Rezaeian</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Petersen_Welcome_Overview_Motivation.pdf">Overview and Motivation for the 2018 NSHM Update</a> (181kb) – Mark Petersen</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Luco_Motivation_for_Timing_of_2018_NSHM_Update.pdf">Motivation for Timing of the 2018 NSHM Update</a> (744kb) – Nico Luco</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Rezaeian_User_Needs.pdf">Overview of User Needs for the 2018 Update and Beyond</a> (744kb) – Sanaz Rezaeian</li>
     </ul>
   </li>
 
   <li>Update of the CEUS GMMs in the 2018 NSHM
     <ul>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Petersen_Available_CEUS_GMMs">Available CEUS GMMs for the 2018 NSHM Update</a> (877kb) – Mark Petersen</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Petersen_Available_CEUS_GMMs.pdf">Available CEUS GMMs for the 2018 NSHM Update</a> (877kb) – Mark Petersen</li>
       <li><a href="/static/lfs/nshm/workshops/Day_1_Goulet_NGA-East.pdf">Overview of the NGA-East Project</a> (2.7MB) – Christine Goulet</li>
       <li>Overview and Updates to the NGA-East Seed GMMs
         <ul>
-          <li><a href="/static/lfs/nshm/workshops/Day_1_Pezeshk_GMPE_SP16_clean">Summary of SP16 GMM</a> (5MB) – Shahram Pezeshk</li>
+          <li><a href="/static/lfs/nshm/workshops/Day_1_Pezeshk_GMPE_SP16_clean.pdf">Summary of SP16 GMM</a> (5MB) – Shahram Pezeshk</li>
           <li><a href="/static/lfs/nshm/workshops/Day_1_Graizer_NSHM_2018_v2.pdf">G16 and G16v2 GMPEs for CENA</a> (2.1MB) – Vladimir Graizer</li>
       </ul>
       </li>
-      <li>Concerns about the NGA-East GMMs – Art Frankel</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Frankel_Concerns_about_NGA-East_GMMs.pdf">Concerns about the NGA-East GMMs</> (1.1MB) – Art Frankel</li>
     </ul>
   </li>
 
   <li>Proposed 2018 NSHM CEUS GMMs
     <ul>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Rezaeian_Aleatory_Uncertainty">NGA-East Epistemic and Aleatory Uncertainties</a> (3.5MB) – Sanaz Rezaeian</li>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_McNamara_CEUS_Residual_Study">CEUS GMM Residual Study</a> (2.3MB) – Dan McNamara</li>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Petersen_Proposed_Epistemic_Weighting">Proposed Epistemic Weighting</a> (618kb) – Mark Petersen</li>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Shumway_Sensitvity_Studies">Sensitivity Studies</a> (1.4MB) – Allison Shumway</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Rezaeian_Aleatory_Uncertainty.pdf">NGA-East Epistemic and Aleatory Uncertainties</a> (3.5MB) – Sanaz Rezaeian</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_McNamara_CEUS_Residual_Study.pdf">CEUS GMM Residual Study</a> (2.3MB) – Dan McNamara</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Petersen_Proposed_Epistemic_Weighting.pdf">Proposed Epistemic Weighting</a> (618kb) – Mark Petersen</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Shumway_Sensitvity_Studies.pdf">Sensitivity Studies</a> (1.4MB) – Allison Shumway</li>
     </ul>
   </li>
 
   <li>NGA-East Site Amplification Model
     <ul>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Stewart_NGA-East_Amplification_Model">CENA Site Amplification</a> (2.6MB) – Jon Stewart</li>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Thompson_Implementation_of_NGA-East_Amplification_Model">Implementation of the NGA-East Site Amplification Model</a> (1.2MB) – Eric Thompson</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Stewart_NGA-East_Amplification_Model.pdf">CENA Site Amplification</a> (2.6MB) – Jon Stewart</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Thompson_Implementation_of_NGA-East_Amplification_Model.pdf">Implementation of the NGA-East Site Amplification Model</a> (1.2MB) – Eric Thompson</li>
     </ul>
   </li>
 
   <li>Consensus on the Update of the CEUS GMMs in the 2018 NSHM
     <ul>
-      <li><a href="/static/lfs/nshm/workshops/Day_1_Petersen_Consensus_Discussion_Points">Discussion Points</a> (87kb) - Mark Petersen</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_1_Petersen_Consensus_Discussion_Points.pdf">Discussion Points</a> (87kb) - Mark Petersen</li>
     </ul>
   </li>
 
-  <li><a href="/static/lfs/nshm/workshops/Day_1_Powers_Additional_Modeling_Details">Additional 2018 NSHM Modeling Details and Discussion</a> (507kb) - Peter Powers</li>
+  <li><a href="/static/lfs/nshm/workshops/Day_1_Powers_Additional_Modeling_Details.pdf">Additional 2018 NSHM Modeling Details and Discussion</a> (507kb) - Peter Powers</li>
 </ul>
 
 <h2>Thursday, March 8<sup>th</sup></h2>
 <ul>
   <li>Welcome
     <ul>
-      <li><a href="/static/lfs/nshm/workshops/Day_2_Petersen_Welcome_Overview_Recap">Overview and Motivation for the 2018 NSHM Update and Recap of Day 1</a> (151kb) – Mark Petersen</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_2_Petersen_Welcome_Overview_Recap.pdf">Overview and Motivation for the 2018 NSHM Update and Recap of Day 1</a> (151kb) – Mark Petersen</li>
     </ul>
   </li>
 
   <li>Motivation and Issues with Incorporating Basin Models
     <ul>
-<li><a href="/static/lfs/nshm/workshops/Day_2_Rezaeian_User_Needs">Overview of User Needs for the 2018 Update and Beyond</a> (441kb) – Sanaz Rezaeian</li>
-<li><a href="/static/lfs/nshm/workshops/Day_2_Crouse_SCEC_UGMS">Southern California Earthquake Center (SCEC) Utilization of Ground Site-Specific Earthquake Motions for Design of Structures in the LA Region from 3D Numerical Simulations and NGA-West2 GMPEs</a> (3MB) – C.B. Crouse</li>
-<li><a href="/static/lfs/nshm/workshops/Day_2_Luco_Use_Cases">Use Cases Relevant to Basin-Effect Modeling</a> (578kb) – Nico Luco</li>
-<li><a href="/static/lfs/nshm/workshops/Day_2_Petersen_Overview_and_Methods">Proposed Implementation of Basin Effects into the 2018 NSHM: Overview and Methods</a> (532kb) – Mark Petersen</li>
-<li><a href="/static/lfs/nshm/workshops/Day_2_Frankel_Proposed_Approach_Basin_Amplification">Overview of Amplification for Basins Deeper than 3km</a> (5.8MB) – Art Frankel</li>
+<li><a href="/static/lfs/nshm/workshops/Day_2_Rezaeian_User_Needs.pdf">Overview of User Needs for the 2018 Update and Beyond</a> (441kb) – Sanaz Rezaeian</li>
+<li><a href="/static/lfs/nshm/workshops/Day_2_Crouse_SCEC_UGMS.pdf">Southern California Earthquake Center (SCEC) Utilization of Ground Site-Specific Earthquake Motions for Design of Structures in the LA Region from 3D Numerical Simulations and NGA-West2 GMPEs</a> (3MB) – C.B. Crouse</li>
+<li><a href="/static/lfs/nshm/workshops/Day_2_Luco_Use_Cases.pdf">Use Cases Relevant to Basin-Effect Modeling</a> (578kb) – Nico Luco</li>
+<li><a href="/static/lfs/nshm/workshops/Day_2_Petersen_Overview_and_Methods.pdf">Proposed Implementation of Basin Effects into the 2018 NSHM: Overview and Methods</a> (532kb) – Mark Petersen</li>
+<li><a href="/static/lfs/nshm/workshops/Day_2_Frankel_Proposed_Approach_Basin_Amplification.pdf">Overview of Amplification for Basins Deeper than 3km</a> (5.8MB) – Art Frankel</li>
     </ul>
   </li>
 
@@ -89,14 +89,14 @@
     <ul>
       <li>How to Use/Apply Basin Effects
         <ul>
-        <li><a href="/static/lfs/nshm/workshops/Day_2_Campbell_Basin_Effects_CB14">Empirical Modeling of Basin Effects: The Case of CB14</a> (1.2MB) – Ken Campbell</li>
-        <li><a href="/static/lfs/nshm/workshops/Day_2_Atkinson_Basins_in_Subduction_Models">Some Comments on Injecting Additional Site Terms Beyond Vs30 in GMMs</a> (1.7MB) -Gail Atkinson</li>
+        <li><a href="/static/lfs/nshm/workshops/Day_2_Campbell_Basin_Effects_CB14.pdf">Empirical Modeling of Basin Effects: The Case of CB14</a> (1.2MB) – Ken Campbell</li>
+        <li><a href="/static/lfs/nshm/workshops/Day_2_Atkinson_Basins_in_Subduction_Models.pdf">Some Comments on Injecting Additional Site Terms Beyond Vs30 in GMMs</a> (1.7MB) -Gail Atkinson</li>
         </ul>
       </li>
       <li>o	Looking Toward the 2020 NSHM Update: What Models Should be Used?
         <ul>
-          <li><a href="/static/lfs/nshm/workshops/Day_2_Frankel_M9">M9 Project</a> (7.7MB) – Art Frankel</li>
-          <li><a href="/static/lfs/nshm/workshops/Day_2_Moschetti_3D_Simulations">Use of 3D Earthquake Ground Motion Simulations in Future Updates of the NSHM</a> (2.6MB) – Morgan Moschetti</li>
+          <li><a href="/static/lfs/nshm/workshops/Day_2_Frankel_M9.pdf">M9 Project</a> (7.7MB) – Art Frankel</li>
+          <li><a href="/static/lfs/nshm/workshops/Day_2_Moschetti_3D_Simulations.pdf">Use of 3D Earthquake Ground Motion Simulations in Future Updates of the NSHM</a> (2.6MB) – Morgan Moschetti</li>
         </ul>
       </li>
     </ul>
@@ -104,17 +104,17 @@
 
   <li>Basin Characterization and Sensitivity Studies
     <ul>
-      <li><a href="/static/lfs/nshm/workshops/Day_2_Boyd_Basin_Depth_Maps">Zx ‘Basin Depth’ Maps for WUS GMPEs</a> (2.3MB) – Oliver Boyd</li>
-      <li><a href="/static/lfs/nshm/workshops/Day_2_Thompson_Combining_Estimated_Basin_Depth_with_Assumed_Vs30">Combining Estimated Basin Depth with an Assumed Vs30</a> (333kb) – Eric Thompson</li>
-      <li><a href="/static/lfs/nshm/workshops/Day_2_Shumway_Sensitivity_Studies">Sensitivity Studies</a> (1.1MB) – Allison Shumway</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_2_Boyd_Basin_Depth_Maps.pdf">Zx ‘Basin Depth’ Maps for WUS GMPEs</a> (2.3MB) – Oliver Boyd</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_2_Thompson_Combining_Estimated_Basin_Depth_with_Assumed_Vs30.pdf">Combining Estimated Basin Depth with an Assumed Vs30</a> (333kb) – Eric Thompson</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_2_Shumway_Sensitivity_Studies.pdf">Sensitivity Studies</a> (1.1MB) – Allison Shumway</li>
     </ul>
   </li>
 
   <li>Consensus on Basin Effects in the 2018 NSHM
     <ul>
-      <li><a href="/static/lfs/nshm/workshops/Day_2_Petersen_Consensus_Discussion_Points">Discussion Points</a> (134kb) – Mark Petersen</li>
+      <li><a href="/static/lfs/nshm/workshops/Day_2_Petersen_Consensus_Discussion_Points.pdf">Discussion Points</a> (134kb) – Mark Petersen</li>
     </ul>
   </li>
 
-  <li><a href="/static/lfs/nshm/workshops/Day_2_Mueller_Catalogs_and_Gridded_Rate_Models">Catalogs and Gridded Rate Models in the 2018 NSHM</a> (922kb) - Chuck Mueller</li>
+  <li><a href="/static/lfs/nshm/workshops/Day_2_Mueller_Catalogs_and_Gridded_Rate_Models.pdf">Catalogs and Gridded Rate Models in the 2018 NSHM</a> (922kb) - Chuck Mueller</li>
 </ul>
