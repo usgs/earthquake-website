32f8034829323577473ab2b1cfc94ef0d21c43d8
lisa-usgs <lisa@usgs.gov>
2018-04-17 14:49:05 -0600

    initial checkin of Panamint Valley science article

---
+++ src/htdocs/research/everyone/panamint-seismic-reflection/index.php
@@ -0,0 +1,67 @@
+<?php
+// Author: Lisa Wald
+// Contact: Ryan Gold
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Untangling Faults at Depth – What Lies Beneath Panamint Valley, California?';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<figure class='right'>
+<img src="images/Fig8_DesertroseGraben.jpg" alt=""/>
+<figcaption>
+  Trona Wildrose Road in the Panamint Valley, California, along the section of the seismic reflection survey, looking west.
+</figcaption>
+</figure>
+
+<p>
+  Panamint Valley in southern California stretches out across the horizon like a photo from the surface of Mars.  USGS scientists, along with colleagues from Oregon State University and University of Kentucky, will be surveying along the quiet roads in this remote area on the south-western edge of Death Valley National Park on April 23 to May 7 in an effort to figure out what type of fault is active there.
+</p>
+<p>
+  The eastern edge of Panamint Valley has two types of faults that can be seen in the near-surface geology: low-angle faults at the mountain front and high-angle faults in the valley.   To determine which type of fault is active there, the scientists must understand how these faults move deep below the surface. The answer will ultimately reveal whether faults in the region can break in complex combinations of low- and high-angle faults, or if one or the other of these is most likely to rupture alone.
+</p>
+
+<figure>
+<img src="images/field-location-map.jpg" alt=""/>
+<figcaption>
+  Map of the location of the field study in Panamint Valley, California. The seismic reflection line is shown in yellow, the low-angle faults are black, and the steep faults are orange.
+</figcaption>
+</figure>
+
+<figure class='left'>
+<img src="images/Fig1_ScientificQuestion.png" alt=""/>
+<figcaption>
+  A simplified cross-section sketch of the two possible scenarios for the configuration of the low-angle and steep faults under the surface. Option 1 is capable of producing larger magnitude earthquakes.
+</figcaption>
+</figure>
+
+<p>
+  To get a picture of what’s going on at 500 ft below the surface, the team of scientists will use 150 geophones and a seismic source.  The geophones are extremely sensitive sensors that will record the shaking produced by the seismic source. The seismic source, or “mini-vibe,” is a 3-ton vibrator pulled on trailer behind a truck.  It shakes the ground enough for the sensors to record the signal, but 20 ft beyond the mini-vibe, a person can barely feel the shaking.  The scientists will push the 150 geophones 3 inches into the ground along the side of the road, and then the mini-vibe will crawl down the road, stopping every 15 ft to shake the ground. When the crew reaches the end of the line of geophones, they’ll remove them and place them along the next 1-mile stretch, and then continue with the mini-vibe.  The survey will cover a total of about 3.5 miles by shaking, recording, and then leap-frogging with the geophones.  When all the data is put together from all the shakes at all the geophones, the result is a seismic reflection profile, a picture of the subsurface much like an ultrasound. Scientists hope the seismic-reflection “pictures” will reveal the position of the faults below the surface.
+</p>
+<p>
+  The position of the faults will indicate the potential strength and distribution of ground-shaking during an earthquake. This information is used to make the National Hazard Maps, which are updated with the latest information every few years.  These hazard maps are used by engineers to determine the best building codes for different areas based on how hard and how often the ground shakes. The building codes guide the construction standards for houses, bridges, and other structures to withstand earthquake shaking without collapsing or causing injuries to those inside or nearby.  Ridgecrest is the closest large town to Panamint Valley, but a large earthquake might be felt there as well as in the distant smaller towns surrounding the sparsely-populated valley. Despite its remote location, results from Panamint Valley will help inform hazard estimates in more densely populated regions.
+</p>
+
+<p>-written by Lisa Wald</p>
+
+<h2>For More Information</h2>
+
+
+
+<h2>The Scientist Behind the Science</h2>
+
+<figure class='left'>
+<img src="images/Ryan_logging.jpg" alt=""/>
+<figcaption>
+  Ryan Gold doing paleoseismology logging in a trench near ???
+</figcaption>
+</figure>
+
+
+<p>
+  <strong><a href="https://www.usgs.gov/staff-profiles/ryan-gold?qt-staff_profile_science_products=3#qt-staff_profile_science_products">Ryan Gold</a></strong> (the crew leader) is a Research Geologist with the USGS. His research focuses on documentation of the timing and magnitude of past (prehistoric) earthquakes using clues from the modern landscape, trench exposures of the shallow subsurface, and geophysical imaging. When he’s not working, he can usually be found trail running or skiing in the Colorado Rocky Mountains.
+</p>
+<p>
+  Other crew members include USGS scientists Rich Briggs, Bill Stephenson, Chris DuRoss, and Jaime Delano, and cooperators Eric Kirby (Oregon State University), and Ed Woolery (University of Kentucky).
+</p>
