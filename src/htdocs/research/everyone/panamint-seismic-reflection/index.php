4ae343a2ebb96998fbbb13d34dfce001d62e445b
lisa-usgs <lisa@usgs.gov>
2018-04-18 15:38:41 -0600

    final edits from authors

--- src/htdocs/research/everyone/panamint-seismic-reflection/index.php
+++ src/htdocs/research/everyone/panamint-seismic-reflection/index.php
@@ -11,7 +11,7 @@
 <div class='row'>
   <div class='column one-of-two'>
     <p>
-      Panamint Valley in southern California stretches out across the horizon like a photo from the surface of Mars.  USGS scientists, along with colleagues from Oregon State University and University of Kentucky, will be surveying along the quiet roads in this remote area on the south-western edge of Death Valley National Park on April 23 to May 7 in an effort to figure out what type of fault is active there.
+      Panamint Valley in southern California stretches out across the horizon like a photo from the surface of Mars.  USGS scientists, along with colleagues from Oregon State University and University of Kentucky, will be surveying along the quiet roads in this remote area on the southwestern edge of Death Valley National Park on April 23 to May 4 in an effort to figure out what type of fault is active there.
     </p>
     <p>
       The eastern edge of Panamint Valley has two types of faults that can be seen in the near-surface geology: low-angle faults at the mountain front and high-angle faults in the valley.   To determine which type of fault is active there, the scientists must understand how these faults move deep below the surface. The answer will ultimately reveal whether faults in the region can break in complex combinations of low- and high-angle faults, or if one or the other of these is most likely to rupture alone.
@@ -19,7 +19,7 @@
     <figure>
     <img src="images/minivibe.jpg" alt=""/>
     <figcaption>
-      The trailer-mounted minivibe pulled behind a fll-size truck. The vibrator lowers the contact onto the road and vibrates for 10-60 seconds.
+      The trailer-mounted mini-vibe pulled behind a fll-size truck. The vibrator lowers the contact onto the road and vibrates for 10-60 seconds.
     </figcaption>
     </figure>
 
@@ -31,7 +31,7 @@
     </figure>
 
     <p>
-      To get a picture of what’s going on at 500 ft below the surface, the team of scientists will use 150 geophones and a seismic source.  The geophones are extremely sensitive sensors that will record the shaking produced by the seismic source. The seismic source, or “mini-vibe,” is a 3-ton vibrator pulled on trailer behind a truck.  It shakes the ground enough for the sensors to record the signal, but 20 ft beyond the mini-vibe, a person can barely feel the shaking.
+      To get a picture of what’s going on 500 ft below the surface, the team of scientists will use 150 geophones and a seismic source.  The geophones are extremely sensitive sensors that will record the shaking produced by the seismic source. The seismic source, or “mini-vibe,” is a 3-ton vibrator pulled on trailer behind a truck.  It shakes the ground enough for the sensors to record the signal, but 20 ft beyond the mini-vibe, a person can barely feel the shaking.
     </p>
     <p>
       The scientists will push the 150 geophones 3 inches into the ground along the side of the road, and then the mini-vibe will crawl down the road, stopping every 15 ft to shake the ground. When the crew reaches the end of the line of geophones, they’ll remove them and place them along the next 1-mile stretch, and then continue with the mini-vibe.  The survey will cover a total of about 3.5 miles by shaking, recording, and then leap-frogging with the geophones.  When all the data is put together from all the shakes at all the geophones, the result is a seismic reflection profile, a picture of the subsurface much like an ultrasound. Scientists hope the seismic-reflection “pictures” will reveal the position of the faults below the surface.
@@ -50,12 +50,12 @@
     <figure>
     <img src="images/Fig1_ScientificQuestion.png" alt=""/>
     <figcaption>
-      A simplified cross-section sketch of the two possible scenarios for the configuration of the low-angle and steep faults under the surface. Option 1 is capable of producing larger magnitude earthquakes.
+      A simplified cross-section sketch of the two possible scenarios for the configuration of the low-angle and steep faults under the surface. Option 1 is capable of producing larger magnitude earthquakes. (modified from Cichanski, 2000)
     </figcaption>
     </figure>
 
     <p>
-      The position of the faults will indicate the potential strength and distribution of ground-shaking during an earthquake. This information is used to make the National Hazard Maps, which are updated with the latest information every few years.  These hazard maps are used by engineers to determine the best building codes for different areas based on how hard and how often the ground shakes. The building codes guide the construction standards for houses, bridges, and other structures to withstand earthquake shaking without collapsing or causing injuries to those inside or nearby.  Ridgecrest is the closest large town to Panamint Valley, but a large earthquake might be felt there as well as in the distant smaller towns surrounding the sparsely-populated valley. Despite its remote location, results from Panamint Valley will help inform hazard estimates in more densely populated regions.
+      The position of the faults will indicate the potential strength and distribution of ground-shaking during an earthquake. This information is used to make the National Seismic Hazard Maps, which are updated with the latest information every few years.  These hazard maps are used by engineers to determine the best building codes for different areas based on how hard and how often the ground shakes. The building codes guide the construction standards for houses, bridges, and other structures to withstand earthquake shaking without collapsing or causing injuries to those inside or nearby.  Ridgecrest is the closest large town to Panamint Valley, but a large earthquake might be felt there as well as in the distant smaller towns surrounding the sparsely-populated valley. Despite its remote location, results from Panamint Valley will help inform hazard estimates in more densely populated regions.
     </p>
     <p>-written by Lisa Wald</p>
 
@@ -71,8 +71,8 @@
 
 <h2>For More Information</h2>
 <ul>
-  <li><a href="">A link to some related information</a></li>
-  <li><a href="">A link to some related information</a></li>
+  <li><a href="/research/faults/overview.php">Faults, Earthquake Geology, and Special Earthquake Studies</a></li>
+  <li><a href="/hazards/hazmaps/">Seismic Hazard Maps and Site-Specific Data</a></li>
 </ul>
 
 <h2>The Scientist Behind the Science</h2>
@@ -80,7 +80,7 @@
 <figure class='left'>
 <img src="images/Ryan_logging.jpg" alt=""/>
 <figcaption>
-  Ryan Gold in a trench near ??? loggin paleoseismology data.
+  Ryan Gold logging paleoseismology data in the Buffalo Bowl trench site near Teton National Park, WY.
 </figcaption>
 </figure>
 
