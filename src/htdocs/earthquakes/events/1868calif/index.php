dba43ff9d2299c3ebeacb59dec2bdb0b5a76bb7a
Scott Haefner <shaefner@usgs.gov>
2018-05-01 15:10:44 -0700

    create new archive page and move old resources to archive

--- src/htdocs/earthquakes/events/1868calif/index.php
+++ src/htdocs/earthquakes/events/1868calif/index.php
@@ -1,11 +1,11 @@
 <?php
-if (!isset($TEMPLATE)){
-  $TITLE='M6.8 October 21, 1868 Hayward Fault Earthquake';
-  $HEAD='<link rel="stylesheet" href="style.css" />';
-  $FOOT='';
-  $NAVIGATION=true;
-  include 'template.inc.php';
-}
+  if (!isset($TEMPLATE)){
+    $TITLE='M6.8 October 21, 1868 Hayward Fault Earthquake';
+    $HEAD='<link rel="stylesheet" href="style.css" />';
+    $FOOT='';
+    $NAVIGATION=true;
+    include 'template.inc.php';
+  }
 ?>
 
 <div class="alert info no-icon">
@@ -50,7 +50,6 @@ if (!isset($TEMPLATE)){
     <p>What if&hellip;<br />A 7.0 earthquake hit the San Francisco Bay Area?
       Are you, your family, and your business prepared to outsmart disaster?</p>
   </li>
-
 </ul>
 
 <h2>New Science and Products</h2>
@@ -143,50 +142,13 @@ if (!isset($TEMPLATE)){
       a virtual tour of the Hayward fault in the east San Francisco Bay Region
       that can be viewed in Google Earth&trade; </p>
   </li>
-
-  <li>
-    <a href="https://pubs.usgs.gov/sim/3045/">
-      <h3>Three-Dimensional Geologic Map of the Hayward Fault Zone</h3>
-      <img src="images/3dmap.gif" alt="Photo of the Three-Dimensional Geologic Map of the Hayward Fault Zone"/>
-    </a>
-    <p>3D geologic map with descriptions of geologic units and structures, along with a discussion of the methods used
-      to map them.</p>
-  </li>
-
-  <li>
-    <a href="https://pubs.usgs.gov/of/2008/1121/"/a>
-      <h3>Intensity Maps for the 1868 Hayward Earthquake</h3>
-      <img src="images/1868intensity.jpg" alt="Photo of the Intensity Maps for the 1868 Hayward Earthquake"/>
-    </a>
-  <p>Modified Mercalli Intensity maps for the Hayward earthquake plotted in
-  ShakeMap format. </p>
-  </li>
-
-  <li>
-    <a href="https://geomaps.wr.usgs.gov/gump/people/ponce/hayward_fault/hf.html">
-      <h3>Geometry and Evolution of the Hayward Fault</h3>
-      <img src="images/geometry.jpg" alt="Image of the Geometry and Evolution of the Hayward Fault"/>
-    </a>
-    <p>Geophysical data along the Hayward Fault are used to investigate the
-    nature, spatial relationship, and evolution of the Hayward Fault Zone. </p>
-  </li>
-
 </ul>
+
+<p class="archive">See <a href="archive.php#products">more products</a> &raquo;</p>
 
 <h2>Field Guides</h2>
 
 <ul class="no-style linklist">
-  <li>
-    <a href="https://pubs.usgs.gov/of/2008/1135/">
-      <h3>Where&rsquo;s the Hayward Fault? A Green Guide to the Fault</h3>
-      <img src="images/greenguide.jpg" alt="Photo of the Hayward Fault"/>
-    </a>
-    <p>Self-guided field trips to one of North America&rsquo;s most dangerous
-    earthquake faults, the Hayward Fault. Locations were chosen because of
-    their easy access using mass transit and/or their significance relating to
-    the natural and cultural history of the East Bay landscape.</p>
-  </li>
-
   <li>
     <a href="https://seismo.berkeley.edu/hayward/hayward_tours.html">
       <h3>The Hayward Fault: Maps &amp; Tours</h3>
@@ -195,31 +157,9 @@ if (!isset($TEMPLATE)){
       you know where and how to look. This tour covers The Hayward Fault at the
       Campus of the University of California, Berkeley.</p>
   </li>
-
-  <li>
-    <a href="pdf/fld007_17e.pdf">
-      <h3>2006 Field Guide to the Hayward Fault (13 MB pdf)</h3>
-      <img src="images/fieldguide.jpg" alt="Cover of the 2006 Field Guide to the Hayward Fault"/>
-    </a>
-    <p>A detailed geologic and architectural field guide to 11 stops along the
-    Hayward Fault, including UC Berkeley Campus and Memorial Stadium, Point
-    Pinole, Cragmont School in Berkeley, Oakland City Hall, and old City Hall
-    in Hayward and it includes discussion of the 1868 earthquake. This guide
-    was published by the Geological Society of America in 2006 as part of the
-    1906 San Francisco Earthquake Centennial Meeting.</p>
-  </li>
-
-  <li>
-    <a href="https://pubs.usgs.gov/bul/b2188/b2188ch2.pdf"/>
-      <h3>Field Guide to the Hayward Fault (29 MB pdf)</h3>
-      <img src="images/slide.jpg" alt="Photo of debris flows on the Hayward Fault"/>
-    </a>
-    <p>Download a 2001 U.S. Geological Survey Field Guide to the Hayward Fault
-    in Downtown Hayward and at the Caldecott Tunnel (between Orinida and
-    Oakland) and to the 1998 Fremont Peak landslide.</p>
-  </li>
-
 </ul>
+
+<p class="archive">See <a href="archive.php#guides">more field guides</a> &raquo;</p>
 
 <h2>See also</h2>
 
@@ -245,24 +185,6 @@ if (!isset($TEMPLATE)){
   </li>
 
   <li>
-    <a href="https://pubs.usgs.gov/of/2004/1083/explanation.html">
-      <h3>Earthquakes From 1984&ndash;2000 Along the Hayward and Calaveras Faults, California</h3>
-      <img src="images/relocated-quakes.png" alt="location map"/>
-    </a>
-    <p>Cross-Sections and Maps Showing Double-Difference Relocated Earthquakes.</p>
-  </li>
-
-  <li>
-    <a href="https://seismo.berkeley.edu/~burgmann/RESEARCH/research_HF.html">
-      <h3>The Accumulation of Interseismic Strain Along the Hayward Fault</h3>
-      <img src="images/insar.jpg" alt="InSAR image of the Hayward Fault"/>
-    </a>
-    <p>Interseismic surface deformation along the Hayward Fault monitored with
-    Interferometric Synthetic Aperture Radar (InSAR) and the Global
-    Positioning System (GPS).</p>
-  </li>
-
-  <li>
     <a href="/learn/topics/hayward_paleo/">
       <h3>Paleoseismology and the Hayward Fault</h3>
       <img src="images/recurrence.png" alt="plot showing earthquake recurrence" /></a>
@@ -281,15 +203,6 @@ if (!isset($TEMPLATE)){
     <p>Detailed account of the 1868 earthquake and aftermath by Andrew Lawson,
       including damage photos.</p>
   </li>
-
-  <li>
-    <a href="http://1868alliance.org/">
-      <h3>1868 Hayward Earthquake Alliance</h3>
-      <img src="images/1868.gif" alt="1868 Hayward Earthquake Alliance logo"/>
-    </a>
-    <p>This web site was created in 2008 to help coordinate and promote efforts
-      and activities between organizations throughout the greater San Francisco
-      Bay Area planning to commemorate the 140th anniversary of the earthquake.</p>
-  </li>
-
 </ul>
+
+<p class="archive">See <a href="archive.php#see-also">more resources</a> &raquo;</p>
