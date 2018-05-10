35b5abcffb3221be997dcdf00197e70d22b3b863
Scott Haefner <shaefner@usgs.gov>
2018-05-10 16:14:12 -0700

    fix broken links

--- src/htdocs/earthquakes/events/1906calif/virtualtour/instructions.php
+++ src/htdocs/earthquakes/events/1906calif/virtualtour/instructions.php
@@ -1,24 +1,24 @@
 <?php
-if (!isset($TEMPLATE)) {
-  $TITLE = 'Instructions &amp; Tips';
-  $HEAD = '';
-  $FOOT = '';
-  $NAVIGATION = true;
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Instructions &amp; Tips';
+    $HEAD = '';
+    $FOOT = '';
+    $NAVIGATION = true;
 
-  include 'template.inc.php';
-}
- ?>
-          <h3>Instructions</h3>
-          <ol>
-            <li>Download and install <a href="http://earth.google.com">Google Earth</a>. We recommend using the latest version.</li>
-            <li>Uncheck (turn off) any layers that may be active within the left panel, <em>Places</em> and <em>Layers</em>. To provide a 3D perspective, turn on the Terrain layer under <em>Layers</em> or on the <em>Navigation Panel</em>.</li>
-            <li>Begin the 1906 Earthquake Virtual Tour by stepping through the chapters beginning with <a href="global.php">Global Geologic Setting of the 1906 Earthquake</a>. Within each chapter you will find layers (in KMZ format) to load into Google Earth. After clicking the download link, open the file in Google Earth.</li>
-          </ol>
-          <h3>Tips</h3>
-          <ol>
-            <li><strong>To avoid confusion</strong>, turn off previous layers before downloading a new layer into Google Earth. After examining the feature of interest, you may then turn on and off layers as you desire.</li>
-            <li><strong>To increase performance</strong>, delete layers after you have examined them to satisfaction. Performance of your computer may decrease upon loading multiple files into Google Earth.</li>
-            <li>Many of the features on the map are clickable and contain more information or links to additional information.</li>
-          </ol>
-          <p><a href="http://www.keyhole.com/GoogleEarthHelp/GoogleEarth.htm">Learn more about navigating in Google Earth</a></p>
-          <p>Any use of trade, product or firm names is for descriptive purposes only and does not imply endorsement by the U.S. Government.</p>
+    include 'template.inc.php';
+  }
+?>
+
+<h3>Instructions</h3>
+<ol>
+  <li>Download and install <a href="http://earth.google.com">Google Earth</a>. We recommend using the latest version.</li>
+  <li>Uncheck (turn off) any layers that may be active within the left panel, <em>Places</em> and <em>Layers</em>. To provide a 3D perspective, turn on the Terrain layer under <em>Layers</em> or on the <em>Navigation Panel</em>.</li>
+  <li>Begin the 1906 Earthquake Virtual Tour by stepping through the chapters beginning with <a href="global.php">Global Geologic Setting of the 1906 Earthquake</a>. Within each chapter you will find layers (in KMZ format) to load into Google Earth. After clicking the download link, open the file in Google Earth.</li>
+</ol>
+<h3>Tips</h3>
+<ol>
+  <li><strong>To avoid confusion</strong>, turn off previous layers before downloading a new layer into Google Earth. After examining the feature of interest, you may then turn on and off layers as you desire.</li>
+  <li><strong>To increase performance</strong>, delete layers after you have examined them to satisfaction. Performance of your computer may decrease upon loading multiple files into Google Earth.</li>
+  <li>Many of the features on the map are clickable and contain more information or links to additional information.</li>
+</ol>
+<p>Any use of trade, product or firm names is for descriptive purposes only and does not imply endorsement by the U.S. Government.</p>
--- src/htdocs/earthquakes/events/1906calif/virtualtour/modern.php
+++ src/htdocs/earthquakes/events/1906calif/virtualtour/modern.php
@@ -36,7 +36,7 @@ include_once ('_functions.inc.php');
   <div class="column four-of-five">
     <figure>
       <h3>Earthquake Probabilities for the Bay Area</h3>
-      <p>In 2003, earthquake scientists led by the USGS estimated that there is a 62% probability of a magnitude 6.7 or greater earthquake in the Bay Area in the next 30 years. Some faults are more likely to rupture than others. The two most hazardous faults in the region are the Hayward-Rodgers Creek fault system (27%) and the San Andreas fault (21%). The East Bay has a higher earthquake hazard than the  San Francisco peninsula due to the higher number of faults that traverse the East Bay. Which fault is closest to your house? What probability for a magnitude 6.7 or greater earthquake in the next 30 years have scientists assigned to it? For a detailed explanation visit the <a href="http://earthquake.usgs.gov/regional/nca/wg02/">2003 Earthquake Probability study</a>.</p>
+      <p>In 2003, earthquake scientists led by the USGS estimated that there is a 62% probability of a magnitude 6.7 or greater earthquake in the Bay Area in the next 30 years. Some faults are more likely to rupture than others. The two most hazardous faults in the region are the Hayward-Rodgers Creek fault system (27%) and the San Andreas fault (21%). The East Bay has a higher earthquake hazard than the  San Francisco peninsula due to the higher number of faults that traverse the East Bay. Which fault is closest to your house? What probability for a magnitude 6.7 or greater earthquake in the next 30 years have scientists assigned to it? For a detailed explanation visit the <a href="http://www.wgcep.org/ucerf3/">California Earthquake Rupture Forecast study</a>.</p>
          <p class="kml"><a href="kml/BayAreaEQProbability.kmz">Earthquake Probabilities for the Bay Area</a><?php print(simpleFileSize("kml/BayAreaEQProbability.kmz")); ?></p>
     </figure>
   </div>
@@ -52,12 +52,12 @@ include_once ('_functions.inc.php');
       <h3>Liquefaction Susceptibility in San Francisco</h3>
       <p>Liquefaction occurs when young, sandy, water-saturated deposits are shaken strongly during earthquakes. When this occurs, these deposits lose their load-bearing strength and behave like a liquid. Buildings and other structures can be damaged when their foundations sink into these soupy sands. Geologists map the potential for liquefaction by mapping young, sandy deposits that are likely to contain water. The following map shows the distribution of deposits in San Francisco most likely to suffer liquefaction during the next big earthquake in the Bay Area. Detailed <a href="http://pubs.usgs.gov/of/2006/1037/">maps showing liquefaction susceptibility</a> in other regions of the Bay Area are also available.</p>
       <p class="kml"><a href="kml/Liquefaction_SanFrancisco.kmz">Liquefaction Susceptibility in San Francisco</a> <?php print(simpleFileSize("kml/Liquefaction_SanFrancisco.kmz")); ?></p>
-        <figcaption>This Liquefaction Susceptibility layer is intended solely as an educational tool.  The <a href="http://www.conservation.ca.gov/cgs/shzp/">California Geological Survey</a> produces State-mandated regulatory maps that show "<a href="http://www.abag.ca.gov/bayarea/eqmaps/liquefac/liquefac.html">Zones of Required Investigation</a>" for liquefaction (and landslide) hazard. These state maps depict zones where site specific studies are required for new construction. These zone maps also are used in real estate transactions&mdash;sellers of properties within a "Zone of Required Investigation" must disclose that fact to prospective buyers.</figcaption>
+        <figcaption>This Liquefaction Susceptibility layer is intended solely as an educational tool.  The <a href="http://www.conservation.ca.gov/cgs/shzp/">California Geological Survey</a> produces State-mandated regulatory maps that show "<a href="http://resilience.abag.ca.gov/earthquakes/">Zones of Required Investigation</a>" for liquefaction (and landslide) hazard. These state maps depict zones where site specific studies are required for new construction. These zone maps also are used in real estate transactions&mdash;sellers of properties within a "Zone of Required Investigation" must disclose that fact to prospective buyers.</figcaption>
     </figure>
   </div>
 </div>
 </li>
 </ul>
 
-          <h2>What Can I Do?</h2>
-          <p>To find out what you and your family should do to prepare, and to learn more about earthquake hazards in your community and neighborhood, please see these additional <a href="/regional/nca/resources/">Earthquake Hazard Resources</a>.</p>
+<h2>What Can I Do?</h2>
+<p>To find out what you and your family should do to prepare, and to learn more about earthquake hazards in your community and neighborhood, please see these additional <a href="/contactus/menlo/prepare/">Earthquake Hazard Resources</a>.</p>
