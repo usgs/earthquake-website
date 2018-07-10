d537a08722749a0f573f9c39aac2cd04e1ec8c39
lisa-usgs <lisa@usgs.gov>
2018-07-10 14:30:10 -0600

    initial commit of Rodgers Creek article

---
+++ src/htdocs/research/everyone/2018/rodgers-creek/index.php
@@ -0,0 +1,76 @@
+<?php
+// Author: Lisa Wald
+// Contact: Suzanne Hecker
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'A New Map of Rodgers Creek Fault in Sonoma County, California';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<figure class='right'>
+<img src="images/trace.jpg" alt=""/>
+<figcaption>
+  A Google Earth&#8482; image of the principal zone of faulting where it crosses the town Healdsburg (along the newly mapped north end of the fault), showing the 2007 lidar swath (one of two LiDAR datasets that were used).
+</figcaption>
+</figure>
+
+<p>
+  A new map of the Rodgers Creek Fault in Sonoma County, California, has been produced using aerial photography and hillshade imagery derived from LiDAR data (see <a href="/research/everyone/2015/wasatch/collect.php">Down in the Trenches…</a>). “Bare-earth” imagery from LiDAR data allows mapping of faulting-related landform features hidden beneath trees and vegetation, as well as the identification of finer-scale features than were previously known.
+</p>
+<p>
+  The Rodgers Creek Fault, which lies east of the San Andreas Fault, is a main strand of the North American-Pacific Plate boundary north of San Francisco Bay. The plate motion along the Rodgers Creek fault is 6-10 mm/yr, and it has been estimated there is a 33% chance of a M>=6.7 earthquake on this fault combined with the Hayward Fault to the south in the next 30 years. The last large earthquake to occur along this fault was likely between 1715 and 1776.
+</p>
+<p>
+  New information that came to light as a result of this new map includes: 1) that the Rodgers Creek Fault extends about 17 km farther north than previously thought and flanks the east side of the town of Healdsburg; 2)  identification of where the fault crosses the Santa Rosa Creek floodplain in central Santa Rosa; 3) parts of the fault zone that extend toward the Bennett Valley-Maacama Fault system to the east, and toward the Hayward Fault to the south beneath San Pablo Bay; and 4) an overall increase in the known width and complexity of the fault zone.
+</p>
+<p>
+  These new findings indicate a greater hazard than previously thought for the area.
+</p>
+
+<p>- written by Lisa Wald, U.S. Geological Survey, July 16, 2018</p>
+
+<div class="row">
+  <div class="column one-of-two">
+    <figure>
+    <img src="images/street-slope.jpg" alt=""/>
+    <figcaption>
+      A fault scarp in Santa Rosa can be seen as the gentle rise in the street.
+    </figcaption>
+    </figure>
+
+    <h2>For More Information</h2>
+    <ul>
+      <li>
+        <a href="https://www.usgs.gov/news/rodgers-creek-fault-traced-through-santa-rosa">Rodgers Creek Fault Traced through Santa Rosa</a> - USGS News Release, April 4, 2016.
+      </li>
+      <li>
+        Hecker, S, and Randolph Loar, C.E., 2018, <a href="https://pubs.er.usgs.gov/publication/sim3410">Map of recently active traces of the Rodgers Creek Fault, Sonoma County, California</a>: U.S. Geological Survey Scientific Investigations Map 3410, 7 p., 1 sheet, https://doi.org/10.3133/sim3410.
+      </li>
+    </ul>
+
+  </div>
+  <div class="column one-of-two">
+    <figure>
+    <img src="images/street-creep.jpg" alt=""/>
+    <figcaption>
+      A creeping trace of the fault crosses an old road in the Healdsburg Ridge Open Space Preserve.
+    </figcaption>
+    </figure>
+
+  </div>
+</div>
+
+
+<h2>The Scientist Behind the Science</h2>
+
+<figure class='left'>
+<img src="images/hecker.jpg" style="width: 300px;" alt=""/>
+<figcaption>
+  Suzanne Hecker.
+</figcaption>
+</figure>
+
+<p>
+  <strong>Suzanne Hecker</strong> is a geologist with the USGS, where she studies the geologic record of large prehistoric earthquakes and the behavior of active faults. When not in the office or in the field, she enjoys leisurely outdoor pursuits and the pleasure of reading a good book.
+</p>
