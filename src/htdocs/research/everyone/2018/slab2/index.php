5eb215dc17ef0cb8bf743a074140c4ff1b8e2b5c
lisa-usgs <lisa@usgs.gov>
2018-09-19 15:12:07 -0600

    initial commit of slab2 article

---
+++ src/htdocs/research/everyone/2018/slab2/index.php
@@ -0,0 +1,100 @@
+<?php
+// Author: Lisa Wald
+// Contact: Gavin Hayes
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Collection of 3D Geometries for Global Subduction Zones';
+    $HEAD = '<link rel="stylesheet" href="index.css"/>';
+    $NAVIGATION = true;
+    include 'template.inc.php';
+  }
+?>
+
+<!--
+<p>
+  <a href="us-stress.pdf">
+    <i class="material-icons">save_alt</i> Download PDF
+  </a>
+</p>
+-->
+
+  <figure class='right'>
+  <img src="images/fig1.jpg" alt=""/>
+  <figcaption>
+    The global distribution of models included in Slab2.
+  </figcaption>
+  </figure>
+
+<p>
+  The world’s largest earthquakes (magnitude 9+) occur in subduction zones, places on the planet where one tectonic plate is sliding under another. In fact, 90% of all the energy released in all the historical earthquakes are from the uppermost area of subduction zones where the two brittle plates are in contact.  We know these areas are capable of generating enormous earthquakes, but just how large depends on the geometry of the slabs and their areas of contact with the upper (overlying) tectonic plate. Although subduction zones extend from the surface to as deep as ~700 km, the most hazardous area is the upper ~50 km where the large (“megathrust”) earthquakes are generated.  This is also the part of the slab that can produce tsunamis when an earthquake occurs.
+</p>
+
+<figure class='left'>
+<img src="images/fig2.jpg" alt=""/>
+<figcaption>
+  Here, five slabs (the Manila, Philippine, Cotabato, Halmahera, and Sulawesi slabs, from north to south) interact with one another. The Halmahera slab is an inverted U-shaped subduction zone that is completely buried and has no surface expression. It underlies the Cotabato and Sulawesi slabs on its western limb and abuts or overlies the Philippine slab on its eastern limb.
+</figcaption>
+</figure>
+
+<p>
+  Knowledge of the geometry of subducting slabs is a critical requirement and is used as input into many types of seismological analyses. A previous model of worldwide subduction zones called Slab1.0 has been heavily used by seismology researchers. Slab1.0, was ripe for updates because of new information about subduction zone structure and the need for an improved approach to the modeling procedure that would create a better picture of the most important upper portion of the slab. The new model, Slab2, includes more data, additional types of data, and a new approach that allowed the researchers to create a 3D picture of all known active subduction zones globally, creating a complete collection of subduction zone geometry models, built routinely and via the same modeling procedure. Slab2, is a world-wide picture of all active subduction zones that includes the detailed geometry of over 24 million square kilometers of subducted slabs from the near-surface to the deepest part in the earth’s upper mantle layer.
+</p>
+<p>
+  This comprehensive analysis revealed that the deep limit of the seismogenic zone (the area where the slab is brittle and can produce earthquakes) varies from a depth of about 37 km to 54 km. The width (measured along the fault surface in the direction of subduction) varies from about 70 km (Manila slab) to 195 km (Alaska slab), with an average of about 115 km. The subsequent estimates of the seismogenic portion of the subduction zone are approximately 30% larger than the older estimates, which results in an increase in the estimated associated hazard.  Yet, some of the largest earthquakes have occurred on subduction zones with relatively narrow width, indicating that the width of the fault is not the primary influential factor in the potential earthquake size.
+</p>
+
+<figure class="xsect" >
+<img src="images/figS7.jpg" alt=""/>
+<figcaption>
+  Slab2 model for the Middle America slab in Mexico. Main panel shows a
+  cross-section of Slab2 (blue dot-dashed) vs Slab1.0 in this region (red
+  dashed). Results (20) (blue diamonds) imply a more
+  northeasterly extent to the flat slab than is imaged by earthquake data and
+  focal mechanisms (circles and beachball symbols). Earthquakes and focal
+  mechanisms are colored by depth, according to the scale used in the inset. The
+  location and focal mechanism solution of the recent M 7.1 intraplate
+  earthquake near Mexico City is highlighted. The projection width of the
+  cross-section is +/- 50 km.
+</figcaption>
+</figure>
+
+<p>
+  If the extensive use of SLAB1.0 is any indication, this new collection will be used as a reference for, or starting point for:
+</p>
+<ul>
+  <li>earthquake source characterization - defining the type of earthquake likely at each subduction zone site;</li>
+  <li>global hazard analysis – estimating the detailed hazard at each site around the world;</li>
+  <li>modeling megathrust behavior – puzzling out the complex motions of the large plate interactions at subduction zones;</li>
+  <li>analyzing slow slip – figuring out when and how large plates slip slowly without causing a large earthquake;</li>
+  <li>•seismic anisotropy – estimating the structures in subduction zones that affect seismic wave propagation;</li>
+  <li>volcano petrology – determining what type of geologic content may be in volcanic magma associated with the subducting slabs at each location;</li>
+  <li>tsunami hazard – what subduction zones are most likely to cause a damaging tsunami; and</li>
+  <li>mantle flow – the motion of the viscous mantle material directly beneath the earth’s crust.</li>
+</ul>
+</p>
+
+<p>- written by Lisa Wald, USGS, September 2018</p>
+
+<h2>For More Information</h2>
+<ul>
+  <li>
+    <a href="https://pubs.usgs.gov/gip/dynamic/dynamic.html">This Dynamic Earth: The Story of Plate Tectonics</a>
+  <li>
+    <a href="http://science.sciencemag.org/content/early/2018/08/08/science.aat4723">Slab2, a comprehensive subduction zone geometry model</a> - replace with citation and final link
+  </li>
+  <li>
+    <a href="https://doi.org/10.5066/F7PV6JNV">Hayes, G., 2018, Slab2 - A Comprehensive Subduction Zone Geometry Model: U.S. Geological Survey data release</a>.
+  </li>
+</ul>
+
+<h2>The Scientist Behind the Science</h2>
+
+<figure class='left'>
+<img src="images/gavin.jpg"  style="width: 300px;" alt=""/>
+<figcaption>
+  Gavin Hayes with his children, Azalea, Winston and Calliope.
+</figcaption>
+</figure>
+
+<p>
+  <strong>Gavin Hayes</strong> has worked at the USGS NEIC for 11 years. In his research, he attempts to use seismology to address key questions like "Why do earthquakes occur where they do, how big can they be, and how often do the largest events (re)occur?". He is also heavily involved in earthquake response, working on earthquakes in real-time to improve our knowledge of their size, slip distributions, tectonic settings, and their impact. When he's not working, Gavin loves to spend time with his wife, Vanessa, and their three children, Azalea, Winston and Calliope.
+</p>
