9c1d6fdb612eb6a427c52ae26a01bba12a8618f7
nluco-usgs <nluco@usgs.gov>
2018-12-21 13:01:03 -0700

    Update /hazards/designmaps/ ...
    ... in part for consistency with hazards/designmaps/designmaps-gone.php

--- src/htdocs/hazards/designmaps/index.php
+++ src/htdocs/hazards/designmaps/index.php
@@ -12,10 +12,18 @@
   }
 ?>
 
-<p>
+<p class="alert info">
   Engineers should typically use the tools below for seismic design; the
 	parameter values they provide are not identical to those from hazard tools
 	available elsewhere on the USGS website.
+</p>
+
+<p>
+  The USGS collaborates with organizations that develop building codes (for buildings, bridges, and 
+	other structures) to make seismic design parameter values available to engineers. The design code
+	developers first decide how USGS earthquake hazard information should be applied in design 
+	practice. Then, the USGS calculates values of seismic design parameters based on USGS hazard 
+	values and in accordance with design code procedures.
 </p>
 
 <hr/>
@@ -23,36 +31,34 @@
 <h2><a href="/ws/designmaps/">U.S. Seismic Design Maps Web Services</a></h2>
 
 <p>
-  The USGS collaborates with organizations (such as the Building Seismic
-	Safety Council) that develop model building and bridge design codes to make
-	seismic design parameter values available to engineers. The design code
-	developers first decide how USGS earthquake hazard information should be
-	applied in design practice. Then the USGS calculates gridded values of
-	seismic design parameters based on USGS hazard values in accordance with
-	design code procedures. The <i>U.S. Seismic Design Maps Web Services</i>
-	provide seismic design parameter values from numerous design code editions.
+  These web services provide seismic design parameter values from numerous design code editions. 
+	Your options for using these web services are:
 </p>
 
-<h3>USGS U.S. Seismic Design Maps Web Services</h3>
-
-<ul>
-  <li>
-    <a href="/ws/designmaps/">View USGS Web Services</a>
-  </li>
-  <li>
-    <a href="/hazards/designmaps/step-by-step-instructions-for-webservice_v3.pdf">Step-by-step Instructions</a><br />
-      - how to use the U.S. Seismic Design Web Services, e.g. for the recently published <i>ASCE/SEI 7-16 Standard</i>
-  </li>
-</ul>
-
-<h3>Third-party GUIs</h3>
+<h3>Third-party Graphical User Interfaces (GUIs)</h3>
 <p>
-  Most users will find third-party graphical users interfaces (GUIs) like the following to be more usable, and more like the former USGS application(s):
+  Most users obtain seismic design parameter values from the USGS web services through third-party 
+	GUIs like the following:
 </p>
 <ul>
   <li><a href="https://asce7hazardtool.online/">ASCE 7 Hazard Tool</a></li>
   <li><a href="https://seismicmaps.org/">SEAOC/OSHPD Seismic Design Maps Tool</a></li>
   <li><a href="https://hazards.atcouncil.org/">ATC Hazards by Location Tool</a></li>
+</ul>
+
+<h3>USGS U.S. Seismic Design Maps Web Services</h3>
+<p>
+  It is possible but less convenient to obtain seismic design parameter values directly from the 
+	USGS web services:
+</p>
+<ul>
+  <li>
+    <a href="/ws/designmaps/">Documentation</a>
+  </li>
+  <li>
+    <a href="/hazards/designmaps/step-by-step-instructions-for-webservice_v3.pdf">Step-by-step Instructions</a><br />
+      - an example for the recently published <i>ASCE/SEI 7-16 Standard</i>
+  </li>
 </ul>
 
 <h2><a href="rtgm.php">Risk-Targeted Ground Motion Calculator</a></h2>
