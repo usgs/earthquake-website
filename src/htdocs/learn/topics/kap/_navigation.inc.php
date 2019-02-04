2ad0b9c63c1da78df7cf552068d7c1b98980ef9a
Scott Haefner <shaefner@usgs.gov>
2019-02-04 14:44:01 -0800

    update nav

--- src/htdocs/learn/topics/kap/_navigation.inc.php
+++ src/htdocs/learn/topics/kap/_navigation.inc.php
@@ -1,13 +1,19 @@
 <?php
   $section = "/learn/topics/kap";
 
+  $matchesIndex = false;
+  $url = $_SERVER['REQUEST_URI'];
+  if (preg_match("@^$section/((index\.php)?|location\.php)$@", $url)) {
+    $matchesIndex = true;
+  }
+
   echo "<a href='/learn/topics' class='up-one-level'>Earthquake Topics</a>",
 
   "<section>",
 
     "<header>Kite Aerial Photography</header>",
 
-    navItem("${section}/", "Repeat Photography: George Lawrence&rsquo;s &ldquo;San Francisco in Ruins&rdquo;") .
+    navItem("${section}/", "Repeat Photography: George Lawrence&rsquo;s &ldquo;San Francisco in Ruins&rdquo;", $matchesIndex) .
     navItem("${section}/carrizo.php", "San Andreas Fault at Wallace Creek") .
     navItem("/research/parkfield/shake/", "Parkfield Shake Table Exhibit") .
 
