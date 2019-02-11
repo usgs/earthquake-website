84d2cda7c2016df0e4c605641de6c1bc5c3a307c
lisa-usgs <lisa@usgs.gov>
2019-02-11 11:02:22 -0700

    modifications from Gavin

--- src/htdocs/data/finitefault/index.php
+++ src/htdocs/data/finitefault/index.php
@@ -15,15 +15,10 @@ if (!isset($TEMPLATE)) {
 }
 ?>
 
-<h2>Data Process and Inversion</h2>
-<p>
-  We used GSN broadband waveforms downloaded from the NEIC waveform server. We analyzed 46 teleseismic broadband P waveforms, 6 broadband SH waveforms, and 57 long period surface waves selected based on data quality and azimuthal distribution. Waveforms are first converted to displacement by removing the instrument response and are then used to constrain the slip history using a finite fault inverse algorithm (Ji et al., 2002). We begin modeling using a hypocenter matching or adjusted slightly from the initial NEIC solution ( location = 6.0°N 126.8°E, depth = 60.0 km ), and a fault plane defined using either the rapid W-Phase moment tensor (for near-real time solutions), or the gCMT moment tensor (for historic solutions).
-</p>
-
 <div class="row right-to-left" >
 
   <div class="column two-of-five">
-      <h3>Finite Faults, Past 30 Days</h3>
+      <h3>Finite Faults, Past 1 Year</h3>
 
       <div class="recent-products" style="overflow:scroll; height:400px; background:#f4f4f4;">
         <noscript class="error alert">
@@ -36,41 +31,71 @@ if (!isset($TEMPLATE)) {
         <h3>View Map and List of Finite Fault Events</h3>
         <ul>
           <li>
-            <a href="/earthquakes/map/#%7B%22feed%22%3A%221450197623756%22%2C%22search%22%3A%7B%22id%22%3A%221450197623756%22%2C%22name%22%3A%22Search%20Results%22%2C%22isSearch%22%3Atrue%2C%22params%22%3A%7B%22starttime%22%3A%22-1%20week%22%2C%22minfelt%22%3A5%2C%22producttype%22%3A%22finite-fault%22%2C%22orderby%22%3A%22time%22%7D%7D%2C%22listFormat%22%3A%22finite-fault%22%2C%22sort%22%3A%22newest%22%2C%22basemap%22%3A%22terrain%22%2C%22autoUpdate%22%3Afalse%2C%22restrictListToMap%22%3Atrue%2C%22timeZone%22%3A%22utc%22%2C%22mapposition%22%3A%5B%5B-85%2C0%5D%2C%5B85%2C360%5D%5D%2C%22overlays%22%3A%7B%22plates%22%3Atrue%7D%2C%22viewModes%22%3A%7B%22map%22%3Atrue%2C%22list%22%3Atrue%2C%22settings%22%3Afalse%2C%22help%22%3Afalse%7D%7D">Past 7 Days</a>
-          </li>
-          <li>
             <a href="/earthquakes/map/#%7B%22feed%22%3A%221450197866418%22%2C%22search%22%3A%7B%22id%22%3A%221450197866418%22%2C%22name%22%3A%22Search%20Results%22%2C%22isSearch%22%3Atrue%2C%22params%22%3A%7B%22minfelt%22%3A5%2C%22producttype%22%3A%22finite-fault%22%2C%22orderby%22%3A%22time%22%7D%7D%2C%22listFormat%22%3A%22finite-fault%22%2C%22sort%22%3A%22newest%22%2C%22basemap%22%3A%22terrain%22%2C%22autoUpdate%22%3Afalse%2C%22restrictListToMap%22%3Atrue%2C%22timeZone%22%3A%22utc%22%2C%22mapposition%22%3A%5B%5B-85%2C0%5D%2C%5B85%2C360%5D%5D%2C%22overlays%22%3A%7B%22plates%22%3Atrue%7D%2C%22viewModes%22%3A%7B%22map%22%3Atrue%2C%22list%22%3Atrue%2C%22settings%22%3Afalse%2C%22help%22%3Afalse%7D%7D">Past 30 Days
           </a>
+          <li>
+            <a href="/earthquakes/map/#%7B%22feed%22%3A%221450197623756%22%2C%22search%22%3A%7B%22id%22%3A%221450197623756%22%2C%22name%22%3A%22Search%20Results%22%2C%22isSearch%22%3Atrue%2C%22params%22%3A%7B%22starttime%22%3A%22-1%20week%22%2C%22minfelt%22%3A5%2C%22producttype%22%3A%22finite-fault%22%2C%22orderby%22%3A%22time%22%7D%7D%2C%22listFormat%22%3A%22finite-fault%22%2C%22sort%22%3A%22newest%22%2C%22basemap%22%3A%22terrain%22%2C%22autoUpdate%22%3Afalse%2C%22restrictListToMap%22%3Atrue%2C%22timeZone%22%3A%22utc%22%2C%22mapposition%22%3A%5B%5B-85%2C0%5D%2C%5B85%2C360%5D%5D%2C%22overlays%22%3A%7B%22plates%22%3Atrue%7D%2C%22viewModes%22%3A%7B%22map%22%3Atrue%2C%22list%22%3Atrue%2C%22settings%22%3Afalse%2C%22help%22%3Afalse%7D%7D">Past 1 Year</a>
           </li>
+
+          </li>
+      </ul>
+
+      <ul class="no-style linklist">
+
+        <li>
+          <a href="/earthquakes/search/#%7B%22feed%22%3A%221437493916387%22%2C%22search%22%3A%7B%22id%22%3A%221437493916387%22%2C%22name%22%3A%22Search%20Results%22%2C%22isSearch%22%3Atrue%2C%22params%22%3A%7B%22producttype%22%3A%22finite-fault%22%2C%22orderby%22%3A%22time%22%7D%7D%2C%22listFormat%22%3A%22finite-fault%22%2C%22sort%22%3A%22newest%22%2C%22basemap%22%3A%22grayscale%22%2C%22autoUpdate%22%3Afalse%2C%22restrictListToMap%22%3Atrue%2C%22timeZone%22%3A%22utc%22%2C%22mapposition%22%3A%5B%5B-85%2C0%5D%2C%5B85%2C360%5D%5D%2C%22overlays%22%3A%7B%22plates%22%3Atrue%7D%2C%22viewModes%22%3A%7B%22map%22%3Atrue%2C%22list%22%3Atrue%2C%22settings%22%3Atrue%2C%22help%22%3Afalse%7D%7D">
+          <h4>Search Finite Fault Archives</h4>
+          <img src="/data/shakemap/images/search-2x.gif" alt="" width="75"/></a>
+          <p>Search for events that include Finite Faults. The Search form link from here is already configured to return only events with Finite Fault products. You enter additional search parameters. The default time window is the past 30 days.</p>
+        </li>
       </ul>
 
   </div>
 
   <div class="column three-of-five">
 
-    <ul class="no-style linklist">
-
-      <li>
-        <a href="/earthquakes/search/#%7B%22feed%22%3A%221437493916387%22%2C%22search%22%3A%7B%22id%22%3A%221437493916387%22%2C%22name%22%3A%22Search%20Results%22%2C%22isSearch%22%3Atrue%2C%22params%22%3A%7B%22producttype%22%3A%22finite-fault%22%2C%22orderby%22%3A%22time%22%7D%7D%2C%22listFormat%22%3A%22finite-fault%22%2C%22sort%22%3A%22newest%22%2C%22basemap%22%3A%22grayscale%22%2C%22autoUpdate%22%3Afalse%2C%22restrictListToMap%22%3Atrue%2C%22timeZone%22%3A%22utc%22%2C%22mapposition%22%3A%5B%5B-85%2C0%5D%2C%5B85%2C360%5D%5D%2C%22overlays%22%3A%7B%22plates%22%3Atrue%7D%2C%22viewModes%22%3A%7B%22map%22%3Atrue%2C%22list%22%3Atrue%2C%22settings%22%3Atrue%2C%22help%22%3Afalse%7D%7D">
-        <h4>Search Finite Fault Archives</h4>
-        <img src="/data/shakemap/images/search-2x.gif" alt="" width="75"/></a>
-        <p>Search for events that include Finite Faults. The Search form link from here is already configured to return only events with Finite Fault products. You enter additional search parameters. The default time window is the past 30 days.</p>
-      </li>
-    </ul>
+    <h2>Data Process and Inversion</h2>
+    <p>
+      Finite fault models generated by the USGS NEIC generally employ a kinematic finite fault inversion approach based on the method of Ji et al. (2002), which carries out the inversion in the wavelet domain. The procedure inverts both body-wave (P and SH, band-pass filtered between 1 and 200 s) and surface wave (Rayleigh and Love, band-pass filtered between 200 and 500 s) data on a fault surface defined a priori, typically aligned with estimates from CMT (either USGS W-phase or gCMT) solutions.
+    </p>
+    <p>
+      Both nodal planes of the initial CMT solution are tested to account for the uncertainty over which plane describes the causative fault. We may further divide a favored plane into multiple fault segments if required by the data or suggested by other information. Similarly, strike and dip of the inverted fault plane (or planes) are systematically varied from the initial geometry to test model sensitivity to these assumptions.
+    </p>
+    <p>
+      Data are chosen based on producing an azimuthally balanced data set, while avoiding the inclusion of data with small signal-to-noise ratios. Rupture velocity can be fixed or allowed to vary; to account for unknown rupture characteristics, variable velocities are generally tested, as are a broad distribution of fixed velocities, before settling on a final model where rupture velocity is allowed to vary about a favored fixed value.
+    </p>
+    <p>
+      Initial fault length is estimated from empirical relations between duration and moment (Dahlen and Tromp, 1998), scaled to length assuming a rupture velocity of 2.5 km/s, and doubled to account for uncertainty in rupture direction (i.e., centered bilaterally on the hypocenter). Green's functions are generated using a velocity model based on PREM and Crust2.0 (Bassin et al., 2000), and the frequency-wavenumber algorithm of Zhu & Rivera (2001).
+    </p>
+    <p>
+      Fault planes are divided into a series of sub-faults along the strike and dip directions, and the inversion uses a simulated annealing approach to simultaneously solve for the slip amplitude, slip direction, rise-time and rupture initiation time of each sub-fault, where sub-fault source time functions are modeled with an asymmetric cosine function (Ji et al., 2002, 2003).
+    </p>
+    <p>
+      The model is referenced spatially to the USGS NEIC hypocenter, at least initially; as with other key parameters, location is varied when other information (e.g., regional network solutions and/or published studies) indicates a necessary relocation. For more details on this approach and our results for past earthquakes, see Hayes (2017).
+    </p>
 
     <h2>References</h2>
     <ul class="referencelist">
       <li>
-        Ji, C., D.J. Wald, and D.V. Helmberger, Source description of the 1999 Hector Mine, California earthquake; Part I: Wavelet domain inversion theory and resolution analysis, Bull. Seism. Soc. Am., Vol 92, No. 4. pp. 1192-1207, 2002.
+        Bassin, C., Laske, G. and Masters, G., The Current Limits of Resolution for Surface Wave Tomography in North America, EOS Trans AGU, 81, F897, 2000.
       </li>
       <li>
-        Bassin, C., Laske, G. and Masters, G., The Current Limits of Resolution for Surface Wave Tomography in North America, EOS Trans AGU, 81, F897, 2000.
+        Dahlen, F.A., Tromp, J., 1998. Theoretical Global Seismology. Princeton University Press, Princeton, NJ.
+      </li>
+      <li>
+        Hayes, G.P., 2017. The finite, kinematic rupture properties of great-sized earthquakes since 1990. Earth Planet. Sci. Lett. 468, doi:10.1016/j.epsl.2017.04.003.
+      </li>
+      <li>
+        Ji, C., D.J. Wald, and D.V. Helmberger, Source description of the 1999 Hector Mine, California earthquake; Part I: Wavelet domain inversion theory and resolution analysis, Bull. Seism. Soc. Am., Vol 92, No. 4. pp. 1192-1207, 2002.
       </li>
       <li>
         Ji, C., D. V. Helmberger, D. J. Wald, and K. F. Ma (2003), Slip history and dynamic implications of the 1999 Chi-Chi, Taiwan, earthquake, J Geophys Res-Sol Ea, 108(B9).
       </li>
       <li>
         Shao, G. F., X. Y. Li, C. Ji, and T. Maeda (2011), Focal mechanism and slip history of the 2011 M-w 9.1 off the Pacific coast of Tohoku Earthquake, constrained with teleseismic body and surface waves, Earth Planets Space, 63(7), 559-564.
+      </li>
+      <li>
+        Zhu, L., and L. A. Rivera (2001). Computation of dynamic and static displacement from a point source in multi-layered media, Geophys. J. Int. 148, no. 3, 619–627.
       </li>
     </ul>
 
