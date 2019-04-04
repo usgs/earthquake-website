1beedbd54655d43e649732b95cf0c4e4be724258
emthompson@usgs.gov <emthompson@usgs.gov>
2019-04-04 15:27:35 -0600

    Initial catalog description for New Madrid scenairo

---
+++ src/htdocs/scenarios/catalog/nm19/index.php
@@ -0,0 +1,34 @@
+<?php
+include '../_catalog.inc.php';
+?>
+
+
+<h2 id="description">Description</h2>
+
+<p>
+  This catalog includes a single earthquake scenario for the southern arm of
+  the New Madrid Seismic Zone. 
+</p>
+
+
+<h3 id="event-selection">Event Selection</h3>
+
+<p>
+  The earthquake source parameters were selected by Thomas Pratt in coordination
+  with FEMA.
+</p>
+
+
+<h3 id="gmm">Ground Motion Models</h3>
+<p>
+  The ground motions are computed using the same methods as with the 2014
+  BSSC Scenario Catalog, described
+  <a href="../bssc2014/index.php">here</a>.
+</p>
+
+
+<h3 id="contributors">Contributors</h3>
+<ul>
+  <li><a href="../../contributor/us/">US (Scenario Contributor)</a></li>
+</ul>
+
