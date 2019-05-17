116ee803bc8c8ae2ae6fa0ac89bf177102b9f254
Jeremy Fee <jmfee@usgs.gov>
2019-05-17 13:04:28 -0600

    Update oaf list and links to be 1 month

--- src/htdocs/data/oaf/index.js
+++ src/htdocs/data/oaf/index.js
@@ -2,7 +2,7 @@
 
 var EqList = require("listwidget/EqList");
 
-var url = "/fdsnws/event/1/query.geojson?producttype=oaf&starttime=-1+year";
+var url = "/fdsnws/event/1/query.geojson?producttype=oaf&starttime=-1+month";
 
 EqList({
 	container: document.querySelector(".recent-products"),
