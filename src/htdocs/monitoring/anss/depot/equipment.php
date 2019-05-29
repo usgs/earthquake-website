2dadd26e9c839901106c7937ed74d7fa475c497d
lisa-usgs <lisa@usgs.gov>
2019-05-29 09:03:39 -0600

    updated list of ANSS equipment for Paul Bastien

--- src/htdocs/monitoring/anss/depot/equipment.php
+++ src/htdocs/monitoring/anss/depot/equipment.php
@@ -8,27 +8,47 @@
   }
 ?>
 
+<h2>ANSS Depot Supported Equipment</h2>
 <ul>
-  <li>Guralp CMG-5TD</li>
-  <li>Guralp CMG-3 ESP</li>
-  <li>Guralp CMG-3T</li>
-  <li>Kinemetrics Basalt</li>
-  <li>Kinemetrics K2 (4 Channel)</li>
-  <li>Kinemetrics K2 (6 Channel)</li>
-  <li>Kinemetrics Episensor</li>
-  <li>Nanometrics Taurus</li>
-  <li>Nanometrics TridentM</li>
-  <li>Nanometrics Trillium 120</li>
-  <li>Nanometrics Trillium 240</li>
-  <li>Nanometrics Trillium compact</li>
-  <li>NetQuake device</li>
-  <li>Quanterra Q330</li>
+  <li>Kinemetrics EpiSensor</li>
+  <li>Kinemetrics Etna2</li>
+  <li>Kinemetrics Obsidian W/Internal Episensor</li>
+  <li>Nanometrics Centaur (6 Channel)</li>
+  <li>Nanometrics Trillium 120 PA/QA/Horizon</li>
+  <li>Nanometrics Trillium 120 Post Hole</li>
+  <li>Nanometrics Trillium Compact</li>
+  <li>Nanometrics Trillium Compact Post Hole</li>
+  <li>Nanometrics Titan</li>
+  <li>Reftek 130-S (3 Channel)</li>
+  <li>Reftek 130-S (6 Channel)</li>
+  <li>Reftek 151B-120</li>
+  <li>Quanterra Q330HRS</li>
+  <li>Quanterra Q330S+</li>
+  <li>Streckeisen STS-2.5</li>
+</ul>
+
+<h2>Legacy Equipment {typical replacement if no spare is available}</h2>
+<ul>
+  <li>Guralp CMG-5TD {Etna2}</li>
+  <li>Guralp CMG-3 ESP {Trillium Compact}</li>
+  <li>Guralp CMG-3T {Trillium 120 Horizon}</li>
+  <li>Kinemetrics Basalt 4x {Centaur 6 Channel}</li>
+  <li>Kinemetrics Basalt 8x {Obsidian 8x}</li>
+  <li>Kinemetrics Basalt W/Internal Episensor {Obsidian W/Internal Episensor}</li>
+  <li>Kinemetrics K2 (4 Channel) {Etna2}</li>
+  <li>Kinemetrics K2 (6 Channel) {Centaur 6 Channel}</li>
+  <li>Nanometrics Taurus/Trident {Centaur 6 Channel}</li>
+  <li>Nanometrics Trillium 240 {Trillium 120 Horizon or STS-2.5}</li>
+  <li>NetQuake {Etna2}</li>
   <li>Quanterra Baler</li>
-  <li>Reftek 130 (3 Channel)</li>
-  <li>Reftek 130 (6 Channel)</li>
-  <li>Reftek 147</li>
-  <li>Reftek 151-60</li>
-  <li>Reftek 151-60</li>
-  <li>Reftek Mem sensors</li>
-  <li>Streckeisen STS-2</li>
+  <li>Quanterra Q330 3 Channel {Q330S+}</li>
+  <li>Quanterra Q330 6 Channel {Q330S+}</li>
+  <li>Quanterra Q330-HR 6 Channel {Q330HRS}</li>
+  <li>Quanterra Q330-S {Q330S+}</li>
+  <li>Reftek 130 (3 Channel) {130-S}</li>
+  <li>Reftek 130 (6 Channel) {130-S}</li>
+  <li>Reftek 131A {EpiSensor or Titan}</li>
+  <li>Reftek 147 {EpiSensor or Titan}</li>
+  <li>Reftek 151-120 {151B-120}</li>
+  <li>Streckeisen STS-2 {STS-2.5}</li>
 </ul>
