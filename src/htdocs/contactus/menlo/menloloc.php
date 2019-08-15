467e09719192b3de513d33f22e5ad4ee4e90fd33
Scott Haefner <shaefner@usgs.gov>
2019-08-15 12:18:57 -0700

    fix broken image and add new address

--- src/htdocs/contactus/menlo/menloloc.php
+++ src/htdocs/contactus/menlo/menloloc.php
@@ -3,49 +3,62 @@
   // Contact: Steve Hickman,
 
   if (!isset($TEMPLATE)) {
-  $TITLE = 'Earthquake Science Center, Menlo Park, CA';
-  $NAVIGATION = true;
-  include 'template.inc.php';
+    $TITLE = 'Earthquake Science Center, Menlo Park, CA';
+    $NAVIGATION = true;
+    $HEAD = '
+      <style>
+        .directions {
+          margin-top: -.75em;
+        }
+      </style>
+    ';
+
+    include 'template.inc.php';
   }
 ?>
 
 <div class="row">
-  <div class="column one-of-five">
-    <p>
-      <b>Center Director:</b><br/>
-      Steve Hickman
-    </p>
-    <p>
-      <b>Public Information:</b><br/>
-      <a href="mailto:garcia@usgs.gov">Susan Garcia</a><br/>
-      650-329-4668
-    </p>
-    <p>
-      <b>Write </b>to us at:<br/>
-      U.S. Geological Survey<br/>
-      345 Middlefield Road<br/>
-		    MS 977<br/>
-        Menlo Park, California 94025
-      </p>
-  	<p>
-  		<b>Telephone</b> us at:<br/>
-    	650-329-4668, and speak with Susan Garcia, who will help
-    	you find what you&rsquo;re looking for.
-  	</p>
+  <div class="column two-of-five">
+    <h2>Center Director</h2>
+    <p>Steve Hickman</p>
 
-  	<p>
-  		<b>Fax</b> us at:<br/>
-    	650-329-5163
-  	</p>
+    <h2>Public information</h2>
+    <p>
+      <a href="mailto:garcia@usgs.gov">Susan Garcia</a><br />
+      (650) 439-2668
+    </p>
+
+    <h2>Addresses</h2>
+    <p>
+      <b>Moffett Field</b><br />
+      U.S. Geological Survey<br />
+      Earthquake Science Center<br />
+      350 N. Akron Road<br />
+      Moffett Field, CA 94035
+      <p class="directions">
+        <a href="https://www.google.com/maps/search/350+N.+Akron+Road+Moffett+Field,+CA+94035/@37.4121026,-122.0594695,17z/data=!3m1!4b1" title="Interactive map and driving directions">Directions</a>
+      </p>
+    </p>
+    <p>
+      <b>Menlo Park</b><br />
+      U.S. Geological Survey<br />
+      345 Middlefield Road<br/>
+      MS 977<br/>
+      Menlo Park, California 94025
+      <p class="directions">
+        <a href="https://www.google.com/maps/place/345+Middlefield+Rd,+Menlo+Park,+CA+94025/@37.4564874,-122.1734351,17z/data=!3m1!4b1!4m5!3m4!1s0x808fbb527b1c3be3:0x1dcf07a4bc2295a2!8m2!3d37.4564874!4d-122.1712464" title="Interactive map and driving directions">Directions</a>
+      </p>
+    </p>
+
+    <h2>Media only</h2>
+    <p>
+      Susan Garcia<br />
+      (650) 346-0998 (cell)
+    </p>
   </div>
 
-  <div class="column four-of-five">
-  	<a href="http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=345+Middlefield+Rd,+Menlo+Park,+CA+94025&sll=37.0625,-95.677068&sspn=71.686451,83.320312&ie=UTF8&hq=&hnear=345+Middlefield+Rd,+Menlo+Park,+San+Mateo,+California+94025&ll=37.452785,-122.167282&spn=0.118559,0.119133&z=13" title="Click for interactive map and driving directions"><img src="../images/menlomap.png" alt="map of Menlo campus location" width="784" height="586" /></a>
-
-    <p>
-      <b>Campus</b><br/>
-      <a href="https://online.wr.usgs.gov/kiosk/mparea3.html">Menlo Park, CA campus map and driving directions</a>
-    </p>
-
-	</div>
+  <div class="column three-of-five">
+    <h2>Menlo Park Campus Layout</h2>
+    <img src="images/menloCampus.gif" alt="map of Menlo campus" width="600" />
+  </div>
 </div>
