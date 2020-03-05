44d315fc1d3c89e173e13991563ae39b2b1ce2bd
alexandros-savvaidis <34084290+alexandros-savvaidis@users.noreply.github.com>
2020-03-04 20:43:47 -0600

    Update index.php

--- src/htdocs/data/comcat/contributor/tx/index.php
+++ src/htdocs/data/comcat/contributor/tx/index.php
@@ -1,3 +1,59 @@
 <?php
 include '../_contributor.inc.php';
 ?>
+<h2 id="partners">Regional Partners</h2>
+<ul>
+  <li><a href="https://earthquake.usgs.gov/contactus/golden/neic.php" target="_blank">USGS National Earthquake Information Center</a></li>
+  <li><a href="https://www.geology.arkansas.gov/" target="_blank">Arkansas Geological Survey</a></li>
+  <li><a href="https://geoinfo.nmt.edu/nmtso/home.html" target="_blank">New Mexico Tech Seismological Observatory</a></li>
+  <li><a href="http://www.ssn.unam.mx/english/" target="_blank">Servicio Sismologico Nacional</a></li>
+  <li><a href="http://www.ou.edu/ogs/research/earthquakes/recentearthquakes" target="_blank">Oklahoma Geological Survey Statewide Network</a></li>
+</ul>
+
+<h2 id="contributingNetworks">Contributing Networks</h2>
+<ul>
+  <li><a href="https://www.fdsn.org/networks/detail/TX/" target="_blank">Texas Seismological Network (TX)</a></li>
+	<li><a href="http://ds.iris.edu/mda/2T/" target="_blank">Texas Seismological Network: Temporary Stations (2T)</a></li>
+	<li><a href="http://ds.iris.edu/mda/4T/" target="_blank">Texas Seismological Network: Stations out of Texas (4T)</a></li>
+	<li><a href="http://ds.iris.edu/mda/4O/" target="_blank">Texas Seismological Network: Private arrays providing waveform data (4O)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/IM/" target="_blank">International Miscellaneous Stations (IM)</a></li>
+  <li><a href="https://www.fdsn.org/networks/detail/IU/" target="_blank">Global Seismographic Network (IU)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/N4/" target="_blank">Central and Eastern US Network (N4)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/NQ/" target="_blank">NetQuakes (NQ)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/MX/" target="_blank">Mexican National Seismic Network (MX)</a></li>
+  <li><a href="https://www.fdsn.org/networks/detail/OK/" target="_blank">Oklahoma Geological Survey (OK)</a></li>	
+	<li><a href="https://www.fdsn.org/networks/detail/SC/" target="_blank">New Mexico Tech Seismic Network (SC)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/TA/" target="_blank">USArray Transportable Array (TA)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/US/" target="_blank">United States National Seismic Network (US)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/ZP_2016/" target="_blank">Seismic Investigation of South Central Oklahoma (ZP)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/ZW_2013/" target="_blank">North Texas Earthquake Study: Azle and Irving/Dallas (ZW)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/4F_2015/" target="_blank">North Texas Earthquake Study: Venus (Johnson County) (4F)</a></li>
+	<li><a href="https://www.fdsn.org/networks/detail/AM/" target="_blank">Raspberry Shake - OSOP (AM)</a></li>
+</ul>
+
+<h2 id="mission">Mission</h2>
+<p>
+TexNet is an independent scientific body that measures, analyzes, and distributes earthquake data and objective information to benefit the State of Texas.
+</p>
+
+<h2 id="extent">Extent</h2>
+<dl>
+  <dt>2017 to present</dt>
+  <dd>
+    <dl>
+      <dt>Latitude</dt>
+      <dd>[25, 37]</dd>
+
+      <dt>Longitude</dt>
+      <dd>[-107, -93]</dd>
+
+      <dt>Magnitude</dt>
+      <dd>[-1.0, 7.5]</dd>
+    </dl>
+  </dd>
+</dl>
+
+<h3 id="contributors">Contributors</h3>
+<ul>
+<li><a href="../../catalog/tx/">TX (Catalog)</a></li>
+</ul>
