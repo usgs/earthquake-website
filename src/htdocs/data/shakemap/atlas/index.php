a75d4a1cdee6abe6d7fb87ced0cc60ce3dea8c25
Jeremy Fee <jmfee@usgs.gov>
2020-04-24 14:26:29 -0600

    Update atlas browse form

--- src/htdocs/data/shakemap/atlas/index.php
+++ src/htdocs/data/shakemap/atlas/index.php
@@ -42,20 +42,19 @@ if (!isset($TEMPLATE)) {
   <div class="column one-of-two">
     <h3>ShakeMap Atlas, By Year</h3>
 
+    <form action="index.php?year=<?php echo $year ?>" class="browse-form">
+      <label for="year" class="browse-year-label">
+        Enter a year from <?php echo $min_year ?> to
+        <?php echo $current_year ?>:
+      </label>
+
+      <input type="number" class="browse-year" name="year" id="year"
+          min="<?php echo $min_year ?>" max="<?php echo $current_year ?>"
+          step="1" value="<?php echo $year ?>" autofocus/>
+      <button type="submit" class="browse-year-button">Apply</button>
+    </form>
+
     <div class="recent-products" style="overflow:scroll; height:800px; background:#f4f4f4;">
-
-      <form action="index.php?year=<?php echo $year ?>">
-        <label for="year" class="browse-year-label">
-          Enter a year from <?php echo $min_year ?> to
-          <?php echo $current_year ?>:
-        </label>
-
-        <input type="number" class="browse-year" name="year" id="year"
-        min="<?php echo $min_year ?>" max="<?php echo $current_year ?>"
-        step="1" value="<?php echo $year ?>" autofocus/>
-        <button type="submit" class="browse-year-button">Apply</button>
-      </form>
-
       <div id="earthquake-list">
         <noscript>
           This page requires javascript.
@@ -63,8 +62,8 @@ if (!isset($TEMPLATE)) {
           <a href="/earthquakes/search/">use the Earthquake Search</a>.
         </noscript>
       </div>
-
     </div>
+
     <p><a href="http://earthquake.usgs.gov/earthquakes/map/#%7B%22feed%22%3A%221581370083650%22%2C%22sort%22%3A%22newest%22%2C%22basemap%22%3A%22grayscale%22%2C%22restrictListToMap%22%3A%5B%22restrictListToMap%22%5D%2C%22timezone%22%3A%22utc%22%2C%22mapposition%22%3A%5B%5B-85%2C0%5D%2C%5B85%2C360%5D%5D%2C%22overlays%22%3A%5B%22plates%22%5D%2C%22viewModes%22%3A%5B%22list%22%2C%22map%22%5D%2C%22listFormat%22%3A%22shakemap%22%2C%22autoUpdate%22%3Afalse%2C%22search%22%3A%7B%22id%22%3A%221581370083650%22%2C%22name%22%3A%22Search%20Results%22%2C%22isSearch%22%3Atrue%2C%22params%22%3A%7B%22starttime%22%3A%222000-01-01%2000%3A00%3A00%22%2C%22endtime%22%3A%222006-01-01%2000%3A00%3A00%22%2C%22minmagnitude%22%3A2.5%2C%22contributor%22%3A%22atlas%22%2C%22orderby%22%3A%22time%22%2C%22producttype%22%3A%22shakemap%22%7D%7D%7D">View Atlas ShakeMaps from 2000-2005</a></p>
   </div>
 
