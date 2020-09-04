9b075db74ce43cebb6e86b69862cecc506835ac4
lisa-usgs <lisa@usgs.gov>
2020-09-04 13:09:45 -0600

    removed broken links for External Research CF pages

--- src/htdocs/research/cf_footer.php
+++ src/htdocs/research/cf_footer.php
@@ -8,14 +8,7 @@ if (!isset($TEMPLATE)) {
   ob_start();
   $TITLE = 'this doesn\'t matter';
   $NAVIGATION =
-      '<a href="/research/external" class="up-one-level">External Grants</a>' .
-      '<section>' .
-          navItem('/cfusion/external_grants/research.cfm', 'Funded Research') .
-          navItem('/cfusion/external_grants/networks.cfm', 'Funded Networks') .
-          navItem('/cfusion/external_grants/monitoring_upgrades.cfm', 'ARRA-Funded Monitoring Upgrades') .
-          navItem('/research/external/forpis.php', 'Info for PIs') .
-          navItem('/research/external/contact.php', 'Contacts') .
-      '</section>';
+      '<a href="https://www.usgs.gov/natural-hazards/earthquake-hazards/external-grants" class="up-one-level">External Grants</a>';
   include 'template.inc.php';
 }
 
